d53585449928e7451768d01293515ae02ad47441
Raphael Amiard <amiard@adacore.com>
2016-03-25 17:03:57 +0100

    Shorten Let expression test
    TN: P303-009
    Change-Id: Ia23bc181265d5299fb4f8b4d94093fb77195be6a

--- testsuite/tests/properties/invalid_let/test.py
+++ testsuite/tests/properties/invalid_let/test.py
@@ -23,53 +23,18 @@ def run(name, expr):
     print('== {} =='.format(name))
     reset_langkit()
 
-    @abstract
     @root_grammar_class
     class FooNode(ASTNode):
         pass
 
-    @abstract
-    class BodyNode(FooNode):
-        pass
-
-    class NullNode(BodyNode):
-        pass
-
-    @abstract
-    class Expression(BodyNode):
-        pass
-
-    class Number(Expression):
-        tok = Field()
-
-    class Compound(Expression):
-        prefix = Field()
-        suffix = Field()
-
-    class ExampleNode(FooNode):
-        body = Field()
-
+    class BarNode(FooNode):
         prop = Property(expr)
 
     foo_grammar = Grammar()
     foo_grammar.main_rule_name = 'main_rule'
     foo_grammar.add_rules(
-        main_rule=Row(
-            'example',
-            Or(foo_grammar.expression,
-               Row('null') ^ NullNode)
-        ) ^ ExampleNode,
-
-        number=Tok(Token.Number) ^ Number,
-
-        expression=Or(
-            Row(foo_grammar.number,
-                ',',
-                foo_grammar.expression) ^ Compound,
-            foo_grammar.number
-        ),
+        main_rule=Row('example') ^ BarNode,
     )
-
     emit_and_print_errors(foo_grammar)
     print('')
 
