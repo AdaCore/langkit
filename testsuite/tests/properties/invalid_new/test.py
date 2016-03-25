da5354c4f7e371f9dd778362017855c7d1f53829
Raphael Amiard <amiard@adacore.com>
2016-03-25 22:38:22 +0100

    P303-009: Add a test for invalid new expr
    Change-Id: I71e3b731b82d5245e8a2994c123471a4f40fbacc

---
+++ testsuite/tests/properties/invalid_new/test.py
@@ -0,0 +1,50 @@
+from langkit.compiled_types import (
+    ASTNode, root_grammar_class, LongType, Field, Struct
+)
+from langkit.diagnostics import LangSourceDir
+from langkit.expressions import Property, New, Literal, No
+from langkit.parsers import Grammar, Row
+
+from os import path
+from utils import emit_and_print_errors, reset_langkit
+
+
+def run(name, expr):
+    """
+    Emit and print the errors we get for the below grammar with "expr" as
+    a property in BarNode.
+    """
+
+    global FooNode, BarNode, MyStruct
+
+    LangSourceDir.set_lang_source_dir(path.abspath(__file__))
+
+    print('== {} =='.format(name))
+    reset_langkit()
+
+    class MyStruct(Struct):
+        a = Field(type=LongType)
+        b = Field(type=LongType)
+
+    @root_grammar_class
+    class FooNode(ASTNode):
+        pass
+
+    class BarNode(FooNode):
+        prop = Property(expr)
+
+    foo_grammar = Grammar()
+    foo_grammar.main_rule_name = 'main_rule'
+    foo_grammar.add_rules(
+        main_rule=Row('example') ^ BarNode,
+    )
+    emit_and_print_errors(foo_grammar)
+    print('')
+
+
+run("Correct code", lambda: New(MyStruct, a=Literal(12), b=Literal(15)))
+run("Incorrect new 1", lambda: New(MyStruct, a=Literal(12)))
+run("Incorrect new 2", lambda: New(MyStruct, a=Literal(12), b=No(FooNode)))
+run("Incorrect new 1", lambda: New(MyStruct, a=Literal(12), b=Literal(15),
+                                   c=Literal(19)))
+print 'Done'
