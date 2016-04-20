953eb75d73b38da7f6b80d05f6790df075dc2d46
Pierre-Marie de Rodat <derodat@adacore.com>
2016-04-20 18:05:54 +0200

    Add a testcase for type consistency checks in properties overriding
    Change-Id: Ib2feab7501ef910c149d616c5f0eded9cd04529d
    TN: minor

---
+++ testsuite/tests/properties/override/test.py
@@ -0,0 +1,64 @@
+"""
+Test that when a property B overrides a property A, B's return type must be a
+subtype of A's.
+"""
+
+import os.path
+
+from langkit.compiled_types import (
+    ASTNode, TypeRepo, abstract, root_grammar_class
+)
+from langkit.diagnostics import Diagnostics
+from langkit.expressions import AbstractProperty, No, Property, Self
+from langkit.parsers import Grammar, Or, Row
+
+from utils import emit_and_print_errors, reset_langkit
+
+
+def run(name, astnode_fn):
+    """
+    Emit and print the errors we get for the below grammar with "match_expr" as
+    a property in ExampleNode.
+    """
+
+    Diagnostics.set_lang_source_dir(os.path.abspath(__file__))
+
+    print('== {} =='.format(name))
+    reset_langkit()
+
+    T = TypeRepo()
+    astnode = astnode_fn(T)
+
+    @abstract
+    @root_grammar_class
+    class FooNode(ASTNode):
+        pass
+
+    @abstract
+    class MiddleNode(FooNode):
+        get_random_node = AbstractProperty(type=T.MiddleNode)
+
+    class ExampleNode(MiddleNode):
+        get_random_node = Property(No(astnode))
+
+    @abstract
+    class NullNode(FooNode):
+        pass
+
+    foo_grammar = Grammar()
+    foo_grammar.main_rule_name = 'main_rule'
+    foo_grammar.add_rules(
+        main_rule=Or(Row('example') ^ ExampleNode,
+                     Row('null') ^ NullNode)
+    )
+
+    emit_and_print_errors(foo_grammar)
+    print('')
+
+
+# Incomplete set of matchers
+run('FooNode', lambda T: T.FooNode)
+run('MiddleNode', lambda T: T.MiddleNode)
+run('NullNode', lambda T: T.NullNode)
+
+print 'Done'
