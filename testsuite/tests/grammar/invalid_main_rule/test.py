b4e44f1c05b53f4e1d923d51fc0aabdb33d0449a
Raphael Amiard <amiard@adacore.com>
2016-04-29 15:23:36 +0200

    P429-013: Add test for wrong main rule detection
    Change-Id: Ia59f83488e5e23718e6748054394091518b91f7e

---
+++ testsuite/tests/grammar/invalid_main_rule/test.py
@@ -0,0 +1,29 @@
+from langkit.compiled_types import ASTNode, abstract, root_grammar_class
+from langkit.diagnostics import Diagnostics
+from langkit.parsers import Grammar, Row
+
+from os import path
+
+from utils import emit_and_print_errors
+
+Diagnostics.set_lang_source_dir(path.abspath(__file__))
+
+
+@abstract
+@root_grammar_class
+class FooNode(ASTNode):
+    pass
+
+
+class ExampleNode(FooNode):
+    pass
+
+
+foo_grammar = Grammar('main_rulezz')
+foo_grammar.add_rules(
+    main_rule=Row('example') ^ ExampleNode
+)
+
+
+emit_and_print_errors(foo_grammar)
+print 'Done'
