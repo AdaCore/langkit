bf7a7a81435bdc1fece461397297c0e25314868b
Pierre-Marie de Rodat <derodat@adacore.com>
2016-05-02 18:30:39 +0200

    Remove duplicate source code emission for Ada units in the C API
    Change-Id: I4053e8171b7d6185668afd7d2f7e01313a0f3826
    TN: P428-014

--- langkit/compile_context.py
+++ langkit/compile_context.py
@@ -827,9 +827,6 @@ class CompileCtx():
 
         self.write_ada_module(src_path, "c_api/pkg_analysis",
                               ["Analysis", "C"])
-        self.write_ada_module(src_path, "c_api/pkg_analysis",
-                              ["Analysis", "C"])
-        self.write_ada_module(src_path, "c_api/pkg_ast", ["AST", "Types", "C"])
         self.write_ada_module(src_path, "c_api/pkg_ast", ["AST", "Types", "C"])
 
     def emit_python_api(self, python_path):
