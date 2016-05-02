31809e0039bb68453d6d3e556fdd88ddfa698229
Raphael Amiard <amiard@adacore.com>
2016-05-02 13:32:19 +0200

    P303-009: Add tests for invalid grammar rules
    Change-Id: I282e490c42b9d69a90bb721eb83170f8384d52fc

---
+++ testsuite/tests/grammar/invalid_rule_dup/test.py
@@ -0,0 +1,32 @@
+from langkit.compiled_types import ASTNode, abstract, root_grammar_class
+from langkit.diagnostics import Diagnostics
+from langkit.parsers import Grammar, Row
+
+from os import path
+
+from utils import emit_and_print_errors
+
+Diagnostics.set_lang_source_dir(path.abspath(__file__))
+
+
+def lang_def():
+    @abstract
+    @root_grammar_class
+    class FooNode(ASTNode):
+        pass
+
+    class ExampleNode(FooNode):
+        pass
+
+    foo_grammar = Grammar('main_rule')
+    foo_grammar.add_rules(
+        sec_rule=Row('example'),
+    )
+
+    foo_grammar.add_rules(
+        sec_rule=Row('example'),
+        main_rule=Row(foo_grammar.sec_rule) ^ ExampleNode
+    )
+
+emit_and_print_errors(lang_def)
+print 'Done'
