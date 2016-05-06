31ed56ab41b6cce8149a1b8267033d38b9d894a5
Raphael Amiard <amiard@adacore.com>
2016-05-06 18:02:16 +0200

    O413-018: Add test for invalid property literals
    Change-Id: I13c8b0783d77a5d00305a2dac0f7e1b726b85ace

---
+++ testsuite/tests/properties/invalid_prop_literal/test.py
@@ -0,0 +1,31 @@
+from langkit.compiled_types import (
+    ASTNode, root_grammar_class, Field
+)
+from langkit.diagnostics import Diagnostics
+from langkit.expressions import Property, Self
+from langkit.parsers import Grammar, Row
+
+from os import path
+from utils import emit_and_print_errors
+
+
+Diagnostics.set_lang_source_dir(path.abspath(__file__))
+
+
+def make_lang_def(lit):
+    def lang_def():
+        @root_grammar_class
+        class FooNode(ASTNode):
+            b = Property(lit)
+
+        foo_grammar = Grammar('main_rule')
+        foo_grammar.add_rules(main_rule=Row('example') ^ FooNode)
+        return foo_grammar
+    return lang_def
+
+print 'Valid case'
+emit_and_print_errors(make_lang_def(12))
+print 'Invalid case'
+emit_and_print_errors(make_lang_def("lol"))
+print('')
+print 'Done'
