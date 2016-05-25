ebea8b0e56f01389c5cc491c2a872fad8f38a1f8
Pierre-Marie de Rodat <derodat@adacore.com>
2016-05-25 18:00:45 -0400

    Testsuite: build Adalog during the tear_up phase
    Change-Id: I0721274451646bde9d640d777906542c941cf584
    TN: P524-025

--- testsuite/testsuite_support/__init__.py
+++ testsuite/testsuite_support/__init__.py
@@ -3,13 +3,14 @@ import os
 with_gnatpython = False
 if not os.environ.get('WITHOUT_GNATPYTHON'):
     try:
+        from gnatpython.ex import Run
         from gnatpython.testsuite import Testsuite as BaseTestsuite
     except ImportError:
         pass
     else:
         with_gnatpython = True
 if not with_gnatpython:
-    from testsuite_support.polyfill import BaseTestsuite
+    from testsuite_support.polyfill import BaseTestsuite, Run
 
 
 import testsuite_support.adalog_driver
@@ -37,3 +38,14 @@ class Testsuite(BaseTestsuite):
             '--rewrite', '-r', action='store_true',
             help='Rewrite test baselines according to current output.'
         )
+
+    def tear_up(self):
+        # Build Adalog so that each testcase do not try to build it in
+        # parallel.
+        p = Run(['gprbuild', '-p', '-q', '-P',
+                 os.path.join(self.root_dir, '..', 'langkit', 'adalog',
+                              'adalog.gpr')])
+        if p.status != 0:
+            raise RuntimeError(
+                'Adalog build failed (GPRbuild returned {})'.format(p.status)
+            )
