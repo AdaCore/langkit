6e2ca35572525ec5804105fce806c6d3857a7e9d
Pierre-Marie de Rodat <derodat@adacore.com>
2016-06-17 16:15:01 +0200

    Add a testcase for the use of unbound env hook
    Change-Id: I47812441e4ea2ef2c20a8e56e593541a42644cdc
    TN: P518-014

---
+++ testsuite/tests/lexical_envs/env_hook_unbound/test.py
@@ -0,0 +1,33 @@
+import os.path
+
+from langkit.compiled_types import ASTNode, root_grammar_class
+from langkit.diagnostics import Diagnostics
+from langkit.envs import EnvSpec
+from langkit.expressions import Property, Self
+from langkit.parsers import Grammar, Row
+
+from utils import emit_and_print_errors
+
+
+Diagnostics.set_lang_source_dir(os.path.abspath(__file__))
+
+
+@root_grammar_class
+class FooNode(ASTNode):
+    pass
+
+
+class BarNode(FooNode):
+    env_spec = EnvSpec(env_hook_arg=Self)
+
+
+def lang_def():
+    foo_grammar = Grammar('main_rule')
+    foo_grammar.add_rules(
+        main_rule=Row('example') ^ BarNode,
+    )
+    return foo_grammar
+
+
+emit_and_print_errors(lang_def)
+print 'Done'
