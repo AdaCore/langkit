fac1e2e7b11b2b8690aec21a19da23f908b3a7b7
Raphael Amiard <amiard@adacore.com>
2016-06-27 12:51:30 +0200

    OA27-034: Add Langkit_Support.Cheap_Sets
    Change-Id: I04eb0a22c9a0097b7cc561f63133f54ca2cbcf5a

---
+++ langkit/support/langkit_support-cheap_sets.adb
@@ -0,0 +1,145 @@
+with Ada.Unchecked_Deallocation;
+
+package body Langkit_Support.Cheap_Sets is
+
+   function Find
+     (Self      : Set;
+      E         : Element_Type;
+      Do_Remove : Boolean := True) return Boolean;
+
+   ------------
+   -- Remove --
+   ------------
+
+   function Remove
+     (Self      : Set;
+      E         : Element_Type) return Boolean is
+   begin
+      return Find (Self, E, Do_Remove => True);
+   end Remove;
+
+   ---------
+   -- Add --
+   ---------
+
+   function Add (Self : in out Set; E : Element_Type) return Boolean is
+      use Elements_Vectors;
+
+      type El_Access is access all Element_Type;
+      First_No : El_Access := null;
+   begin
+
+      --  Create the inner vector if it has not been created yet
+
+      if Self.Elements = null then
+         Self.Elements := new Elements_Vectors.Vector;
+      end if;
+
+      --  Search for an element equal to E. Also search for the first
+      --  No_Element in the same run.
+
+      for I in First_Index (Self.Elements.all)
+        .. Last_Index (Self.Elements.all)
+      loop
+         declare
+            Current : constant Element_Access
+              := Get_Access (Self.Elements.all, I);
+         begin
+
+            --  We found a No_Element. Keep it for later if we have to add E to
+            --  the set.
+
+            if Current.all = No_Element then
+               First_No := El_Access (Current);
+            end if;
+
+            --  We found E. Return False, no need to insert anything in the
+            --  set.
+
+            if Current.all = E then
+               return False;
+            end if;
+         end;
+      end loop;
+
+      --  Add the element to the set, either at the first empty cell we found,
+      --  or in a new cell.
+
+      if First_No /= null then
+         First_No.all := E;
+      else
+         Append (Self.Elements.all, E);
+      end if;
+
+      return True;
+   end Add;
+
+   ----------
+   -- Find --
+   ----------
+
+   function Find
+     (Self      : Set;
+      E         : Element_Type;
+      Do_Remove : Boolean := True) return Boolean
+   is
+      use Elements_Vectors;
+   begin
+      --  If inner vector was not created, we're sure there is no element
+      --  corresponding to E.
+
+      if Self.Elements = null then
+         return False;
+      end if;
+
+      --  Else, let's search in the inner vector for E
+
+      for I in First_Index (Self.Elements.all)
+        .. Last_Index (Self.Elements.all)
+      loop
+         declare
+            Current : constant Element_Access
+              := Get_Access (Self.Elements.all, I);
+         begin
+
+            --  We found something, delete the element if specified and return
+            --  True.
+
+            if Current.all = E then
+               if Do_Remove then
+                  Current.all := No_Element;
+               end if;
+               return True;
+            end if;
+         end;
+      end loop;
+
+      --  We found nothing
+
+      return False;
+   end Find;
+
+   ---------
+   -- Has --
+   ---------
+
+   function Has (Self : Set; E : Element_Type) return Boolean is
+   begin
+      return Find (Self, E, False);
+   end Has;
+
+   ----------
+   -- Free --
+   ----------
+
+   procedure Destroy (Self : in out Set) is
+      procedure Unchecked_Free is new Ada.Unchecked_Deallocation
+        (Elements_Vectors.Vector, Elements_Vector);
+   begin
+      if Self.Elements /= null then
+         Elements_Vectors.Destroy (Self.Elements.all);
+         Unchecked_Free (Self.Elements);
+      end if;
+   end Destroy;
+
+end Langkit_Support.Cheap_Sets;
---
+++ langkit/support/langkit_support-cheap_sets.ads
@@ -0,0 +1,47 @@
+with Langkit_Support.Vectors;
+
+--  This package implements generic sets in a fast and cheap way, using a
+--  vector underneath. This is done because:
+--
+--  1. Ada.Containers.Sets are controlled objects, which is not always
+--  acceptable.
+--
+--  2. It is expected that for the size of the data sets that we have, an array
+--  will be more efficient.
+--
+--  3. Formal sets could be an option, but you have to precise their size in
+--  advance, which is not convenient in our case.
+
+generic
+   type Element_Type is private;
+   No_Element : Element_Type;
+   with function "=" (L, R : Element_Type) return Boolean is <>;
+package Langkit_Support.Cheap_Sets is
+
+   type Set is private;
+   --  For ease of use in our case, Set is made to take a minimal amount of
+   --  space when it is not used (size of an access).
+
+   function Add (Self : in out Set; E : Element_Type) return Boolean;
+   --  Add a new element to the vector. Returns True if the element was added,
+   --  False if it was already in the set.
+
+   function Remove (Self : Set; E : Element_Type) return Boolean;
+   --  Remove an element from the set. Return True if the element was removed,
+   --  False if it wasn't in the set.
+
+   function Has (Self : Set; E : Element_Type) return Boolean;
+   --  Return whether E is part of the set
+
+   procedure Destroy (Self : in out Set);
+   --  Destroy the set
+
+private
+   package Elements_Vectors is new Langkit_Support.Vectors (Element_Type);
+   type Elements_Vector is access all Elements_Vectors.Vector;
+
+   type Set is record
+      Elements : Elements_Vector := null;
+   end record;
+
+end Langkit_Support.Cheap_Sets;
