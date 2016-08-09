80a1b8ddcc6b4a50a0cced07d0879d03a430a278
Raphael Amiard <amiard@adacore.com>
2016-08-09 13:49:55 +0200

    OA27-034: Fix mem management in Variadic_And/Or
    Change-Id: Ifa7521f0f6e6f3d94047ef97691a7a499ddc9698

--- langkit/adalog/src/adalog-variadic_operations.adb
+++ langkit/adalog/src/adalog-variadic_operations.adb
@@ -30,14 +30,30 @@ package body Adalog.Variadic_Operations is
    ------------------
 
    function Variadic_And (Rels : Relation_Array) return Relation is
-      Ret : Relation;
+      Ret, Old_Ret : Relation;
    begin
       pragma Assert (Rels'Length > 0);
 
       Ret := Rels (Rels'First);
+      Inc_Ref (Ret);
+
+      --  We Inc_Ref here because:
+      --
+      --  - If Rels has only one element, we return a new ownership share for
+      --    an already existing relation.
+      --
+      --  - If Rels has several elements, this share will be automatically
+      --    Dec_Ref'd in the for loop below.
 
       for I in Rels'First + 1 .. Rels'Last loop
+         Old_Ret := Ret;
          Ret := Relation (Ret and Rels (I));
+
+         Dec_Ref (Old_Ret);
+         --  Here we Dec_Ref, because either Old_Ret is the first rel and
+         --  has been Inc_Ref'd before, either it is an And relation that was
+         --  created with an ownership that we now renounce, because it is
+         --  owned by the new And.
       end loop;
 
       return Ret;
@@ -48,14 +64,18 @@ package body Adalog.Variadic_Operations is
    -----------------
 
    function Variadic_Or (Rels : Relation_Array) return Relation is
-      Ret : Relation;
+      Ret, Old_Ret : Relation;
    begin
+      --  See Variadic_Or for documentation of the memory management
       pragma Assert (Rels'Length > 0);
 
       Ret := Rels (Rels'First);
+      Inc_Ref (Ret);
 
       for I in Rels'First + 1 .. Rels'Last loop
+         Old_Ret := Ret;
          Ret := Ret or Rels (I);
+         Dec_Ref (Old_Ret);
       end loop;
 
       return Ret;
