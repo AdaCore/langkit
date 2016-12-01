183a07e30be9ad7b5dc27b67aad7ff30ca20d201
Pierre-Marie de Rodat <derodat@adacore.com>
2016-12-01 12:05:53 +0100

    Add a testcase for Env usage validity checking wrt. implicit envs
    Change-Id: Iaf7ef0b4039d89065de1f16779745a125fae45af
    TN: PB18-030

---
+++ testsuite/tests/properties/invalid_env/test.py
@@ -0,0 +1,50 @@
+from langkit.compiled_types import (
+    ASTNode, root_grammar_class, LongType, Field
+)
+from langkit.diagnostics import Diagnostics
+from langkit.expressions import Env, Property, Self
+from langkit.parsers import Grammar, Row, List, Tok
+
+from lexer_example import Token
+from os import path
+from utils import emit_and_print_errors
+
+
+def run(expr):
+    """
+    Emit and print the errors we get for the below grammar for the given
+    "expr" property expression.
+    """
+
+    Diagnostics.set_lang_source_dir(path.abspath(__file__))
+
+    print '== {} =='.format(expr)
+
+    @root_grammar_class()
+    class FooNode(ASTNode):
+        pass
+
+    class ExampleNode(FooNode):
+        tok = Field()
+
+        implicit_prop = Property(Self, has_implicit_env=True)
+        explicit_prop = Property(Self, has_implicit_env=False)
+
+        prop = Property(expr, has_implicit_env=False)
+
+    def lang_def():
+        foo_grammar = Grammar('main_rule')
+        foo_grammar.add_rules(
+            main_rule=Row(Tok('example', keep=True)) ^ ExampleNode,
+        )
+        return foo_grammar
+
+    emit_and_print_errors(lang_def)
+    print('')
+
+
+run(Env.get(Self.tok))
+run(Self.implicit_prop)
+run(Self.node_env.eval_in_env(Env.get(Self.tok)))
+run(Self.node_env.eval_in_env(Self.implicit_prop))
+print 'Done'
