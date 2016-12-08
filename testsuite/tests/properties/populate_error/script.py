4bb4023f5c0485731a6e4c6ff4ff9e85948af758
Pierre-Marie de Rodat <derodat@adacore.com>
2016-12-08 16:52:55 +0100

    Add a testcase for Populate_Lexical_Env exc. handling in C/Python APIs
    Change-Id: I3302038693dfc624338e1c5882f4169b98287be1
    TN: PC02-013

---
+++ testsuite/tests/properties/populate_error/script.py
@@ -0,0 +1,20 @@
+import sys
+
+import libfoolang
+
+
+ctx = libfoolang.AnalysisContext()
+u = ctx.get_from_buffer('source.txt', 'example')
+if u.diagnostics:
+    for d in u.diagnostics:
+        print(d)
+    sys.exit(1)
+
+print 'Calling AnalysisUnit.populate_lexical_env()...'
+try:
+    u.populate_lexical_env()
+except libfoolang.PropertyError:
+    print 'Got a PropertyError as expected!'
+else:
+    print 'Got no PropertyError, which is unexpected!'
+print 'Done.'
---
+++ testsuite/tests/properties/populate_error/test.py
@@ -0,0 +1,28 @@
+import os.path
+
+from langkit.compiled_types import ASTNode, Struct, root_grammar_class
+from langkit.diagnostics import Diagnostics
+from langkit.envs import EnvSpec
+from langkit.expressions import Self
+from langkit.parsers import Grammar, Row
+
+from utils import build_and_run
+
+
+Diagnostics.set_lang_source_dir(os.path.abspath(__file__))
+
+
+@root_grammar_class()
+class FooNode(ASTNode):
+    pass
+
+
+class Example(FooNode):
+    env_spec = EnvSpec(initial_env=Self.parent.parent.children_env)
+
+
+foo_grammar = Grammar('main_rule')
+foo_grammar.add_rules(
+    main_rule=Row('example') ^ Example,
+)
+build_and_run(foo_grammar, 'script.py')
