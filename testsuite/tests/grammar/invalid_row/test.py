0b45b66396bef28719d730c46388c0bffeec5a92
Pierre-Marie de Rodat <derodat@adacore.com>
2016-12-14 17:14:45 +0100

    Add a testcase for badly typed top-level grammar rules
    Change-Id: I195ab9980f31e13d99a4c712f529e2c15d7ef4a9
    TN: PB18-030

---
+++ testsuite/tests/grammar/invalid_row/test.py
@@ -0,0 +1,36 @@
+"""
+Test that top-level Row parsers are properly reported as errors.
+"""
+
+import os.path
+
+from langkit.compiled_types import ASTNode, Field, root_grammar_class
+from langkit.diagnostics import Diagnostics
+from langkit.parsers import Grammar, Row, List, Tok
+
+from lexer_example import Token
+from utils import emit_and_print_errors
+
+
+Diagnostics.set_lang_source_dir(os.path.abspath(__file__))
+
+
+@root_grammar_class()
+class FooNode(ASTNode):
+    pass
+
+
+class Literal(FooNode):
+    tok = Field()
+
+
+def lang_def():
+    foo_grammar = Grammar('main_rule')
+    foo_grammar.add_rules(
+        main_rule=Row(List(Tok(Token.Number, keep=True) ^ Literal)),
+    )
+    return foo_grammar
+
+
+emit_and_print_errors(lang_def)
+print('Done')
