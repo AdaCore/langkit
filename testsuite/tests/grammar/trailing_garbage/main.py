45ca152bc9834ae962ca11f63ef5479d154983cb
Pierre-Marie de Rodat <derodat@adacore.com>
2016-12-14 10:50:07 +0100

    Add a testcase for parsing error for garbage text after main rule compl.
    Change-Id: Ie2a13d27e42ab3aa0eb6f350316808904a0da1ce
    TN: PC13-017

---
+++ testsuite/tests/grammar/trailing_garbage/main.py
@@ -0,0 +1,12 @@
+import sys
+
+import libfoolang
+
+
+ctx = libfoolang.AnalysisContext()
+
+text = '1 2'
+u = ctx.get_from_buffer('main.txt', text)
+if u.diagnostics:
+    for d in u.diagnostics:
+        print(d)
---
+++ testsuite/tests/grammar/trailing_garbage/test.py
@@ -0,0 +1,33 @@
+"""
+Test that garbage tokens left after the main parsing rule completes does not
+crash. It used to!
+"""
+
+import os.path
+
+from langkit.compiled_types import ASTNode, Field, root_grammar_class
+from langkit.diagnostics import Diagnostics
+from langkit.parsers import Grammar, Row, Tok
+
+from lexer_example import Token
+from utils import build_and_run
+
+
+Diagnostics.set_lang_source_dir(os.path.abspath(__file__))
+
+
+@root_grammar_class()
+class FooNode(ASTNode):
+    pass
+
+
+class Literal(FooNode):
+    tok = Field()
+
+
+foo_grammar = Grammar('main_rule')
+foo_grammar.add_rules(
+    main_rule=Row(Tok(Token.Number, keep=True)) ^ Literal,
+)
+build_and_run(foo_grammar, 'main.py')
+print 'Done'
