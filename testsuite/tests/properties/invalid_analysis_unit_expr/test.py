3efcbc86bb955b33c7f5a283d5c5628903b27908
Pierre-Marie de Rodat <derodat@adacore.com>
2016-12-23 16:31:35 +0100

    Add a testcase for invalid field access for analysis unit in properties
    Change-Id: Icd81827ec9cbc4ae7d4e2b7fcbc2629c014241e9
    TN: PC20-010

---
+++ testsuite/tests/properties/invalid_analysis_unit_expr/test.py
@@ -0,0 +1,38 @@
+"""
+Test that invalid uses of analysis unit in the properties DSL are properly
+detected and reported.
+"""
+
+import os.path
+
+from langkit.compiled_types import (
+    AnalysisUnitType, ASTNode, Field, abstract, root_grammar_class
+)
+from langkit.diagnostics import Diagnostics
+from langkit.expressions import Property, Self
+from langkit.parsers import Grammar, Row
+
+from utils import emit_and_print_errors
+
+
+def run(name, prop):
+    Diagnostics.set_lang_source_dir(os.path.abspath(__file__))
+    print('== {} =='.format(name))
+
+    @root_grammar_class()
+    class FooNode(ASTNode):
+        pass
+
+    class Example(FooNode):
+        result = Property(prop)
+
+    def lang_def():
+        foo_grammar = Grammar('main_rule')
+        foo_grammar.add_rules(main_rule=Row('example') ^ Example)
+        return foo_grammar
+    emit_and_print_errors(lang_def)
+    print('')
+
+
+run('Invalid field', Self.unit.foobar)
+print 'Done'
