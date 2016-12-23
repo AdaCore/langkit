3ada86afb488cfd165feb18dd0f9796a08a6c71e
Pierre-Marie de Rodat <derodat@adacore.com>
2016-12-23 16:30:50 +0100

    Add a test for the ASTNode.unit primitive in the properties DSL
    Change-Id: I51d01606fe1af81f97e6b13c20d14c50ccc9fe9b
    TN: PC20-010

---
+++ testsuite/tests/properties/analysis_unit_from_node/main.py
@@ -0,0 +1,20 @@
+print 'main.py: Running...'
+
+
+import sys
+
+import libfoolang
+
+
+ctx = libfoolang.AnalysisContext()
+u = ctx.get_from_file('main.txt')
+if u.diagnostics:
+    for d in u.diagnostics:
+        print(d)
+    sys.exit(1)
+
+u.populate_lexical_env()
+for node in u.root.findall(lambda _: True):
+    print '{}.root = {}'.format(node, node.p_root_node)
+
+print 'main.py: Done.'
---
+++ testsuite/tests/properties/analysis_unit_from_node/test.py
@@ -0,0 +1,51 @@
+"""
+Test getting the analysis unit of a node in the properties DSL.
+"""
+
+import os.path
+
+from langkit.compiled_types import (
+    AnalysisUnitType, ASTNode, Field, LongType, abstract, root_grammar_class
+)
+from langkit.diagnostics import Diagnostics
+from langkit.expressions import (
+    AbstractProperty, ExternalProperty, Property, Self
+)
+from langkit.parsers import Grammar, Or, Row, Tok
+
+from lexer_example import Token
+from utils import build_and_run
+
+
+Diagnostics.set_lang_source_dir(os.path.abspath(__file__))
+
+
+@root_grammar_class()
+class FooNode(ASTNode):
+    root_node = Property(Self.unit.root)
+
+
+@abstract
+class Expression(FooNode):
+    pass
+
+
+class Literal(Expression):
+    tok = Field()
+
+
+class Plus(Expression):
+    left = Field()
+    right = Field()
+
+
+foo_grammar = Grammar('main_rule')
+foo_grammar.add_rules(
+    main_rule=Or(
+        Row(foo_grammar.atom, '+', foo_grammar.main_rule) ^ Plus,
+        foo_grammar.atom
+    ),
+    atom=Row(Tok(Token.Number, keep=True)) ^ Literal,
+)
+build_and_run(foo_grammar, 'main.py')
+print 'Done'
