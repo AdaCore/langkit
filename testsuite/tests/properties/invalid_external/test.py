909225ada4e53444db1b500293503134f29ef957
Pierre-Marie de Rodat <derodat@adacore.com>
2016-12-23 14:38:44 +0100

    Add a testcase for invalid external properties
    Change-Id: Idfaf355bc972c26da597cf693ce50beeb4fd749f
    TN: PC13-017

---
+++ testsuite/tests/properties/invalid_external/test.py
@@ -0,0 +1,50 @@
+from langkit.compiled_types import ASTNode, T, abstract, root_grammar_class
+from langkit.diagnostics import Diagnostics
+from langkit.expressions import ExternalProperty, Property, Self
+from langkit.parsers import Grammar, Row, Or
+
+from os import path
+from utils import emit_and_print_errors
+
+
+def run(name, abstract_prop, prop=None):
+    """
+    Emit and print the errors we get for the below grammar with "expr" as
+    a property in BarNode.
+    """
+
+    Diagnostics.set_lang_source_dir(path.abspath(__file__))
+
+    print('== {} =='.format(name))
+
+    @root_grammar_class()
+    class FooNode(ASTNode):
+        pass
+
+    @abstract
+    class AbstractExample(FooNode):
+        p = abstract_prop()
+
+    class Example(AbstractExample):
+        p = prop() if prop else None
+
+    def lang_def():
+        foo_grammar = Grammar('main_rule')
+        foo_grammar.add_rules(
+            main_rule=Row('example') ^ Example,
+        )
+        return foo_grammar
+    emit_and_print_errors(lang_def)
+    print('')
+
+
+run("Missing type",
+    lambda: ExternalProperty())
+
+run("Invalid abstract",
+    lambda: ExternalProperty(abstract=True, type=T.FooNode),
+    lambda: Property(Self))
+
+run("Invalid memoized",
+    lambda: ExternalProperty(memoized=True))
+print 'Done'
