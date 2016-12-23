f92c6a8bbd02714a34fb9ba6c96afbac93f32363
Pierre-Marie de Rodat <derodat@adacore.com>
2016-12-23 15:20:09 +0100

    Add a testcase for analysis units in the properties DSL
    Change-Id: Id90f7189c6aab5d14df18a1c3a89f4c298a8d284
    TN: PC20-010

---
+++ testsuite/tests/properties/analysis_unit/main.py
@@ -0,0 +1,21 @@
+print 'main.py: Running...'
+
+
+import sys
+
+import libfoolang
+
+
+ctx = libfoolang.AnalysisContext()
+for f in ('main.txt', ):
+    print '=== {} ==='.format(f)
+    u = ctx.get_from_file(f)
+    if u.diagnostics:
+        for d in u.diagnostics:
+            print(d)
+        sys.exit(1)
+
+    u.populate_lexical_env()
+    print 'result = {}'.format(u.root.p_result)
+
+print 'main.py: Done.'
---
+++ testsuite/tests/properties/analysis_unit/test.py
@@ -0,0 +1,64 @@
+"""
+Test the handling of analysis units in the properties DSL.
+"""
+
+import os.path
+
+from langkit.compiled_types import (
+    AnalysisUnitType, ASTNode, Field, LongType, abstract, root_grammar_class
+)
+from langkit.diagnostics import Diagnostics
+from langkit.expressions import (
+    AbstractProperty, ExternalProperty, Property, Self
+)
+from langkit.parsers import Grammar, Or, Row, Tok
+
+from lexer_example import Token
+from utils import build_and_run
+
+
+Diagnostics.set_lang_source_dir(os.path.abspath(__file__))
+
+
+@root_grammar_class()
+class FooNode(ASTNode):
+    pass
+
+
+@abstract
+class Expression(FooNode):
+    result = AbstractProperty(type=LongType)
+
+
+class Literal(Expression):
+    tok = Field()
+
+    result = ExternalProperty()
+
+
+class Name(Expression):
+    tok = Field()
+
+    designated_unit = ExternalProperty(type=AnalysisUnitType)
+    result = Property(Self.designated_unit.root.cast(Expression).result)
+
+class Plus(Expression):
+    left = Field()
+    right = Field()
+
+    result = Property(Self.left.result + Self.right.result)
+
+
+foo_grammar = Grammar('main_rule')
+foo_grammar.add_rules(
+    main_rule=Or(
+        Row(foo_grammar.atom, '+', foo_grammar.main_rule) ^ Plus,
+        foo_grammar.atom
+    ),
+    atom=Or(
+        Row(Tok(Token.Number, keep=True)) ^ Literal,
+        Row(Tok(Token.Identifier, keep=True)) ^ Name,
+    ),
+)
+build_and_run(foo_grammar, 'main.py')
+print 'Done'
