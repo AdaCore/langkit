871268b02b58af33904f2f5fc3a975e65394d254
Raphael Amiard <amiard@adacore.com>
2017-01-04 14:55:08 +0100

    PC19-022: Add test for access to env element fields

---
+++ testsuite/tests/properties/env_element_field_access/test.py
@@ -0,0 +1,45 @@
+"""
+Test that garbage tokens left after the main parsing rule completes does not
+crash. It used to!
+"""
+
+import os.path
+
+from langkit.compiled_types import ASTNode, Field, root_grammar_class, LongType
+from langkit.diagnostics import Diagnostics
+from langkit.expressions import AbstractProperty, Property, Self
+from langkit.parsers import Grammar, Row, Tok
+
+from lexer_example import Token
+from utils import build_and_run
+
+
+Diagnostics.set_lang_source_dir(os.path.abspath(__file__))
+
+
+@root_grammar_class()
+class FooNode(ASTNode):
+    prop = AbstractProperty(runtime_check=True, type=LongType)
+
+
+class BarNode(FooNode):
+    pass
+
+
+class Literal(FooNode):
+    tok = Field()
+
+    a = AbstractProperty(runtime_check=True, type=FooNode.env_element())
+
+    b = Property(Self.a.match(
+        lambda b=BarNode.env_element(): b.prop,
+        lambda c=FooNode.env_element(): c.prop,
+    ))
+
+
+foo_grammar = Grammar('main_rule')
+foo_grammar.add_rules(
+    main_rule=Row(Tok(Token.Number, keep=True)) ^ Literal,
+)
+build_and_run(foo_grammar, 'main.py')
+print 'Done'
