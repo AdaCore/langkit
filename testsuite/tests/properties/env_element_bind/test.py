9397767573615c68d93e06a22998836c72401831
Raphael Amiard <amiard@adacore.com>
2017-01-07 13:40:29 +0100

    PC19-022: Add test for Bind with env_element
    Change-Id: If5d177bc2482831c99740d053a476bd57a53ff76

---
+++ testsuite/tests/properties/env_element_bind/test.py
@@ -0,0 +1,44 @@
+"""
+Test that Bind works when binding from env elements.
+"""
+
+import os.path
+
+from langkit.compiled_types import (
+    ASTNode, Field, root_grammar_class, LongType, UserField, LogicVarType
+)
+from langkit.diagnostics import Diagnostics
+from langkit.expressions import AbstractProperty, Property, Self, Bind
+from langkit.parsers import Grammar, Row, Tok
+
+from lexer_example import Token
+from utils import build_and_run
+
+
+Diagnostics.set_lang_source_dir(os.path.abspath(__file__))
+
+
+@root_grammar_class()
+class FooNode(ASTNode):
+    prop = AbstractProperty(runtime_check=True, type=LongType)
+
+
+class BarNode(FooNode):
+    pass
+
+
+class Literal(FooNode):
+    tok = Field()
+
+    a = AbstractProperty(runtime_check=True, type=FooNode.env_element())
+    var = UserField(LogicVarType, is_private=True)
+
+    b = Property(Bind(Self.var, Self.a), private=True)
+
+
+foo_grammar = Grammar('main_rule')
+foo_grammar.add_rules(
+    main_rule=Row(Tok(Token.Number, keep=True)) ^ Literal,
+)
+build_and_run(foo_grammar, 'main.py')
+print 'Done'
