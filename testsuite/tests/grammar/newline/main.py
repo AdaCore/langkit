e65f8580d355582f5b006a5a727876ef95c992d4
Raphael Amiard <amiard@adacore.com>
2017-01-15 19:16:30 +0100

    Q116-024: Add test for valid layout parsers

---
+++ testsuite/tests/grammar/newline/main.py
@@ -0,0 +1,31 @@
+import libfoolang
+
+ctx = libfoolang.AnalysisContext()
+
+
+def process(text):
+    u = ctx.get_from_buffer('main.txt', text)
+    if u.diagnostics:
+        print "Found errors:"
+        for d in u.diagnostics:
+            print "", d
+    else:
+        u.root.dump()
+
+
+process("""
+1
+2
+""")
+process("1 2")
+process("""
+1
+   2
+""")
+
+process("""
+1
+2
+   3
+4
+""")
