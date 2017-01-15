c5b98648e0f8040b2347ad2665035b7507cd11d5
Raphael Amiard <amiard@adacore.com>
2017-01-15 12:30:12 +0100

    Q116-024: update list test to check invalid list_cls

--- testsuite/tests/grammar/invalid_list/test.py
+++ testsuite/tests/grammar/invalid_list/test.py
@@ -5,7 +5,7 @@ from langkit.diagnostics import Diagnostics
 from langkit.parsers import Grammar, Row, List, Tok
 
 from lexer_example import Token
-from utils import emit_and_print_errors
+from utils import emit_and_print_errors, reset_langkit
 
 
 Diagnostics.set_lang_source_dir(os.path.abspath(__file__))
@@ -20,11 +20,28 @@ class ListNode(FooNode):
     items = Field()
 
 
+class Num(FooNode):
+    field = Field()
+
+
 def lang_def():
     foo_grammar = Grammar('main_rule')
     foo_grammar.add_rules(
         main_rule=Row(List(Tok(Token.Number, keep=True))) ^ ListNode,
     )
     return foo_grammar
+
+
+def lang_def_2():
+    foo_grammar = Grammar('main_rule')
+    foo_grammar.add_rules(
+        num=Num(Tok(Token.Number, keep=True)),
+        main_rule=List(foo_grammar.num, list_cls=ListNode)
+    )
+    return foo_grammar
+
+
 emit_and_print_errors(lang_def)
+reset_langkit()
+emit_and_print_errors(lang_def_2)
 print('Done')
