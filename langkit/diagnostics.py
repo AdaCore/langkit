332df9caeececfdf8c6d0702e19f2a8214c2e6d3
Raphael Amiard <amiard@adacore.com>
2017-01-25 10:45:41 +0100

    Minor: fix style

--- langkit/diagnostics.py
+++ langkit/diagnostics.py
@@ -181,13 +181,14 @@ def print_context(recovered=False):
         if EMIT_PARSABLE_ERRORS:
             print "{}:{}: {}".format(ctx_loc.file, ctx_loc.line, ctx_msg)
         else:
-            # We only want to show the file information one time if it is the same
+            # We only want to show the file information one time if it is the
+            # same.
             file_info = 'File "{}", '.format(col(ctx_loc.file, Colors.CYAN))
             if last_file_info == file_info:
                 file_info = '  '
             else:
                 last_file_info = file_info
-                
+
             print ('{file_info}line {line}, {msg}'.format(
                 file_info=file_info,
                 line=col(ctx_loc.line, Colors.CYAN),
