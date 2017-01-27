b8596e7d320062a104cd6bc54ff466cab3f56b27
Pierre-Marie de Rodat <derodat@adacore.com>
2017-01-27 16:24:54 +0100

    Add a testcase for non-overriding homonym fields/properties rejection
    TN: Q127-021

---
+++ testsuite/tests/structs/field_override/test.py
@@ -0,0 +1,41 @@
+"""
+Test the handling of analysis units in the properties DSL.
+"""
+
+import os.path
+
+from langkit.compiled_types import (
+    ASTNode, Field, T, abstract, root_grammar_class
+)
+from langkit.diagnostics import DiagnosticError, Diagnostics
+from langkit.expressions import Property, Self
+
+
+Diagnostics.set_lang_source_dir(os.path.abspath(__file__))
+
+
+def run():
+    @root_grammar_class()
+    class FooNode(ASTNode):
+        pass
+
+    @abstract
+    class RootNode(FooNode):
+        name = Property(Self.match(
+            lambda e=T.Expr: e.name,
+            lambda n=T.Name: n
+        ))
+
+    class Expr(RootNode):
+        name = Field()
+
+    class Name(RootNode):
+        tok = Field()
+
+
+try:
+    run()
+except DiagnosticError:
+    pass
+print ''
+print 'Done'
