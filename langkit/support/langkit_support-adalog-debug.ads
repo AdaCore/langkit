6b3dfb1d80d5cb4f3128cb1e5bd6c64936b7d7dd
Raphael Amiard <amiard@adacore.com>
2017-02-10 11:58:02 +0100

    O413-018: Document Langkit_Support.Debug

--- langkit/support/langkit_support-adalog-debug.ads
+++ langkit/support/langkit_support-adalog-debug.ads
@@ -1,9 +1,18 @@
+--  This package Contains the debug configuration for Adalog. There are two
+--  main ways of configuring the debug mode in Adalog:
+--
+--  - The Debug_Enabled constant will determine at compile time whether
+--    debugging is enabled or not. If it is False, no traces will be output,
+--    and the Step mode won't be usable.
+--  - If you set the Debug_Enabled constant to True, you still need to activate
+--    a debug mode at runtime via Set_Debug_State.
+
 package Langkit_Support.Adalog.Debug is
    type Debug_State_Type is (None, Trace, Step);
    --  If Trace, will trace the execution. If step, will trace and stop at
    --  every step of the solve so that you can trace the solve operation
    --  step-by-step.
-   --  WARNING!!! Trace and step mode are not thread safe. It would not make
+   --  WARNING: Trace and step mode are not thread safe. It would not make
    --  any sense to try to use them with solving happning in several threads at
    --  the same time.
 
@@ -13,8 +22,12 @@ package Langkit_Support.Adalog.Debug is
    with Inline;
 
    procedure Set_Debug_State (Val : Debug_State_Type);
+   --  Set the debug state
+
    function Debug_State return Debug_State_Type;
+   --  Get the debug state
 
    procedure Trace (Str : String);
-   --  Will output a string to stdout only if Debug is True
+   --  Will output a string to stdout only if Debug is in Trace | Step
+
 end Langkit_Support.Adalog.Debug;
