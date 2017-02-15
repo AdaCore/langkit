54167496daa39fc13282e329b89b5e436c74f962
Pierre-Marie de Rodat <derodat@adacore.com>
2017-02-15 15:35:44 +0100

    Python API: add a test for Is_Ghost binding
    TN: Q215-025

---
+++ testsuite/tests/python-api/ghost_nodes/main.py
@@ -0,0 +1,13 @@
+import libfoolang
+
+
+ctx = libfoolang.AnalysisContext()
+unit = ctx.get_from_file('foo.txt')
+for node in unit.root.findall(lambda _: True):
+    print('{} node: {}'.format(
+        'Ghost' if node.is_ghost else 'Regular',
+        node
+    ))
+    assert not node.text or not node.is_ghost
+
+print 'Done.'
---
+++ testsuite/tests/python-api/ghost_nodes/test.py
@@ -0,0 +1,47 @@
+"""
+Test the handling of analysis units in the properties DSL.
+"""
+
+import os.path
+
+from langkit.compiled_types import ASTNode, Field, T, root_grammar_class
+from langkit.diagnostics import Diagnostics
+from langkit.parsers import Grammar, List, Or, Tok
+
+from lexer_example import Token
+from utils import build_and_run
+
+
+Diagnostics.set_lang_source_dir(os.path.abspath(__file__))
+
+
+@root_grammar_class()
+class FooNode(ASTNode):
+    pass
+
+
+class Enum(T.EnumNode):
+    alternatives = ['null', 'example', 'default']
+
+
+class Param(FooNode):
+    name = Field(type=T.Name)
+    mode = Field(type=T.Enum)
+
+
+class Name (FooNode):
+    tok = Field(type=T.Token)
+
+
+foo_grammar = Grammar('main_rule')
+foo_grammar.add_rules(
+    main_rule=List(Param(foo_grammar.name, foo_grammar.mode)),
+    name=Name(Tok(Token.Identifier, keep=True)),
+    mode=Or(
+        Enum.alt_null('null'),
+        Enum.alt_example('example'),
+        Enum.alt_default(),
+    ),
+)
+build_and_run(foo_grammar, 'main.py')
+print 'Done'
