88f2cf558f02e50d7a018a11148d1d7ad9fd2e53
Pierre-Marie de Rodat <derodat@adacore.com>
2017-02-16 15:48:56 +0100

    Add a testcase for null safety checks
    TN: Q215-051

---
+++ testsuite/tests/properties/null_checks/main.py
@@ -0,0 +1,37 @@
+print 'main.py: Running...'
+
+
+import sys
+
+import libfoolang
+
+
+ctx = libfoolang.AnalysisContext()
+u = ctx.get_from_file('foo.txt')
+if u.diagnostics:
+    for d in u.diagnostics:
+        print(d)
+    sys.exit(1)
+
+u.populate_lexical_env()
+expr = u.root
+iden = expr.f_left
+for node, prop in [
+    (expr, 'p_null_unit'),
+    (expr, 'p_null_node'),
+
+    (expr, 'p_deref_null_unit'),
+    (expr, 'p_deref_null_node'),
+    (expr, 'p_null_node_unit'),
+
+    (expr, 'p_cast_null_node'),
+
+    (iden, 'p_deref_env_element'),
+]:
+    try:
+        result = getattr(node, prop)
+    except libfoolang.PropertyError:
+        result = 'PropertyError'
+    print('{}.{}: {}'.format(node, prop, result))
+
+print 'main.py: Done.'
---
+++ testsuite/tests/properties/null_checks/test.py
@@ -0,0 +1,70 @@
+"""
+Test that property checks are properly emitted when null checks should trigger
+them.
+"""
+
+import os.path
+
+from langkit.compiled_types import (
+    AnalysisUnitType, ASTNode, Field, T, Token as TokenType, abstract,
+    root_grammar_class
+)
+from langkit.diagnostics import Diagnostics
+from langkit.expressions import No, Property, Self
+from langkit.parsers import Grammar, Or, Row, Tok
+
+from lexer_example import Token
+from utils import build_and_run
+
+
+Diagnostics.set_lang_source_dir(os.path.abspath(__file__))
+
+
+@root_grammar_class()
+class FooNode(ASTNode):
+    null_unit = Property(No(AnalysisUnitType), public=True)
+    null_node = Property(No(T.Expression), public=True)
+
+    deref_null_unit = Property(Self.null_unit.root, public=True)
+    deref_null_node = Property(Self.null_node.null_node, public=True)
+    null_node_unit = Property(Self.null_node.unit, public=True)
+
+    cast_null_node = Property(Self.null_node.cast(T.Name), public=True)
+
+
+@abstract
+class Expression(FooNode):
+    pass
+
+
+class Literal(Expression):
+    tok = Field(type=TokenType)
+
+
+class Name(Expression):
+    tok = Field(type=TokenType)
+
+    env_element = Property(Self.children_env.get(Self.tok.symbol).at(0))
+    deref_env_element = Property(Self.env_element.null_node, public=True)
+
+
+class Plus(Expression):
+    left = Field()
+    right = Field()
+
+
+foo_grammar = Grammar('main_rule')
+foo_grammar.add_rules(
+    main_rule=foo_grammar.expression,
+    expression=Or(
+        Row('(', foo_grammar.expression, ')')[1],
+        Plus(foo_grammar.atom, '+', foo_grammar.main_rule),
+        foo_grammar.atom,
+    ),
+    atom=Or(
+        Row(Tok(Token.Number, keep=True)) ^ Literal,
+        Row(Tok(Token.Identifier, keep=True)) ^ Name,
+    ),
+)
+build_and_run(foo_grammar, 'main.py')
+print 'Done'
