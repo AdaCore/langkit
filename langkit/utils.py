832c45031677b43de1138e9883301b44228c471b
Pierre-Marie de Rodat <derodat@adacore.com>
2017-02-20 17:20:19 +0100

    Fix a typo
    TN: minor

--- langkit/utils.py
+++ langkit/utils.py
@@ -139,7 +139,7 @@ def printcol(msg, color):
 
     # Colored messages are used to show the user how the compilation process is
     # going, so flushing on a regular basis matters. When \n-based flushing is
-    # disable (because of a pipe, for instance), force flushing here.
+    # disabled (because of a pipe, for instance), force flushing here.
     sys.stdout.flush()
 
 
