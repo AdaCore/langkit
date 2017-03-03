9340f55bd04b8ca82c6361eec39480af05cbf703
Pierre-Marie de Rodat <derodat@adacore.com>
2017-03-03 12:20:37 +0100

    Update EquationType Python API binding testcase...
    ... so that it tests as much as the testcase corresponding to
    LogicVarType.
    
    TN: Q221-031

--- testsuite/tests/python-api/equation_type/main.py
+++ testsuite/tests/python-api/equation_type/main.py
@@ -17,5 +17,10 @@ if u.diagnostics:
     sys.exit(1)
 
 u.populate_lexical_env()
+res_none = u.root.p_eq(None)
+res_field = u.root.p_eq(u.root.f_eq_field)
 print('u.root.p_eq(None) = {}'.format(u.root.p_eq(None)))
+print('u.root.p_eq(u.root.f_eq_field): result has type {}'.format(
+    type(res_field)
+))
 print('main.py: Done.')
