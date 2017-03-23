a6c895cea85dcadaae29f61646d4f5d51feffead
Pierre-Marie de Rodat <derodat@adacore.com>
2017-03-23 13:05:51 +0100

    tests/python-api/equation_type: update to test type safety
    TN: Q221-031

--- testsuite/tests/python-api/equation_type/main.py
+++ testsuite/tests/python-api/equation_type/main.py
@@ -17,10 +17,19 @@ if u.diagnostics:
     sys.exit(1)
 
 u.populate_lexical_env()
+
 res_none = u.root.p_eq(None)
-res_field = u.root.p_eq(u.root.f_eq_field)
 print('u.root.p_eq(None) = {}'.format(u.root.p_eq(None)))
+
+try:
+    res_int = u.root.p_eq(42)
+except TypeError as exc:
+    res_int = '<TypeError: {}>'.format(exc)
+print('u.root.p_eq(42) = {}'.format(res_int))
+
+res_field = u.root.p_eq(u.root.f_eq_field)
 print('u.root.p_eq(u.root.f_eq_field): result has type {}'.format(
     type(res_field)
 ))
+
 print('main.py: Done.')
