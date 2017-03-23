5f78e55cc7895e9f85de194cde8b57bef4e6f070
Pierre-Marie de Rodat <derodat@adacore.com>
2017-03-23 13:10:45 +0100

    tests/python-api/env_rebindings_type: update to test type safety
    TN: Q221-031

--- testsuite/tests/python-api/env_rebindings_type/main.py
+++ testsuite/tests/python-api/env_rebindings_type/main.py
@@ -17,5 +17,13 @@ if u.diagnostics:
     sys.exit(1)
 
 u.populate_lexical_env()
+
 print('u.root.p_er(None) = {}'.format(u.root.p_er(None)))
+
+try:
+    res_int = u.root.p_er(42)
+except TypeError as exc:
+    res_int = '<TypeError: {}>'.format(exc)
+print('u.root.p_er(42) = {}'.format(res_int))
+
 print('main.py: Done.')
