c1ca4b3dbb385012ce034c3f94946671263f90be
Pierre-Marie de Rodat <derodat@adacore.com>
2017-03-23 13:06:11 +0100

    tests/python-api/logic_var_type: update to test type safety
    TN: Q221-031

--- testsuite/tests/python-api/logic_var_type/main.py
+++ testsuite/tests/python-api/logic_var_type/main.py
@@ -19,9 +19,17 @@ if u.diagnostics:
 u.populate_lexical_env()
 
 res_none = u.root.p_var(None)
-res_field = u.root.p_var(u.root.f_var_field)
 print('u.root.p_var(None) = {}'.format(res_none))
+
+try:
+    res_int = u.root.p_var(42)
+except TypeError as exc:
+    res_int = '<TypeError: {}>'.format(exc)
+print('u.root.p_var(42) = {}'.format(res_int))
+
+res_field = u.root.p_var(u.root.f_var_field)
 print('u.root.p_var(root.f_var_field): result has type {}'.format(
     type(res_field)
 ))
+
 print('main.py: Done.')
