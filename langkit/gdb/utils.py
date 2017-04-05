efdde298ef090957f33b43298a5179cc979183ce
Pierre-Marie de Rodat <derodat@adacore.com>
2017-04-05 21:01:11 +0200

    gdb.utils: create tagged_field accessor and refactor record_to_tag
    TN: Q405-006

--- langkit/gdb/utils.py
+++ langkit/gdb/utils.py
@@ -14,17 +14,28 @@ def ptr_to_int(ptr_value):
     return int(ptr_value.cast(system_address))
 
 
+def tagged_field(record_value, field_name):
+    """
+    Helper to look for a field in a tagged record.
+
+    This is useful because we see tagged records in GDB as a record that
+    contains another record (for fields from the parent type) that contains
+    another record, etc.
+    """
+    while True:
+        try:
+            return record_value[field_name]
+        except gdb.error:
+            pass
+        try:
+            record_value = record_value['_parent']
+        except gdb.error:
+            raise gdb.error('There is no member {}'.format(field_name))
+
+
 def record_to_tag(record_value):
     """
     Assuming "record_value" is a tagged record value, return the corresponding
     tag as a Python integer.
     """
-    # First, get the ultimate parent record (i.e. record for base type)
-    while True:
-        try:
-            record_value = record_value['_parent']
-        except gdb.error:
-            break
-
-    # Now, the tag must be there
-    return ptr_to_int(record_value['_tag'])
+    return ptr_to_int(tagged_field(record_value, '_tag'))
