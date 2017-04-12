df2692b6e5c04a2e552f8de1873471aab8d6cc03
Pierre-Marie de Rodat <derodat@adacore.com>
2017-04-12 17:45:14 +0200

    Add a testcase for entity resolvers
    TN: Q412-012

---
+++ testsuite/tests/lexical_envs/entity_resolver/test.py
@@ -0,0 +1,71 @@
+"""
+Test the handling of analysis units in the properties DSL.
+"""
+
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import os.path
+
+from langkit.compiled_types import ASTNode, Field, T, root_grammar_class
+from langkit.diagnostics import Diagnostics
+from langkit.envs import EnvSpec, add_to_env
+from langkit.expressions import New, No, Self, langkit_property
+from langkit.parsers import Grammar, List, Tok
+
+from lexer_example import Token
+from utils import build_and_run
+
+
+Diagnostics.set_lang_source_dir(os.path.abspath(__file__))
+
+
+@root_grammar_class()
+class FooNode(ASTNode):
+
+    @langkit_property()
+    def resolve_ref():
+        return Self.match(
+            lambda r=T.Ref: r.parent.parent.node_env.get(r.name.symbol).at(0),
+            lambda _: No(T.entity),
+        )
+
+
+class Decl(FooNode):
+    name = Field()
+    refs = Field()
+
+    env_spec = EnvSpec(
+        add_env=True,
+        add_to_env=add_to_env(
+            New(T.env_assoc, key=Self.name.symbol, val=Self)
+        )
+    )
+
+
+class Ref(FooNode):
+    name = Field()
+
+    env_spec = EnvSpec(add_to_env=add_to_env(
+        New(T.env_assoc, key=Self.name.symbol, val=Self),
+        resolver=FooNode.fields.resolve_ref
+    ))
+
+    @langkit_property(public=True)
+    def resolve():
+        return Self.node_env.get(Self.name.symbol).at(0)
+
+
+foo_grammar = Grammar('main_rule')
+foo_grammar.add_rules(
+    main_rule=List(foo_grammar.decl),
+    decl=Decl(
+        Tok(Token.Identifier, keep=True),
+        Tok(Token.LPar),
+        List(foo_grammar.ref, empty_valid=True),
+        Tok(Token.RPar)
+    ),
+    ref=Ref(Tok(Token.Identifier, keep=True)),
+)
+build_and_run(foo_grammar, 'main.py')
+print('Done')
