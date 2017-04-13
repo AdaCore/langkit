633a45d7512a2ddd79f1692fb4b6f70716760d02
Pierre-Marie de Rodat <derodat@adacore.com>
2017-04-13 11:15:43 +0200

    Add a testcase for type unification for If result sub-expressions
    TN: Q413-003

---
+++ testsuite/tests/properties/if_unify_astnode/test.py
@@ -0,0 +1,50 @@
+"""
+Check that the unification of the return type of match expression is correct.
+"""
+
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import os.path
+
+from langkit.compiled_types import ASTNode, T, root_grammar_class
+from langkit.diagnostics import Diagnostics
+from langkit.expressions import If, No, Property, Self
+from langkit.parsers import Grammar, Or, Tok
+
+from lexer_example import Token
+from utils import emit_and_print_errors
+
+
+Diagnostics.set_lang_source_dir(os.path.abspath(__file__))
+
+
+@root_grammar_class()
+class FooNode(ASTNode):
+    pred = Property(True)
+
+    foo_1 = Property(
+        If(Self.pred, No(T.BarNode), No(T.Literal)),
+        public=True
+    )
+
+
+class BarNode(FooNode):
+    pass
+
+
+class Literal(FooNode):
+    pass
+
+
+def lang_def():
+    foo_grammar = Grammar('main_rule')
+    foo_grammar.add_rules(
+        main_rule=Or(
+            BarNode(Tok(Token.Example)),
+            Literal(Tok(Token.Number)),
+        )
+    )
+    return foo_grammar
+emit_and_print_errors(lang_def)
+print('Done')
