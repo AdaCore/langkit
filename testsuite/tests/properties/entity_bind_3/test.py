c9b8d0d1669480720d22fb388e9446fe879d287b
Raphael Amiard <amiard@adacore.com>
2017-06-09 10:58:38 +0200

    PC19-022: new test for Bind
    Change-Id: I4164670aa6c1d27ca6a5e093bf75e93018b0e854

---
+++ testsuite/tests/properties/entity_bind_3/test.py
@@ -0,0 +1,52 @@
+"""
+Test that Bind works when binding entities, and using an equality property that
+takes entities.
+"""
+
+from __future__ import absolute_import, division, print_function
+
+import os.path
+
+from langkit.compiled_types import (
+    ASTNode, Field, root_grammar_class, LongType, UserField, LogicVarType,
+    T
+)
+from langkit.diagnostics import Diagnostics
+from langkit.expressions import (
+    AbstractProperty, Let, Property, Self, Bind, langkit_property
+)
+from langkit.parsers import Grammar, Row, Tok
+
+from lexer_example import Token
+from utils import build_and_run
+
+
+Diagnostics.set_lang_source_dir(os.path.abspath(__file__))
+
+
+@root_grammar_class()
+class FooNode(ASTNode):
+    prop = AbstractProperty(runtime_check=True, type=LongType, public=True)
+
+
+class Literal(FooNode):
+    tok = Field()
+
+    a = AbstractProperty(runtime_check=True, type=FooNode.entity())
+    var = UserField(LogicVarType, public=False)
+
+    @langkit_property(return_type=T.Literal.entity())
+    def node():
+        return Self.as_entity
+
+    b = Property(Bind(Self.var, Self.a, Self.node))
+
+    public_prop = Property(Let(lambda _=Self.b: Self), public=True)
+
+
+foo_grammar = Grammar('main_rule')
+foo_grammar.add_rules(
+    main_rule=Row(Tok(Token.Number, keep=True)) ^ Literal,
+)
+build_and_run(foo_grammar, 'main.py')
+print('Done')
