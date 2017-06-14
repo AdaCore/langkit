427fcc418ee8701f6bb93cbf9a3ee2598e1b209e
Raphael Amiard <amiard@adacore.com>
2017-06-14 12:17:59 +0200

    OA28-063: Add topological sort function
    Change-Id: Icefe1eae1b43e1e993c43b64c6ed5179510e6664

--- langkit/utils.py
+++ langkit/utils.py
@@ -508,3 +508,31 @@ class Log():
             with Log.nest():
                 return fn(*args, **kwargs)
         return internal
+
+
+def topological_sort(items):
+    """
+    'items' is an iterable of (item, dependencies) pairs, where 'dependencies'
+    is an iterable of the same type as 'items'.
+
+    If 'items' is a generator rather than a data structure, it should not be
+    empty. Passing an empty generator for 'items' (zero yields before return)
+    will cause topological_sort() to raise TopologicalSortFailure.
+
+    An empty iterable (e.g. list, tuple, set, ...) produces no items but
+    raises no exception.
+    """
+    provided = set()
+    while items:
+        remaining_items = []
+        emitted = False
+        for item, dependencies in items:
+            dependencies = set(dependencies)
+            if provided.issuperset(dependencies):
+                yield item
+                provided.add(item)
+                emitted = True
+            else:
+                remaining_items.append((item, dependencies))
+        assert(emitted)
+        items = remaining_items
