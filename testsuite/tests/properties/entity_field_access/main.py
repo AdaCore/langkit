27a152412ba0f7cfb54c2f1b1d9787b8c5b10a77
Pierre-Marie de Rodat <derodat@adacore.com>
2017-06-22 16:41:26 +0200

    properties/entity_field_access: update to test entity info forwarding
    TN: Q619-018

--- testsuite/tests/properties/entity_field_access/main.py
+++ testsuite/tests/properties/entity_field_access/main.py
@@ -1,1 +1,22 @@
 from __future__ import absolute_import, division, print_function
+
+print('main.py: Running...')
+
+
+import sys
+
+import libfoolang
+
+
+ctx = libfoolang.AnalysisContext()
+u = ctx.get_from_buffer('main.txt', '((1, 2), 3)')
+if u.diagnostics:
+    for d in u.diagnostics:
+        print(d)
+    sys.exit(1)
+
+u.populate_lexical_env()
+
+print('.test_main:', u.root.p_test_main)
+print('.property_on_entity:', u.root.p_property_on_entity)
+print('main.py: Done.')
--- testsuite/tests/properties/entity_field_access/test.py
+++ testsuite/tests/properties/entity_field_access/test.py
@@ -1,5 +1,11 @@
 """
-Tests that it is possible to call a property of type T on an Entity[T].
+Test that:
+
+ 1. It is possible to call an AST node property on the corresponding entity.
+
+ 2. When accessing an AST node field through the corresponding entity, this
+    returns the component AST node wrapped in an entity with the same entity
+    info.
 """
 
 from __future__ import absolute_import, division, print_function
@@ -7,9 +13,10 @@ from __future__ import absolute_import, division, print_function
 import os.path
 
 from langkit.diagnostics import Diagnostics
-from langkit.dsl import ASTNode, Field, LongType, abstract
-from langkit.expressions import AbstractProperty, Property, Self
-from langkit.parsers import Grammar, Row, Tok
+from langkit.dsl import (ASTNode, BoolType, EnvRebindingsType, Field, Struct,
+                         T, UserField, env_metadata)
+from langkit.expressions import New, No, Self, langkit_property
+from langkit.parsers import Grammar, Or, Tok
 
 from lexer_example import Token
 from utils import build_and_run
@@ -19,31 +26,61 @@ Diagnostics.set_lang_source_dir(os.path.abspath(__file__))
 
 
 class FooNode(ASTNode):
-    prop = AbstractProperty(runtime_check=True, type=LongType)
+
+    @langkit_property(public=True)
+    def test_main():
+        return (
+            Self.cast(T.Couple)
+
+            # This will return an entity with is_first=True
+            .first_entity
+
+            # This is supposed to forward the entity metadata from the previous
+            # line.
+            .first
+        )
 
 
-@abstract
-class BarNode(FooNode):
-    pass
+@env_metadata
+class Metadata(Struct):
+    is_first = UserField(BoolType)
+    is_second = UserField(BoolType)
 
 
 class Literal(FooNode):
     tok = Field()
 
-    a = AbstractProperty(runtime_check=True, type=FooNode.entity())
 
-    b = Property(
-        Self.a.match(
-            lambda b=BarNode.entity(): b.prop,
-            lambda c=FooNode.entity(): c.prop,
-        ),
-        public=True
-    )
+class Couple(FooNode):
+    first = Field()
+    second = Field()
+
+    @langkit_property()
+    def first_entity():
+        return Self.as_entity.get_entity(
+            New(Metadata, is_first=True, is_second=False)
+        )
+
+    @langkit_property()
+    def get_entity(md=Metadata):
+        return New(Couple.entity(),
+                   el=Self,
+                   info=New(T.entity_info,
+                            md=md,
+                            rebindings=No(EnvRebindingsType)))
+
+    @langkit_property(public=True)
+    def property_on_entity():
+        # Check that we can do ".get_entity" even when the prefix is an entity
+        return Self.as_entity.get_entity(Self.as_entity.info.md)
 
 
 foo_grammar = Grammar('main_rule')
 foo_grammar.add_rules(
-    main_rule=Row(Tok(Token.Number, keep=True)) ^ Literal,
+    main_rule=foo_grammar.item,
+    item=Or(foo_grammar.couple, foo_grammar.literal),
+    couple=Couple('(', foo_grammar.item, ',', foo_grammar.item, ')'),
+    literal=Literal(Tok(Token.Number, keep=True)),
 )
 build_and_run(foo_grammar, 'main.py')
 print('Done')
