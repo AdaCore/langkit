7f4a31fbe01f2efc7f7ac2a0e59d4ae1691c81e3
Pierre-Marie de Rodat <derodat@adacore.com>
2017-07-07 12:11:31 +0200

    Add a testcase for KeyError in TypeRepo.Defer resolution
    TN: minor

---
+++ testsuite/tests/misc/typerepo_defer_attr_fail/test.py
@@ -0,0 +1,31 @@
+from __future__ import absolute_import, division, print_function
+
+import os.path
+
+from langkit.diagnostics import Diagnostics
+from langkit.dsl import ASTNode, Field, T
+from langkit.parsers import Grammar, Tok
+
+from lexer_example import Token
+from utils import emit_and_print_errors
+
+
+Diagnostics.set_lang_source_dir(os.path.abspath(__file__))
+
+
+class FooNode(ASTNode):
+    pass
+
+
+class Example(FooNode):
+    tok = Field(type=T.FooNode.does_not_exist)
+
+
+def get_fg():
+    fg = Grammar('main_rule')
+    fg.add_rules(
+        main_rule=Example(Tok(Token.Example, keep=True)),
+    )
+    return fg
+emit_and_print_errors(get_fg)
+print('Done')
