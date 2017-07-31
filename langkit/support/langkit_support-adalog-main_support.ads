23b8478f37ec8ff4c1e4f6aafabe57983068867b
Pierre-Marie de Rodat <derodat@adacore.com>
2017-07-31 11:03:58 +0200

    Adalog/Main_Support: minor reformatting
    TN: Q706-012

--- langkit/support/langkit_support-adalog-main_support.ads
+++ langkit/support/langkit_support-adalog-main_support.ads
@@ -1,3 +1,5 @@
+--  Provide common support material for unit tests
+
 with Langkit_Support.Adalog.Eq_Same;
 with Langkit_Support.Adalog.Predicates;
 use Langkit_Support.Adalog.Predicates;
@@ -6,7 +8,6 @@ package Langkit_Support.Adalog.Main_Support is
 
    function Element_Image (I : Integer) return String is (I'Image);
    package Eq_Int is new Eq_Same (Integer);
+   package Pred_Int is new Dyn_Predicate (Integer, Eq_Int.Refs.Raw_Logic_Var);
 
-   package Pred_Int is
-     new Dyn_Predicate (Integer, Eq_Int.Refs.Raw_Logic_Var);
 end Langkit_Support.Adalog.Main_Support;
