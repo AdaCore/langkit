db8113f7c45dbb482b2ed91174145622505dac27
Pierre-Marie de Rodat <derodat@adacore.com>
2017-08-09 15:20:29 +0200

    Adalog: fix testsuite material support after the Dec_Ref recent fix
    TN: Q301-017

--- langkit/support/langkit_support-adalog-main_support.adb
+++ langkit/support/langkit_support-adalog-main_support.adb
@@ -5,18 +5,18 @@ package body Langkit_Support.Adalog.Main_Support is
    ------------------------
 
    procedure Free_Relation_Tree (R : in out Relation) is
+      Var : Relation;
    begin
       for C of R.Children loop
-         declare
-            C_Var : Relation := C;
-         begin
-            Free_Relation_Tree (C_Var);
-            while C_Var.Ref_Count > 1 loop
-               Dec_Ref (C_Var);
-            end loop;
-         end;
+         Var := C;
+         Free_Relation_Tree (Var);
+         while Var /= null and then Var.Ref_Count > 1 loop
+            Dec_Ref (Var);
+         end loop;
       end loop;
-      Dec_Ref (R);
+      Var := R;
+      Dec_Ref (Var);
+      R := null;
    end Free_Relation_Tree;
 
 end Langkit_Support.Adalog.Main_Support;
