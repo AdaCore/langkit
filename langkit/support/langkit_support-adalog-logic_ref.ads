e33a12d1431e85fde535ae7cd0994b45af2ae1ee
Pierre-Marie de Rodat <derodat@adacore.com>
2017-08-10 15:15:58 +0200

    Adalog/Logic_Ref: add Is_Defined check as Get_Value precondition
    TN: Q810-016

--- langkit/support/langkit_support-adalog-logic_ref.ads
+++ langkit/support/langkit_support-adalog-logic_ref.ads
@@ -55,7 +55,8 @@ package Langkit_Support.Adalog.Logic_Ref is
    procedure Reset (Self : in out Var);
    function Is_Defined (Self : Var) return Boolean;
    procedure Set_Value (Self : in out Var; Data : Element_Type);
-   function Get_Value (Self : Var) return Element_Type;
+   function Get_Value (Self : Var) return Element_Type
+      with Pre => Is_Defined (Self);
    procedure Destroy (Self : in out Var);
    function Image (Self : Var) return String is
      (if Self.Dbg_Name /= null then Self.Dbg_Name.all else "None");
