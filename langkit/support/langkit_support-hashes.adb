a88df45a4494b6635937acc7d2aa756cace04dba
Pierre-Marie de Rodat <derodat@adacore.com>
2017-10-18 15:39:08 -0400

    Langkit_Support.Hashes: copy hash combine function from Boost
    TN: QA16-034

--- langkit/support/langkit_support-hashes.adb
+++ langkit/support/langkit_support-hashes.adb
@@ -1,3 +1,5 @@
+with Interfaces;
+
 package body Langkit_Support.Hashes is
 
    -------------
@@ -5,9 +7,24 @@ package body Langkit_Support.Hashes is
    -------------
 
    function Combine (L, R : Hash_Type) return Hash_Type is
+      use Interfaces;
+
+      --  The following is a translation from Boost's uint32_t hash function
+
+      C1 : constant Unsigned_32 := 16#cc9e2d51#;
+      C2 : constant Unsigned_32 := 16#1b873593#;
+      H1 : Unsigned_32 := Unsigned_32 (L);
+      K1 : Unsigned_32 := Unsigned_32 (R);
    begin
-      --  TODO??? Look for a more clever way to combine hashes
-      return L xor R;
+      K1 := K1 * C1;
+      K1 := Rotate_Left (K1, 15);
+      K1 := K1 * C2;
+
+      H1 := H1 xor K1;
+      H1 := Rotate_Left (H1, 13);
+      H1 := H1 * 5 + 16#e6546b64#;
+
+      return Hash_Type (H1);
    end Combine;
 
    -------------
