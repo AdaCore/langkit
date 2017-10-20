23db95f166ca85c39c1de2731929360d8083b10d
Pierre-Marie de Rodat <derodat@adacore.com>
2017-10-20 15:40:06 -0400

    Test rejection of memoization for properties that deal with side effects
    TN: QA16-034

---
+++ testsuite/tests/properties/invalid_memoized_trans/test.py
@@ -0,0 +1,60 @@
+"""
+Check that memoization for properties that deal with side effects is properly
+rejected.
+"""
+
+from __future__ import absolute_import, division, print_function
+
+from langkit.diagnostics import Diagnostics
+from langkit.dsl import ASTNode, UserField, T
+from langkit.expressions import Self, langkit_property
+from langkit.parsers import Grammar, Row
+
+from os import path
+from utils import emit_and_print_errors
+
+
+Diagnostics.set_lang_source_dir(path.abspath(__file__))
+
+
+class FooNode(ASTNode):
+    var = UserField(type=T.LogicVarType, public=False)
+
+
+class Example(FooNode):
+
+    # Most simple case: trying to memoize a property that does something with
+    # side effect.
+    @langkit_property(public=True, memoized=True)
+    def get_var():
+        return Self.var.get_value
+
+    # This one is just a wrapper, to demonstrate transitivity
+    @langkit_property(public=True)
+    def get_var_wrapper():
+        return Self.get_var
+
+    # This one is not memoized, but is another property with side effects
+    @langkit_property(public=True)
+    def get_var2():
+        return Self.var.get_value
+
+    # This tries to memoize a property that calls one which does directly a
+    # side effect (but the latter is not memoized).
+    @langkit_property(public=True, memoized=True)
+    def get_var2_public_wrapper():
+        return Self.get_var2
+
+    # This is memoized and calling two properties that do side effect by
+    # transitivity.
+    @langkit_property(public=True, memoized=True)
+    def top_wrapper():
+        return Self.get_var_wrapper == Self.get_var2
+
+
+grammar = Grammar('main_rule')
+grammar.add_rules(
+    main_rule=Row('example') ^ Example,
+)
+emit_and_print_errors(grammar)
+print('Done')
