d6426579707075163974af2e2afc90369fb18402
Pierre-Marie de Rodat <derodat@adacore.com>
2017-10-24 12:27:08 +0200

    properties/logging: flush stdout to have deterministic output
    This testcase mixes Python-based and Ada-based writes to the standard
    output, so we must do explicit flushes to keep the final output in the
    same order as execution.
    
    TN: QA19-056

--- testsuite/tests/properties/logging/main.py
+++ testsuite/tests/properties/logging/main.py
@@ -1,9 +1,14 @@
 from __future__ import absolute_import, division, print_function
 
-print('main.py: Running...')
-
-
 import sys
+
+
+def pflush(msg):
+    print(msg)
+    sys.stdout.flush()
+
+pflush('main.py: Running...')
+
 
 import libfoolang
 
@@ -12,12 +17,12 @@ ctx = libfoolang.AnalysisContext()
 u = ctx.get_from_buffer('main.txt', 'a(c) b(a c) +c(a)')
 if u.diagnostics:
     for d in u.diagnostics:
-        print(d)
+        pflush(d)
     sys.exit(1)
 
 u.populate_lexical_env()
 
-print('Calling p_entity_items...')
+pflush('Calling p_entity_items...')
 u.root[0].p_entity_items
 
-print('main.py: Done.')
+pflush('main.py: Done.')
