dd24a38b303d262593e379228db9b5641dedecb5
Pierre-Marie de Rodat <derodat@adacore.com>
2017-11-13 14:21:53 +0100

    invalid_array_lit: new testcase
    TN: OA28-063

---
+++ testsuite/tests/properties/invalid_array_lit/test.py
@@ -0,0 +1,40 @@
+from __future__ import absolute_import, division, print_function
+
+from langkit.diagnostics import Diagnostics
+from langkit.dsl import ASTNode, T
+from langkit.expressions import ArrayLiteral, Property, Self
+from langkit.parsers import Grammar
+
+from os import path
+from utils import emit_and_print_errors
+
+
+def run(name, expr):
+    """
+    Emit and print the errors we get for the below grammar with "expr" as
+    a property in Example.
+    """
+
+    Diagnostics.set_lang_source_dir(path.abspath(__file__))
+
+    print('== {} =='.format(name))
+
+    class FooNode(ASTNode):
+        pass
+
+    class Example(FooNode):
+        p = Property(expr)
+
+    grammar = Grammar('main_rule')
+    grammar.add_rules(
+        main_rule=Example('example'),
+    )
+    emit_and_print_errors(grammar)
+    print('')
+
+
+run('Empty: missing type', [])
+run('Empty: ok', ArrayLiteral([], T.BoolType))
+run('Single: ok', [True])
+run('Multiple: heterogeneous types', [True, Self])
+print('Done')
