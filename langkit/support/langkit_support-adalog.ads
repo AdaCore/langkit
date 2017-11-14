87005a5119fcd59aabda429eee57ffed0fd034e1
Pierre-Marie de Rodat <derodat@adacore.com>
2017-11-14 10:24:35 +0100

    Adalog: comment Early_Binding_Error
    TN: QB14-003

--- langkit/support/langkit_support-adalog.ads
+++ langkit/support/langkit_support-adalog.ads
@@ -1,3 +1,7 @@
 package Langkit_Support.Adalog with Pure is
+
    Early_Binding_Error : exception;
+   --  Exception raised when an equation cannot be solved because a logic
+   --  variable is referenced but is never assigned a value.
+
 end Langkit_Support.Adalog;
