d7dd38875ea8dc1ceeff55970b25ef3ae3891e5c
Pierre-Marie de Rodat <derodat@adacore.com>
2017-11-15 15:49:36 +0100

    Add a test to check automatic calls to Populate_Lexical_Env
    TN: QB15-019

---
+++ testsuite/tests/properties/auto_populate/main.py
@@ -0,0 +1,23 @@
+from __future__ import absolute_import, division, print_function
+
+print('main.py: Running...')
+
+
+import sys
+
+import libfoolang
+
+
+for prop in ('p_decl', 'p_decl_wrapper'):
+    ctx = libfoolang.AnalysisContext()
+    u = ctx.get_from_buffer('main.txt', 'a(c) b(a c) c(a)')
+    if u.diagnostics:
+        for d in u.diagnostics:
+            print(d)
+        sys.exit(1)
+
+    ref = u.root[1].f_items[0]
+    print('{}.{} = {}'.format(ref, prop, getattr(ref, prop)))
+
+
+print('main.py: Done.')
---
+++ testsuite/tests/properties/auto_populate/test.py
@@ -0,0 +1,56 @@
+"""
+Check that Populate_Lexical_Env is automatically called in public properties.
+"""
+
+from __future__ import absolute_import, division, print_function
+
+import os.path
+
+from langkit.diagnostics import Diagnostics
+from langkit.dsl import ASTNode, Field, T
+from langkit.envs import EnvSpec, add_to_env
+from langkit.expressions import Entity, New, Self, langkit_property
+from langkit.parsers import Grammar, List, Tok
+
+from lexer_example import Token
+from utils import build_and_run
+
+
+Diagnostics.set_lang_source_dir(os.path.abspath(__file__))
+
+
+class FooNode(ASTNode):
+    pass
+
+
+class Decl(FooNode):
+    name = Field()
+    items = Field()
+
+    env_spec = EnvSpec(
+        add_to_env(mappings=New(T.env_assoc, key=Self.name.symbol, val=Self))
+    )
+
+
+class Ref(FooNode):
+    name = Field()
+
+    @langkit_property(public=True, return_type=Decl.entity)
+    def decl_wrapper():
+        return Entity.decl
+
+    @langkit_property(public=True, return_type=Decl.entity)
+    def decl():
+        return Self.children_env.get_first(Self.name).cast_or_raise(Decl)
+
+
+fg = Grammar('main_rule')
+fg.add_rules(
+    main_rule=List(fg.decl),
+    decl=Decl(Tok(Token.Identifier, keep=True),
+              '(', fg.ref_list, ')'),
+    ref_list=List(fg.ref, empty_valid=True),
+    ref=Ref(Tok(Token.Identifier, keep=True)),
+)
+build_and_run(fg, 'main.py')
+print('Done')
