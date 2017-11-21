5eda2cb3c28fd5a4c0c8263524ba153ef9e6e7c5
Pierre-Marie de Rodat <derodat@adacore.com>
2017-11-21 12:13:32 +0100

    properties/invalid_bind_2: new test for Bind left/right operand types
    For GitHub issue #7
    TN: QB21-016

---
+++ testsuite/tests/properties/invalid_bind_2/test.py
@@ -0,0 +1,44 @@
+from __future__ import absolute_import, division, print_function
+
+from langkit.diagnostics import Diagnostics
+from langkit.dsl import ASTNode, T, UserField
+from langkit.expressions import Bind, Entity, Self, langkit_property
+from langkit.parsers import Grammar
+
+from os import path
+from utils import emit_and_print_errors
+
+
+def run(name, equation_expr):
+    """
+    Emit and print the errors we get for the below grammar with "expr" as
+    a property in BarNode.
+    """
+
+    Diagnostics.set_lang_source_dir(path.abspath(__file__))
+
+    print('== {} =='.format(name))
+
+    class FooNode(ASTNode):
+        var1 = UserField(type=T.LogicVarType, public=False)
+        var2 = UserField(type=T.LogicVarType, public=False)
+
+    class ExampleNode(FooNode):
+        @langkit_property(public=True)
+        def resolve():
+            return equation_expr.solve
+
+    grammar = Grammar('main_rule')
+    grammar.add_rules(main_rule=ExampleNode('example'))
+    emit_and_print_errors(grammar)
+    print('')
+
+
+# Valid cases
+run('Bind(logic_var, logic_var)', Bind(Self.var1, Self.var2))
+run('Bind(logic_var, entity)', Bind(Self.var1, Entity))
+
+# Invalid cases
+run('Bind(entity, logic_var)', Bind(Entity, Self.var1))
+run('Bind(logic_var, bool)', Bind(Self.var1, True))
+print('Done')
