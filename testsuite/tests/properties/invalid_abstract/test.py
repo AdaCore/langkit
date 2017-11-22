e93fd545afc1dde63ea2ae7f20f47373af8a47d3
Pierre-Marie de Rodat <derodat@adacore.com>
2017-11-22 15:18:03 +0100

    Add a test for invalid abstract overriding properties
    TN: QB21-034

---
+++ testsuite/tests/properties/invalid_abstract/test.py
@@ -0,0 +1,40 @@
+from __future__ import absolute_import, division, print_function
+
+from langkit.diagnostics import Diagnostics
+from langkit.dsl import ASTNode, T, abstract
+from langkit.expressions import AbstractProperty
+from langkit.parsers import Grammar
+
+from os import path
+from utils import emit_and_print_errors
+
+
+def run(name, runtime_check):
+    """
+    Emit and print the errors we get for the below grammar with "expr" as
+    a property in BarNode.
+    """
+
+    Diagnostics.set_lang_source_dir(path.abspath(__file__))
+
+    print('== {} =='.format(name))
+
+    class FooNode(ASTNode):
+        pass
+
+    @abstract
+    class BaseNode(FooNode):
+        prop = AbstractProperty(T.BoolType, public=True)
+
+    class Example(BaseNode):
+        prop = AbstractProperty(T.BoolType, runtime_check=runtime_check)
+
+    grammar = Grammar('main_rule')
+    grammar.add_rules(main_rule=Example('example'))
+    emit_and_print_errors(grammar)
+    print('')
+
+
+run('Abstracts overrides without runtime check', False)
+run('Abstracts overrides with runtime check', True)
+print('Done')
