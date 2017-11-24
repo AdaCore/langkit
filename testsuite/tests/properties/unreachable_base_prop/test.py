765d7ae7c90b5df63ec3458438ea76ed86999977
Pierre-Marie de Rodat <derodat@adacore.com>
2017-11-24 11:30:35 +0100

    Add a testcase for the emission of unreachable base properties warnings
    TN: QB21-034

---
+++ testsuite/tests/properties/unreachable_base_prop/test.py
@@ -0,0 +1,85 @@
+"""
+Test that we properly warn for properties that are unreachable because they are
+defined on an abstract node while all concrete subclasses have it overriden.
+"""
+
+from __future__ import absolute_import, division, print_function
+
+import os.path
+
+from langkit.diagnostics import Diagnostics
+from langkit.dsl import ASTNode, Field, T, abstract
+from langkit.expressions import AbstractProperty, Property
+from langkit.parsers import Grammar, List, Or, Pick, Tok
+
+from lexer_example import Token
+from utils import emit_and_print_errors
+
+
+Diagnostics.set_lang_source_dir(os.path.abspath(__file__))
+
+
+@abstract
+class FooNode(ASTNode):
+    pass
+
+
+@abstract
+class Expr(FooNode):
+
+    prop1 = AbstractProperty(T.LongType, public=True)
+
+    # Warning: all concrete subclasses override this (concrete root property)
+    prop2 = Property(0, public=True)
+
+    # Warning: all concrete subclasses override this (runtime check)
+    prop3 = AbstractProperty(T.LongType, public=True, runtime_check=True)
+
+
+@abstract
+class Atom(Expr):
+
+    # Warning: both concrete subclasses override this (concrete derived
+    # property).
+    prop1 = Property(1)
+
+
+class Lit(Atom):
+    tok = Field()
+
+    prop1 = Property(2)
+    prop2 = Property(2)
+    prop3 = Property(2)
+
+
+class Ref(Atom):
+    tok = Field()
+
+    prop1 = Property(3)
+    prop2 = Property(3)
+    prop3 = Property(3)
+
+
+class Plus(Expr):
+    lhs = Field()
+    rhs = Field()
+
+    prop1 = Property(4)
+    prop2 = Property(4)
+    prop3 = Property(4)
+
+
+grammar = Grammar('main_rule')
+grammar.add_rules(
+    main_rule=List(grammar.expr),
+
+    expr=Or(grammar.atom, grammar.plus),
+
+    atom=Or(grammar.lit, grammar.ref),
+    lit=Lit(Tok(Token.Number, keep=True)),
+    ref=Ref(Tok(Token.Identifier, keep=True)),
+
+    plus=Pick('(', Plus(grammar.expr, '+', grammar.expr), ')'),
+)
+emit_and_print_errors(grammar)
+print('Done')
