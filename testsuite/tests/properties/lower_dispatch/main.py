90d2b8a61258f11b7a522943ae3ce00c858fe8f9
Pierre-Marie de Rodat <derodat@adacore.com>
2017-11-24 10:16:00 +0100

    Add a testcase for the lowering of properties dispatching
    TN: QB21-034

---
+++ testsuite/tests/properties/lower_dispatch/main.py
@@ -0,0 +1,35 @@
+from __future__ import absolute_import, division, print_function
+
+print('main.py: Running...')
+
+
+import sys
+
+import libfoolang
+
+
+ctx = libfoolang.AnalysisContext()
+u = ctx.get_from_buffer('main.txt', """
+def a
+    (1 + (2 + c))
+def b
+    ((10 + (20 + a)) + 30)
+def c
+    100
+""")
+if u.diagnostics:
+    for d in u.diagnostics:
+        print(d)
+    sys.exit(1)
+
+for d in u.root:
+    for prop in ('p_evaluate_abstract', 'p_evaluate_rtcheck',
+                 'p_evaluate_concrete', 'p_evaluate_entity'):
+        try:
+            result = getattr(d.f_expr, prop)
+        except libfoolang.PropertyError as exc:
+            result = str(exc).strip()
+        print('{}.{} = {}'.format(d.f_name.text, prop, result))
+    print('')
+
+print('main.py: Done.')
---
+++ testsuite/tests/properties/lower_dispatch/test.py
@@ -0,0 +1,136 @@
+"""
+Test that the lowering pass for properties dispatching works properly on all
+kinds of root properties:
+
+  * concrete, abstract and runtime-checked abstract;
+  * using Entity and using Self.
+"""
+
+from __future__ import absolute_import, division, print_function
+
+import os.path
+
+from langkit.diagnostics import Diagnostics
+from langkit.dsl import ASTNode, Field, T, abstract
+from langkit.envs import EnvSpec, add_to_env
+from langkit.expressions import (AbstractProperty, DynamicVariable, Entity,
+                                 New, Self, langkit_property)
+from langkit.parsers import Grammar, List, Or, Pick, Tok
+
+from lexer_example import Token
+from utils import build_and_run
+
+
+Diagnostics.set_lang_source_dir(os.path.abspath(__file__))
+
+
+dyn_var = DynamicVariable('dyn_var', T.FooNode)
+
+
+@abstract
+class FooNode(ASTNode):
+    pass
+
+
+@abstract
+class Expr(FooNode):
+
+    evaluate_abstract = AbstractProperty(T.LongType, public=True)
+    evaluate_rtcheck = AbstractProperty(T.LongType, public=True,
+                                        runtime_check=True)
+
+    @langkit_property(public=True)
+    def evaluate_concrete():
+        return 1
+
+    evaluate_entity = AbstractProperty(T.LongType, public=True)
+
+
+@abstract
+class Atom(Expr):
+    pass
+
+
+class Lit(Atom):
+    tok = Field()
+
+    @langkit_property()
+    def evaluate_abstract():
+        return 1
+
+    @langkit_property()
+    def evaluate_rtcheck():
+        return 1
+
+    @langkit_property(public=True)
+    def evaluate_entity():
+        return 1
+
+
+class Ref(Atom):
+    tok = Field()
+
+    @langkit_property(public=True)
+    def resolve():
+        return Self.node_env.get_first(Self.tok.symbol).cast_or_raise(T.Def)
+
+    @langkit_property()
+    def evaluate_abstract():
+        return Self.resolve.expr.evaluate_abstract
+
+    @langkit_property(public=True)
+    def evaluate_concrete():
+        return Self.resolve.expr.evaluate_concrete
+
+    @langkit_property(public=True)
+    def evaluate_entity():
+        return Entity.resolve.expr.evaluate_entity
+
+
+class Plus(Expr):
+    lhs = Field()
+    rhs = Field()
+
+    @langkit_property()
+    def evaluate_abstract():
+        return Self.lhs.evaluate_abstract + Self.rhs.evaluate_abstract
+
+    @langkit_property()
+    def evaluate_rtcheck():
+        return Self.lhs.evaluate_rtcheck + Self.rhs.evaluate_rtcheck
+
+    @langkit_property(public=True)
+    def evaluate_concrete():
+        return Self.lhs.evaluate_concrete + Self.rhs.evaluate_concrete
+
+    @langkit_property(public=True)
+    def evaluate_entity():
+        return Entity.lhs.evaluate_entity + Entity.rhs.evaluate_entity
+
+
+class Def(FooNode):
+    name = Field()
+    expr = Field()
+
+    env_spec = EnvSpec(
+        add_to_env(mappings=New(T.env_assoc, key=Self.name.symbol, val=Self))
+    )
+
+
+grammar = Grammar('main_rule')
+grammar.add_rules(
+    main_rule=List(Or(
+        Def('def', Tok(Token.Identifier, keep=True), grammar.expr),
+        grammar.expr
+    )),
+
+    expr=Or(grammar.atom, grammar.plus),
+
+    atom=Or(grammar.lit, grammar.ref),
+    lit=Lit(Tok(Token.Number, keep=True)),
+    ref=Ref(Tok(Token.Identifier, keep=True)),
+
+    plus=Pick('(', Plus(grammar.expr, '+', grammar.expr), ')'),
+)
+build_and_run(grammar, 'main.py')
+print('Done')
