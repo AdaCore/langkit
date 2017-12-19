f32d616015dac66d2874a049004c0900a536313a
Pierre-Marie de Rodat <derodat@adacore.com>
2017-12-19 16:13:55 +0100

    Add a testcase for env lookups on grouped envs
    TN: QC11-035

---
+++ testsuite/tests/langkit_support/lexical_env_group/main.adb
@@ -0,0 +1,36 @@
+--  Test that lookups on grouped lexical envs works correctly
+
+with Ada.Text_IO; use Ada.Text_IO;
+
+with Langkit_Support.Lexical_Env;
+with Langkit_Support.Symbols; use Langkit_Support.Symbols;
+with Langkit_Support.Text;    use Langkit_Support.Text;
+
+with Support; use Support;
+use Support.Envs;
+
+procedure Main is
+   Symbols : Symbol_Table := Create;
+   Key_X   : constant Symbol_Type := Find (Symbols, "X");
+
+   A_Parent : constant Lexical_Env :=
+      Create (No_Env_Getter, 'P', Owner => True);
+   A        : constant Lexical_Env :=
+      Create (Simple_Env_Getter (A_Parent), 'A', Owner => True);
+   B        : constant Lexical_Env :=
+      Create (No_Env_Getter, 'B', Owner => True);
+
+   type Env_Array is array (Positive range <>) of Lexical_Env;
+   function Group is new Support.Envs.Group (Positive, Env_Array);
+   Grouped : constant Lexical_Env := Group ((A, B));
+begin
+   Add (A_Parent, Key_X, '1');
+   Add (A, Key_X, '2');
+   Add (B, Key_X, '3');
+
+   Put_Line ("Looking in Grouped (Recursive => True):");
+   Put_Line (Get (Grouped, Key_X, Recursive => True));
+
+   Put_Line ("Looking in Grouped (Recursive => False):");
+   Put_Line (Get (Grouped, Key_X, Recursive => False));
+end Main;
