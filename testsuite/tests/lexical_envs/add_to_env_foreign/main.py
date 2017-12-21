320ba6d0416c1cfa9834e47095e4f091036df5e3
Pierre-Marie de Rodat <derodat@adacore.com>
2017-12-21 14:52:01 +0100

    Test that foreign nodes are properly accepted/rejected in add_to_env
    TN: QC21-013

---
+++ testsuite/tests/lexical_envs/add_to_env_foreign/main.py
@@ -0,0 +1,59 @@
+from __future__ import absolute_import, division, print_function
+
+print('main.py: Running...')
+
+
+import sys
+
+import libfoolang
+
+
+ctx = libfoolang.AnalysisContext()
+ctx.discard_errors_in_populate_lexical_env(False)
+
+
+def process(name, buffer):
+    u = ctx.get_from_buffer(name, buffer)
+    if u.diagnostics:
+        for d in u.diagnostics:
+            print('{}:{}'.format(name, d))
+        sys.exit(1)
+
+    print('Processing {}...'.format(name))
+    try:
+        u.populate_lexical_env()
+    except libfoolang.PropertyError as exc:
+        print('   Got an exception: {}'.format(str(exc).strip()))
+    else:
+        print('   All good...')
+
+
+# This deals with only local nodes and environments: there is no error here
+process('foo', """
+    foo {
+        bar {}
+    }
+
+    +foo (foo.bar)
+""")
+
+# Adding a node from the current unit to a foreign unit's lexical env is
+# allowed.
+process('add_to_foreign', """
+    foo.bar
+""")
+
+# Adding a foreign node to the current unit's lexical env is an error
+process('add_foreign_node', """
+    +foo.bar
+""")
+
+# Adding a local node to the current unit's lexical environment is allowed, but
+# when the metadata contains a foreign node, this is an error.
+process('add_foreign_md', """
+    local {}
+    +local (foo)
+""")
+
+
+print('main.py: Done.')
---
+++ testsuite/tests/lexical_envs/add_to_env_foreign/test.py
@@ -0,0 +1,126 @@
+"""
+Test that add_to_env actions that try to insert foreign nodes (as mapping value
+or metadata field) are properly rejected.
+"""
+
+from __future__ import absolute_import, division, print_function
+
+from langkit.dsl import (ASTNode, Field, Struct, T, UserField, abstract,
+                         env_metadata)
+from langkit.envs import EnvSpec, add_to_env, add_env
+from langkit.expressions import AbstractKind, New, Self, langkit_property
+from langkit.parsers import Grammar, List, Opt, Or, Tok
+
+from lexer_example import Token
+from utils import build_and_run
+
+
+@env_metadata
+class Metadata(Struct):
+    node = UserField(T.FooNode)
+
+
+class FooNode(ASTNode):
+    pass
+
+
+class Scope(FooNode):
+    name = Field(type=T.SimpleId)
+    content = Field()
+
+    env_spec = EnvSpec(
+        add_to_env(New(T.env_assoc, key=Self.name.get_symbol, val=Self)),
+        add_env()
+    )
+
+
+@abstract
+class Id(FooNode):
+    @langkit_property(return_type=T.SimpleId, kind=AbstractKind.abstract)
+    def simple_name():
+        pass
+
+    @langkit_property(return_type=T.Scope, kind=AbstractKind.abstract)
+    def resolve(base_env=T.LexicalEnvType):
+        pass
+
+
+class SimpleId(Id):
+    name = Field(type=T.TokenType)
+
+    @langkit_property()
+    def get_symbol():
+        return Self.name.symbol
+
+    @langkit_property()
+    def simple_name():
+        return Self
+
+    @langkit_property()
+    def resolve(base_env=T.LexicalEnvType):
+        return base_env.get_first(Self.get_symbol).el.cast(T.Scope)
+
+
+class ScopedId(Id):
+    scope = Field(type=T.Id)
+    name = Field(type=T.SimpleId)
+
+    @langkit_property()
+    def simple_name():
+        return Self.name
+
+    @langkit_property()
+    def resolve(base_env=T.LexicalEnvType):
+        return (Self.scope.resolve(base_env)
+                .children_env.get_first(Self.name.get_symbol).el
+                .cast(T.Scope))
+
+
+class ForeignDecl(FooNode):
+    id = Field(type=T.Id)
+
+    env_spec = EnvSpec(
+        add_to_env(
+            New(T.env_assoc, key=Self.id.simple_name.get_symbol, val=Self),
+            dest_env=Self.id.match(
+                lambda simple=T.SimpleId:
+                    simple.node_env,
+                lambda scoped=T.ScopedId:
+                    scoped.resolve(Self.node_env).children_env,
+            )
+        )
+    )
+
+
+class SelfDecl(FooNode):
+    id = Field(type=T.Id)
+    md_node = Field(type=T.Id)
+
+    env_spec = EnvSpec(
+        add_to_env(
+            New(T.env_assoc,
+                key=Self.id.simple_name.get_symbol,
+                val=Self.id.resolve(Self.node_env)),
+            metadata=New(
+                T.Metadata,
+                node=Self.md_node.then(lambda n: n.resolve(Self.node_env))
+            )
+        )
+    )
+
+
+G = Grammar('main_rule')
+G.add_rules(
+    main_rule=List(Or(G.scope, G.self_decl, G.foreign_decl)),
+    scope=Scope(G.simple_identifier,
+                '{', List(G.scope, empty_valid=True), '}'),
+
+    identifier=Or(ScopedId(G.identifier, '.', G.simple_identifier),
+                  G.simple_identifier),
+    simple_identifier=SimpleId(Tok(Token.Identifier, keep=True)),
+
+    foreign_decl=ForeignDecl(G.identifier),
+    self_decl=SelfDecl('+', G.identifier, Opt('(', G.identifier, ')')),
+)
+build_and_run(G, 'main.py')
+print('Done')
