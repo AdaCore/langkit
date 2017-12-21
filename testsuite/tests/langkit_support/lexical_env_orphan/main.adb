d32faf7f546cb20124cbe4b35310cd16bb5b9dcc
Pierre-Marie de Rodat <derodat@adacore.com>
2017-12-21 15:28:12 +0100

    Check error raised when trying to orphan with invalid input env
    TN: QC21-013

--- testsuite/tests/langkit_support/lexical_env_orphan/main.adb
+++ testsuite/tests/langkit_support/lexical_env_orphan/main.adb
@@ -1,6 +1,7 @@
 --  Test that lookups on orphaned lexical envs works correctly
 
-with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Exceptions; use Ada.Exceptions;
+with Ada.Text_IO;    use Ada.Text_IO;
 
 with Langkit_Support.Lexical_Env;
 with Langkit_Support.Symbols; use Langkit_Support.Symbols;
@@ -13,6 +14,12 @@ procedure Main is
    Symbols : Symbol_Table := Create;
    Key_A   : constant Symbol_Type := Find (Symbols, "A");
    Key_B   : constant Symbol_Type := Find (Symbols, "B");
+
+   Old_Env : constant Lexical_Env :=
+      Create (No_Env_Getter, 'O', Owner => True);
+   New_Env : constant Lexical_Env :=
+      Create (No_Env_Getter, 'N', Owner => True);
+   Rebindings : constant Env_Rebindings := Append (null, Old_Env, New_Env);
 
    Root     : constant Lexical_Env :=
       Create (No_Env_Getter, 'R', Owner => True);
@@ -34,4 +41,62 @@ begin
 
    Put_Line ("Looking for B in Orphaned:");
    Put_Line (Get (Orphaned, Key_B));
+
+   declare
+      type Env_Array is array (Positive range <>) of Lexical_Env;
+      function Group is new Support.Envs.Group (Positive, Env_Array);
+
+      Transitive_Child : constant Lexical_Env :=
+         Create (Simple_Env_Getter (Root), 'C', True, True);
+
+      Grouped : constant Lexical_Env :=
+         Group ((Root, Child));
+
+      Rebound_TC : constant Lexical_Env :=
+         Rebind_Env (Transitive_Child, Rebindings);
+
+      Rebound_OK : constant Lexical_Env :=
+         Rebind_Env (Child, Rebindings);
+
+      E : Lexical_Env;
+   begin
+
+      Put_Line ("Trying to build an orphan with transitive parent...");
+      begin
+         E := Orphan (Transitive_Child);
+         Put_Line ("  No error raised");
+      exception
+         when Exc : Property_Error =>
+            Put_Line ("  Got a property error: " & Exception_Message (Exc));
+      end;
+
+      Put_Line ("Trying to build an orphan with grouped env...");
+      begin
+         E := Orphan (Grouped);
+         Put_Line ("  No error raised");
+      exception
+         when Exc : Property_Error =>
+            Put_Line ("  Got a property error: " & Exception_Message (Exc));
+      end;
+
+      Put_Line ("Trying to build an orphan with rebound transitive parent...");
+      begin
+         E := Orphan (Rebound_TC);
+         Put_Line ("  No error raised");
+      exception
+         when Exc : Property_Error =>
+            Put_Line ("  Got a property error: " & Exception_Message (Exc));
+      end;
+
+      Put_Line
+        ("Trying to build an orphan with rebound non-transitive parent...");
+      begin
+         E := Orphan (Rebound_OK);
+         Put_Line ("  No error raised");
+      exception
+         when Exc : Property_Error =>
+            Put_Line ("  Got a property error: " & Exception_Message (Exc));
+      end;
+
+   end;
 end Main;
