824bed2f73917113ceeda5809ba45204fd85d526
Pierre-Marie de Rodat <derodat@adacore.com>
2018-01-17 14:37:33 +0100

    Add a basic testcase for the AST rewriting API
    TN: R115-038

---
+++ testsuite/tests/misc/rewriting/main.adb
@@ -0,0 +1,99 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+with System.Assertions;
+
+with Libfoolang.Analysis;  use Libfoolang.Analysis;
+with Libfoolang.Rewriting; use Libfoolang.Rewriting;
+
+procedure Main is
+   Buffer : constant String :=
+     ("def a = 1" & ASCII.LF
+      & "def b = (2 + a) + 3" & ASCII.LF
+      & "def c = a + b" & ASCII.LF);
+
+   procedure Try (Label : String; Proc : access procedure);
+
+   ---------
+   -- Try --
+   ---------
+
+   procedure Try (Label : String; Proc : access procedure) is
+   begin
+      Put_Line (Label & "...");
+      Proc.all;
+      Put_Line ("   Done with no assert failure");
+   exception
+      when System.Assertions.Assert_Failure =>
+         Put_Line ("   Got an assert failure");
+   end Try;
+
+   Ctx : constant Analysis_Context := Create;
+   U   : constant Analysis_Unit := Get_From_Buffer
+     (Ctx, "main.txt", Buffer => Buffer);
+   RH  : Rewriting_Handle;
+   UH  : Unit_Rewriting_Handle;
+begin
+   if Has_Diagnostics (U) then
+      Put_Line ("Errors:");
+      for D of Diagnostics (U) loop
+         Put_Line (Format_GNU_Diagnostic (U, D));
+      end loop;
+      return;
+   end if;
+
+   if Handle (Ctx) /= No_Rewriting_Handle then
+      raise Program_Error;
+   end if;
+
+   declare
+      procedure Proc;
+
+      procedure Proc is
+         Dummy : constant Unit_Rewriting_Handle := Handle (U);
+      begin
+         null;
+      end Proc;
+   begin
+      Try ("Try to get a unit rewriting handle out of a rewriting session",
+           Proc'Access);
+   end;
+
+   declare
+      procedure Proc;
+
+      procedure Proc is
+      begin
+         RH := Start_Rewriting (Ctx);
+      end Proc;
+   begin
+      Put_Line ("Create a rewriting handle");
+      Proc;
+      Try ("Try to create a second rewriting handle", Proc'Access);
+   end;
+
+   if Handle (Ctx) /= RH then
+      raise Program_Error;
+   elsif Context (RH) /= Ctx then
+      raise Program_Error;
+   end if;
+
+   Put_Line ("Get a rewriting handle for the analysis unit");
+   UH := Handle (U);
+   pragma Unreferenced (UH);
+
+   Put_Line ("Apply the rewriting");
+   Apply (RH);
+   if Handle (Ctx) /= No_Rewriting_Handle then
+      raise Program_Error;
+   end if;
+
+   --  Test that we can do a new rewriting session once the previous one is
+   --  done.
+
+   Put_Line ("Create a second rewriting handler");
+   RH := Start_Rewriting (Ctx);
+   Put_Line ("Apply the rewriting");
+   Apply (RH);
+
+   Put_Line ("main.adb: Done.");
+end Main;
---
+++ testsuite/tests/misc/rewriting/test.py
@@ -0,0 +1,63 @@
+"""
+Test that basic rewriting API usage behaves as expected.
+"""
+
+from __future__ import absolute_import, division, print_function
+
+from langkit.dsl import ASTNode, Field, abstract
+from langkit.parsers import Grammar, List, Or
+
+from lexer_example import Token
+from utils import build_and_run
+
+
+class FooNode(ASTNode):
+    pass
+
+
+class Name(FooNode):
+    token_node = True
+
+
+class Def(FooNode):
+    name = Field()
+    expr = Field()
+
+
+@abstract
+class Expr(FooNode):
+    pass
+
+
+class Literal(Expr):
+    token_node = True
+
+
+class Ref(Expr):
+    name = Field()
+
+
+class ParentExpr(Expr):
+    expr = Field()
+
+
+class Plus(Expr):
+    lhs = Field()
+    rhs = Field()
+
+
+g = Grammar('main_rule')
+g.add_rules(
+    main_rule=List(g.def_rule),
+
+    name=Name(Token.Identifier),
+
+    def_rule=Def('def', g.name, '=', g.expr),
+
+    expr=Or(Plus(g.expr, '+', g.expr),
+            ParentExpr('(', g.expr, ')'),
+            Ref(g.name),
+            Literal(Token.Number))
+)
+build_and_run(g, ada_main='main.adb', generate_unparser=True)
+print('Done')
