b7e416d9a9920fefb78a674c1774ebf9d8bde514
Pierre-Marie de Rodat <derodat@adacore.com>
2018-01-25 17:30:04 +0100

    grammar/invalid_token_node: new testcase
    TN: R125-040

---
+++ testsuite/tests/grammar/invalid_token_node/test.py
@@ -0,0 +1,20 @@
+from __future__ import absolute_import, division, print_function
+
+from langkit.diagnostics import DiagnosticError
+from langkit.dsl import ASTNode
+
+
+class FooNode(ASTNode):
+    pass
+
+
+class TokenNode(FooNode):
+    token_node = True
+
+try:
+    class NonTokenNode(TokenNode):
+        token_node = False
+except DiagnosticError:
+    pass
+
+print('Done')
