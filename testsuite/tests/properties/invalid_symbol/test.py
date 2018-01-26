0924c09b745b760ac4c345a57d6027104a1ef5f1
Pierre-Marie de Rodat <derodat@adacore.com>
2018-01-26 13:14:41 +0100

    Add a test for sanitization of .symbol DSL attribute inputs
    TN: R125-040

---
+++ testsuite/tests/properties/invalid_symbol/test.py
@@ -0,0 +1,39 @@
+"""
+Check that invalid inputs for the ".symbol" DSL attribute are properly
+rejected.
+"""
+
+from __future__ import absolute_import, division, print_function
+
+from langkit.dsl import ASTNode, T
+from langkit.expressions import Property, Self
+from langkit.parsers import Grammar
+
+from utils import emit_and_print_errors
+
+
+def run(name, expr):
+    """
+    Emit and print the errors we get for the below grammar with "expr" as
+    a property in BarNode.
+    """
+
+    print('== {} =='.format(name))
+
+    class FooNode(ASTNode):
+        pass
+
+    class Example(FooNode):
+        token_node = True
+        p = Property(expr.symbol, public=True)
+
+    grammar = Grammar('main_rule')
+    grammar.add_rules(main_rule=Example('example'))
+    emit_and_print_errors(grammar)
+    print('')
+
+
+run('Valid   - Token node', Self)
+run('Invalid - Bad type', Self.is_null)
+run('Invalid - Non-token node', Self.cast(T.FooNode))
+print('Done')
