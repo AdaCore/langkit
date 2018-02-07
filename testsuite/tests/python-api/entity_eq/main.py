704f75dfaac5ef48db7c9253e98973e2410e3e26
Pierre-Marie de Rodat <derodat@adacore.com>
2018-02-07 12:04:02 +0100

    Add a testcase to check that entity equality works in the Python API
    TN: R206-016

---
+++ testsuite/tests/python-api/entity_eq/main.py
@@ -0,0 +1,38 @@
+from __future__ import absolute_import, division, print_function
+
+import sys
+
+import libfoolang
+
+
+ctx = libfoolang.AnalysisContext()
+u = ctx.get_from_buffer('main.txt', """
+(T) foo { a b }
+
+(T U) old_bar { c d }
+(T U) new_bar { e f }
+
+(V) old_baz { g }
+(V) new_baz { h }
+""")
+if u.diagnostics:
+    for d in u.diagnostics:
+        print(d)
+    sys.exit(1)
+
+foo, old_bar, new_bar, old_baz, new_baz = u.root
+rebound1 = foo.p_rebind(old_bar, new_bar)
+rebound2 = foo.p_rebind(old_bar, new_bar)
+rebound3 = foo.p_rebind(old_baz, new_baz)
+
+for e1, e2 in [
+    (rebound1, rebound1),
+    (rebound1, rebound2),
+    (rebound1, rebound3),
+]:
+    print('{} vs. {}'.format(e1.entity_repr, e2.entity_repr))
+    print('  identity check:   {}'.format(e1 is e2))
+    print('  equality check:   {}'.format(e1 == e2))
+    print('  inequality check: {}'.format(e1 != e2))
+
+print('main.py: Done.')
---
+++ testsuite/tests/python-api/entity_eq/test.py
@@ -0,0 +1,87 @@
+"""
+Check that "==" and "!=" operators for Entity in the Python API behave as
+expected.
+"""
+
+from __future__ import absolute_import, division, print_function
+
+from langkit.dsl import ASTNode, Field, abstract
+from langkit.envs import EnvSpec, add_env, add_to_env
+from langkit.expressions import (AbstractProperty, Entity, New, Property, Self,
+                                 T, Var, langkit_property)
+from langkit.parsers import Grammar, List, Pick
+
+from lexer_example import Token
+from utils import build_and_run
+
+
+class FooNode(ASTNode):
+    pass
+
+
+class Name(FooNode):
+    token_node = True
+
+
+@abstract
+class Def(FooNode):
+    name = AbstractProperty(T.SymbolType, public=True)
+    env_spec = EnvSpec(add_to_env(mappings=New(T.env_assoc,
+                                               key=Self.name,
+                                               val=Self)))
+
+
+class Block(Def):
+    params = Field(type=T.Params)
+    name_field = Field(type=T.Name)
+    vars = Field(type=T.BlockVar.list)
+
+    name = Property(Self.name_field.symbol)
+
+    @langkit_property(public=True)
+    def rebind(from_block=T.Block.entity, to_block=T.Block.entity):
+        rbdng = Var(Entity.info.rebindings.append_rebinding(
+            from_block.params.children_env,
+            to_block.children_env
+        ))
+
+        e_info = Var(New(T.entity_info,
+                         md=Entity.info.md,
+                         rebindings=rbdng))
+
+        return New(Block.entity, el=Self, info=e_info)
+
+    env_spec = EnvSpec(
+        add_to_env(mappings=New(T.env_assoc, key=Self.name, val=Self)),
+        add_env()
+    )
+
+
+class Param(Def):
+    name_field = Field(type=T.Name)
+    name = Property(Self.name_field.symbol)
+
+
+class Params(Param.list):
+    env_spec = EnvSpec(add_env())
+
+
+class BlockVar(Def):
+    name_field = Field(type=T.Name)
+    name = Property(Self.name_field.symbol)
+
+
+grammar = Grammar('main_rule')
+grammar.add_rules(
+    main_rule=List(grammar.block),
+    name=Name(Token.Identifier),
+    block=Block(grammar.params, grammar.name, grammar.vars),
+
+    params=Pick('(', List(grammar.param, list_cls=Params), ')'),
+    param=Param(grammar.name),
+
+    vars=Pick('{', List(grammar.var), '}'),
+    var=BlockVar(grammar.name),
+)
+build_and_run(grammar, 'main.py')
+print('Done')
