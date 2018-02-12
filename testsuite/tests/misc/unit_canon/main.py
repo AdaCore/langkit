40471c972bee7ccc34a3b3a569dc9b4d8c2aa9f5
Pierre-Marie de Rodat <derodat@adacore.com>
2018-02-12 17:02:54 +0100

    tests/misc/unit_canon: add some logging...
    ... hopping it will help diagnose issues that occur only nightly on
    Windows.
    
    TN: R209-030

--- testsuite/tests/misc/unit_canon/main.py
+++ testsuite/tests/misc/unit_canon/main.py
@@ -1,6 +1,5 @@
 from __future__ import absolute_import, division, print_function
 
-import itertools
 import os.path
 
 import libfoolang
@@ -8,18 +7,39 @@ import libfoolang
 
 ctx = libfoolang.AnalysisContext()
 
-filenames = [
+f1, f2, f3 = [
     ('rel_foo', 'foo.txt'),
     ('abs_foo', os.path.abspath('foo.txt')),
-    ('rel_bar', 'bar'),
+    ('rel_bar', 'bar.txt'),
 ]
 
-for (label1, f1), (label2, f2) in itertools.product(filenames, filenames):
+tests = [
+    (f1, f1, True),
+    (f1, f2, True),
+    (f1, f3, False),
+    (f2, f1, True),
+    (f2, f2, True),
+    (f2, f3, False),
+    (f3, f1, False),
+    (f3, f2, False),
+    (f3, f3, True),
+]
+
+for (label1, f1), (label2, f2), exp_result in tests:
     u1 = ctx.get_from_file(f1)
     u2 = ctx.get_from_file(f2)
+    result = u1.root == u2.root
     print('Comparing root nodes for {} and {}: {}'.format(
-        label1, label2,
-        u1.root == u2.root
+        label1, label2, result
     ))
+    if result != exp_result:
+        print('   u1 = {}'.format(u1.filename))
+        print('   u2 = {}'.format(u2.filename))
+
+        print('   n1:tuple = {}'.format(u1.root._id_tuple))
+        print('   n1:env_rebindings = {}'.format(u1.root._rebindings))
+
+        print('   n2:tuple = {}'.format(u2.root._id_tuple))
+        print('   n2:env_rebindings = {}'.format(u2.root._rebindings))
 
 print('Done.')
