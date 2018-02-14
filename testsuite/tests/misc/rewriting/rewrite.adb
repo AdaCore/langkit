4264d413bb7280a366e621d5c7a220ffe2c89b60
Pierre-Marie de Rodat <derodat@adacore.com>
2018-02-14 10:39:34 +0100

    misc/rewriting: add tests for nodes creation
    TN: R115-038

--- testsuite/tests/misc/rewriting/rewrite.adb
+++ testsuite/tests/misc/rewriting/rewrite.adb
@@ -2,14 +2,18 @@ with Ada.Text_IO; use Ada.Text_IO;
 
 with System.Assertions;
 
-with Libfoolang.Analysis;  use Libfoolang.Analysis;
-with Libfoolang.Rewriting; use Libfoolang.Rewriting;
+with Langkit_Support.Text;     use Langkit_Support.Text;
+with Libfoolang.Analysis;      use Libfoolang.Analysis;
+with Libfoolang.Introspection; use Libfoolang.Introspection;
+with Libfoolang.Rewriting;     use Libfoolang.Rewriting;
 
 procedure Rewrite is
    Buffer : constant String :=
      ("def a = 1" & ASCII.LF
       & "def b = (2 + a) + 3" & ASCII.LF
-      & "def c = a + b" & ASCII.LF);
+      & "def c = a + b" & ASCII.LF
+      & "def d = 4" & ASCII.LF
+      & "def e = 5" & ASCII.LF);
 
    procedure Try (Label : String; Proc : access procedure);
 
@@ -31,6 +35,7 @@ procedure Rewrite is
    U   : constant Analysis_Unit := Get_From_Buffer
      (Ctx, "main.txt", Buffer => Buffer);
    RH  : Rewriting_Handle;
+   N   : Node_Rewriting_Handle;
 begin
    if Has_Diagnostics (U) then
       Put_Line ("Errors:");
@@ -41,10 +46,9 @@ begin
    end if;
 
    RH := Start_Rewriting (Ctx);
+   N := Handle (Root (U));
 
    declare
-      N : constant Node_Rewriting_Handle := Handle (Root (U));
-
       procedure Proc;
 
       ----------
@@ -65,11 +69,70 @@ begin
       Set_Child (N, 2, Clone (Child (N, 3)));
    end;
 
+   New_Line;
+   Put_Line ("Swap first and fourth defs");
+   declare
+      N1 : constant Node_Rewriting_Handle := Child (N, 1);
+      N4 : constant Node_Rewriting_Handle := Child (N, 4);
+   begin
+      if not Tied (N1) then
+         raise Program_Error;
+      end if;
+      if not Tied (N4) then
+         raise Program_Error;
+      end if;
+
+      Set_Child (N, 1, No_Node_Rewriting_Handle);
+      Set_Child (N, 4, No_Node_Rewriting_Handle);
+
+      if Tied (N1) then
+         raise Program_Error;
+      end if;
+      if Tied (N4) then
+         raise Program_Error;
+      end if;
+
+      Set_Child (N, 1, N4);
+      Set_Child (N, 4, N1);
+
+      if not Tied (N1) then
+         raise Program_Error;
+      end if;
+      if not Tied (N4) then
+         raise Program_Error;
+      end if;
+   end;
+
+   New_Line;
+   Put_Line ("Replace the expression of the fifth definition");
+   declare
+      Nested_Expr : constant Node_Rewriting_Handle := Create_Parent_Expr
+        (RH, Create_Plus
+               (RH,
+                Create_Token_Node (RH, Foo_Literal, "5"),
+                Create_Ref (RH, Create_Token_Node (RH, Foo_Name, "c"))));
+
+      Top_Expr : constant Node_Rewriting_Handle := Create_Parent_Expr
+        (RH, Create_Plus
+               (RH,
+                Create_Ref (RH, Create_Token_Node (RH, Foo_Name, "d")),
+                Nested_Expr));
+   begin
+      Set_Child
+        (Child (N, 5),
+         Index (Def_F_Expr),
+         Top_Expr);
+   end;
+
+   New_Line;
+   Put_Line ("Applying the diff...");
    if not Apply (RH) then
+      Put_Line ("Could not parse the following:");
+      Put_Line (Image (Unparse (N)));
       raise Program_Error;
    end if;
 
    Root (U).Print (Show_Slocs => False);
 
-   Put_Line ("main2.adb: Done.");
+   Put_Line ("rewrite.adb: Done.");
 end Rewrite;
