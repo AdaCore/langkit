0c908aaecc62c5429f2ee62fd13a00ed6b0b90e0
Pierre-Marie de Rodat <derodat@adacore.com>
2018-02-21 18:24:06 +0100

    Add a testcase for reparsing/PLE asynchronicity
    TN: R220-021

---
+++ testsuite/tests/lexical_envs/ple_after_reparse/main.adb
@@ -0,0 +1,94 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+with GNATCOLL.Traces;
+with GNATCOLL.VFS; use GNATCOLL.VFS;
+
+with Langkit_Support.Slocs; use Langkit_Support.Slocs;
+
+with Libfoolang.Analysis; use Libfoolang.Analysis;
+
+procedure Main is
+   Ctx    : Analysis_Context := Create;
+   Unit_A : constant Analysis_Unit := Get_From_File (Ctx, "a.txt");
+   Unit_B : constant Analysis_Unit := Get_From_File (Ctx, "b.txt");
+   Unit_C : constant Analysis_Unit := Get_From_File (Ctx, "b-c.txt");
+
+   procedure Resolve;
+   function Visit (Node : Foo_Node'Class) return Visit_Status;
+   function Node_Image (Node : Foo_Node'Class) return String;
+
+   -------------
+   -- Resolve --
+   -------------
+
+   procedure Resolve is
+      Dummy : Visit_Status;
+   begin
+      Put_Line ("Starting resolution...");
+      Dummy := Root (Unit_A).Traverse (Visit'Access);
+      New_Line;
+   end Resolve;
+
+   ----------------
+   -- Node_Image --
+   ----------------
+
+   function Node_Image (Node : Foo_Node'Class) return String is
+   begin
+      if Node.Is_Null then
+         return "None";
+      end if;
+
+      declare
+         Fullname : constant String := Get_Filename (Node.Unit);
+         Basename : constant String := +Create (+Fullname).Base_Name;
+      begin
+         return ("<" & Node.Kind_Name & " " & Basename & ":"
+                 & Image (Node.Sloc_Range) & ">");
+      end;
+   end Node_Image;
+
+   -----------
+   -- Visit --
+   -----------
+
+   function Visit (Node : Foo_Node'Class) return Visit_Status is
+   begin
+      if not Node.Is_Null and then Node.Kind = Foo_Var then
+         declare
+            V    : constant Var := Node.As_Var;
+            Decl : constant Foo_Node := V.F_Value.P_Resolve;
+         begin
+            Put_Line ("   " & Node_Image (V) & " -> " & Node_Image (Decl));
+         end;
+      end if;
+      return Into;
+   end Visit;
+
+begin
+   GNATCOLL.Traces.Parse_Config ("Main_Trace=yes >&1");
+   GNATCOLL.Traces.Set_Active (Main_Trace, True);
+
+   Put_Line ("Performing resolution from scratch...");
+   Resolve;
+
+   Put_Line ("Performing resolution after reparsing a.txt...");
+   Reparse (Unit_A);
+   Resolve;
+
+   Put_Line ("Performing resolution after reparsing b.txt...");
+   Reparse (Unit_B);
+   Resolve;
+
+   Put_Line ("Performing resolution after reparsing b-c.txt...");
+   Reparse (Unit_C);
+   Resolve;
+
+   Put_Line ("Performing resolution after reparsing b.txt and b-c.txt...");
+   Reparse (Unit_B);
+   Reparse (Unit_C);
+   Resolve;
+
+   Destroy (Ctx);
+   Put_Line ("main.adb: Done.");
+end Main;
---
+++ testsuite/tests/lexical_envs/ple_after_reparse/test.py
@@ -0,0 +1,110 @@
+"""
+Test that 1) populate lexicale env is done asynchronously after a reparse and
+that 2) semantic analysis still works after that.
+"""
+
+from __future__ import absolute_import, division, print_function
+
+from langkit.dsl import ASTNode, Field, T, abstract
+from langkit.envs import EnvSpec, add_to_env, add_env, do, set_initial_env
+from langkit.expressions import AbstractProperty, New, Self, langkit_property
+from langkit.parsers import Grammar, List, Or, Pick
+
+from lexer_example import Token
+from utils import build_and_run
+
+
+class FooNode(ASTNode):
+    pass
+
+
+@abstract
+class Name(FooNode):
+    resolve = AbstractProperty(T.FooNode.entity, public=True)
+    suffix_symbol = AbstractProperty(T.SymbolType, public=True)
+
+    @langkit_property(return_type=T.AnalysisUnitType, external=True,
+                      uses_entity_info=False, uses_envs=False)
+    def referenced_unit_or_error(or_error=T.BoolType):
+        pass
+
+    @langkit_property(public=True, return_type=T.AnalysisUnitType)
+    def referenced_unit():
+        return Self.referenced_unit_or_error(False)
+
+
+class Id(Name):
+    token_node = True
+
+    @langkit_property()
+    def resolve():
+        return Self.node_env.get_first(Self)
+
+    @langkit_property()
+    def suffix_symbol():
+        return Self.symbol
+
+
+class Prefix(Name):
+    prefix = Field()
+    suffix = Field()
+
+    @langkit_property(public=True)
+    def resolve():
+        return Self.prefix.resolve.children_env.get_first(Self.suffix.symbol)
+
+    @langkit_property()
+    def suffix_symbol():
+        return Self.suffix.symbol
+
+
+class Scope(FooNode):
+    name = Field()
+    deps = Field()
+    defs = Field()
+
+    @langkit_property()
+    def initial_env():
+        return Self.name.match(
+            lambda p=T.Prefix:
+                p.prefix.referenced_unit.root.children_env,
+            lambda _:
+                Self.children_env,
+        )
+
+    env_spec = EnvSpec(
+        set_initial_env(Self.initial_env),
+        add_to_env(New(T.env_assoc, key=Self.name.suffix_symbol, val=Self)),
+        add_env(),
+    )
+
+
+class Dep(FooNode):
+    name = Field()
+
+    env_spec = EnvSpec(do(Self.name.referenced_unit))
+
+
+class Var(FooNode):
+    name = Field()
+    value = Field()
+
+    env_spec = EnvSpec(
+        add_to_env(New(T.env_assoc, key=Self.name.symbol, val=Self)),
+    )
+
+
+G = Grammar('scope')
+G.add_rules(
+    deps=List(Pick(Dep(Pick('+', G.name)), ';'), empty_valid=True),
+
+    defs=List(Pick(Or(G.scope, G.var), ';'), empty_valid=True),
+
+    scope=Scope(G.name, '{', G.deps, G.defs, '}'),
+    var=Var(G.id, '=', G.name),
+
+    id=Id(Token.Identifier),
+    name=Or(Prefix(G.name, '.', G.id), G.id),
+)
+build_and_run(G, ada_main='main.adb')
+print('Done')
