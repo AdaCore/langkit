eb7d51e13e40036f7d31d2c0d59d61d0cad356ea
Pierre-Marie de Rodat <derodat@adacore.com>
2018-04-26 15:05:47 +0200

    Turn whitespaces into trivia in the testsuite reference lexer
    TN: R115-038

--- testsuite/tests/misc/lookup_token/test.py
+++ testsuite/tests/misc/lookup_token/test.py
@@ -5,9 +5,26 @@ Test that sloc-based token lookup works properly.
 from __future__ import absolute_import, division, print_function
 
 from langkit.dsl import ASTNode
+from langkit.lexer import (Eof, Ignore, Lexer, LexerToken, Literal, Pattern,
+                           WithText, WithTrivia)
 from langkit.parsers import Grammar, List
 
 from utils import build_and_run
+
+
+class Token(LexerToken):
+    Example = WithText()
+    Comment = WithTrivia()
+
+
+foo_lexer = Lexer(Token)
+foo_lexer.add_rules(
+    (Pattern(r'[ \n\r\t]+'), Ignore()),
+    (Eof(),                  Token.Termination),
+
+    (Literal('example'),     Token.Example),
+    (Pattern('#(.?)+'), Token.Comment),
+)
 
 
 class FooNode(ASTNode):
@@ -20,5 +37,5 @@ class Example(FooNode):
 
 g = Grammar('main_rule')
 g.add_rules(main_rule=List(Example('example')))
-build_and_run(g, py_script='main.py', ada_main='main.adb')
+build_and_run(g, lexer=foo_lexer, py_script='main.py', ada_main='main.adb')
 print('Done')
