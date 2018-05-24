a682c7b49567c66a4f4d15fe342ecbd3c7bcafea
Pierre-Marie de Rodat <derodat@adacore.com>
2018-05-24 15:54:03 +0200

    properties/memoized_property_error: new testcase
    TN: R524-019

---
+++ testsuite/tests/properties/memoized_property_error/main.adb
@@ -0,0 +1,52 @@
+with Ada.Exceptions; use Ada.Exceptions;
+with Ada.Text_IO;    use Ada.Text_IO;
+
+with Langkit_Support.Diagnostics; use Langkit_Support.Diagnostics;
+
+with Libfoolang.Analysis; use Libfoolang.Analysis;
+
+procedure Main is
+   Ctx          : Analysis_Context := Create;
+   Unit         : constant Analysis_Unit :=
+      Get_From_Buffer (Ctx, "foo.txt", Buffer => "example");
+   Example_Node : Example;
+begin
+   if Has_Diagnostics (Unit) then
+      for D of Diagnostics (Unit) loop
+         Put_Line (To_Pretty_String (D));
+      end loop;
+      raise Program_Error;
+   end if;
+
+   Discard_Errors_In_Populate_Lexical_Env (Ctx, False);
+
+   Example_Node := Root (Unit).As_Example;
+
+   Put_Line ("Calling P_Prop1...");
+   for I in 1 .. 3 loop
+      declare
+         Dummy : Boolean;
+      begin
+         Dummy := Example_Node.P_Prop1;
+      exception
+         when Exc : Property_Error =>
+            Put_Line ("Got an exception!");
+            Put_Line (Exception_Name (Exc) & ": " & Exception_Message (Exc));
+      end;
+   end loop;
+
+   Put_Line ("Calling P_Prop2...");
+   for I in 1 .. 3 loop
+      declare
+         Dummy : Boolean;
+      begin
+         Dummy := Example_Node.P_Prop2;
+      exception
+         when Exc : Property_Error =>
+            Put_Line ("Got an exception!");
+            Put_Line (Exception_Name (Exc) & ": " & Exception_Message (Exc));
+      end;
+   end loop;
+
+   Destroy (Ctx);
+end Main;
