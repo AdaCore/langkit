cf7d194662e30d56db4348e960a0fe6b6f29d3d9
Pierre-Marie de Rodat <derodat@adacore.com>
2018-05-30 11:30:13 +0200

    properties/big_integer: new testcase
    TN: R529-024

---
+++ testsuite/tests/properties/big_integer/main.py
@@ -0,0 +1,25 @@
+from __future__ import absolute_import, division, print_function
+
+print('main.py: Running...')
+
+
+import sys
+
+import libfoolang
+
+
+ctx = libfoolang.AnalysisContext()
+u = ctx.get_from_buffer('main.txt', """
+def a = 10000000000000000000000000000000
+def b = a + 1
+""")
+if u.diagnostics:
+    for d in u.diagnostics:
+        print(d)
+    sys.exit(1)
+
+for decl in u.root:
+    print('{} evaluates to {}'.format(decl.f_name.text,
+                                      decl.f_expr_tree.p_evaluate))
+
+print('main.py: Done.')
