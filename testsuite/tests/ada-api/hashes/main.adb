712783d791af5b493cb5a528b7fcab34f720b4b1
Pierre-Marie de Rodat <derodat@adacore.com>
2018-07-31 14:59:56 +0200

    Make the Hash primitive of nodes useable in hash tables
    TN: R613-024

--- testsuite/tests/ada-api/hashes/main.adb
+++ testsuite/tests/ada-api/hashes/main.adb
@@ -1,5 +1,6 @@
+with Ada.Containers; use Ada.Containers;
 with Ada.Containers.Hashed_Sets;
-with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Text_IO;    use Ada.Text_IO;
 
 with Libfoolang.Analysis; use Libfoolang.Analysis;
 
@@ -15,9 +16,15 @@ procedure Main is
       Hash                => Hash,
       Equivalent_Elements => "=",
       "="                 => "=");
+   package Node_Sets is new Ada.Containers.Hashed_Sets
+     (Element_Type        => Foo_Node,
+      Hash                => Hash,
+      Equivalent_Elements => "=",
+      "="                 => "=");
 
    CS : Context_Sets.Set;
    US : Unit_Sets.Set;
+   NS : Node_Sets.Set;
 
    C : constant Analysis_Context := Create;
    U1 : constant Analysis_Unit := C.Get_From_Buffer (Filename => "main1.txt",
@@ -31,6 +38,16 @@ begin
    US.Insert (U1);
    US.Include (U1);
    US.Insert (U2);
+   if US.Length /= 2 then
+      raise Program_Error;
+   end if;
+
+   NS.Insert (U1.Root);
+   NS.Include (U1.Root);
+   NS.Insert (U2.Root);
+   if NS.Length /= 2 then
+      raise Program_Error;
+   end if;
 
    Put_Line ("Done.");
 end Main;
