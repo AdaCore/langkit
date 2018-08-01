5eeee6997debafadc09c592664fcb52cf0ac3e05
Pierre-Marie de Rodat <derodat@adacore.com>
2018-08-01 16:51:21 +0200

    ada-api/lifetimes: add stale node reference safety checking
    TN: R601-021

--- testsuite/tests/ada-api/lifetimes/main.adb
+++ testsuite/tests/ada-api/lifetimes/main.adb
@@ -1,6 +1,7 @@
 with Ada.Text_IO; use Ada.Text_IO;
 
 with Libfoolang.Analysis; use Libfoolang.Analysis;
+with Libfoolang.Common;   use Libfoolang.Common;
 
 procedure Main is
 begin
@@ -27,6 +28,22 @@ begin
 
       --  When leaving this scope, the context reference that U owns dies, so
       --  the context will be destroyed.
+   end;
+
+   declare
+      U : constant Analysis_Unit := Create.Get_From_Buffer
+        (Filename => "main.txt",
+         Buffer   => "example");
+      R : constant Foo_Node := U.Root;
+   begin
+      U.Reparse ("example");
+      begin
+         Put_Line (R.Short_Image);
+         Put_Line ("Did not get the expected stale reference error");
+      exception
+         when Stale_Reference_Error =>
+            Put_Line ("Got the expected stale reference error");
+      end;
    end;
 
    Put_Line ("Done.");
