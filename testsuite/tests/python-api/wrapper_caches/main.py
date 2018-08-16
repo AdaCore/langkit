f6e6e9ca6d9da701ab838b558fecf79496e1bc78
Pierre-Marie de Rodat <derodat@adacore.com>
2018-08-16 16:14:49 +0200

    python-api/wrapper_caches: enhance testcase for stale refs checks
    TN: R601-021

--- testsuite/tests/python-api/wrapper_caches/main.py
+++ testsuite/tests/python-api/wrapper_caches/main.py
@@ -24,4 +24,25 @@ assert child.parent is root
 assert root[0] is child
 assert root.unit is unit
 
+# Make sure trying to use a stale reference raises an error
+print('Reparsing...')
+unit.reparse('example (example)')
+for name, computation in [
+    ('.parent', lambda n: n.parent),
+    ('[0]', lambda n: n[0]),
+    ('str()', lambda n: str(n))
+]:
+    print('Trying to compute: {}...'.format(name))
+    try:
+        computation(root)
+    except libfoolang.StaleReferenceError as exc:
+        print('   StaleReferenceError raised!')
+    else:
+        print('   No error raised...')
+
+# ... however the equality/hashing methods should not, to allow stale
+# references in dicts/sets to be free'd after the reparse.
+assert isinstance(hash(root), int)
+assert root == root
+
 print('main.py: Done.')
