e76721fdcc082b7196e7f55b00facde2b7da253d
Pierre-Marie de Rodat <derodat@adacore.com>
2018-08-16 11:56:58 +0200

    properties/siblings: new testcase
    TN: R807-033

---
+++ testsuite/tests/properties/siblings/main.py
@@ -0,0 +1,37 @@
+from __future__ import absolute_import, division, print_function
+print('main.py: Running...')
+
+
+import sys
+
+import libfoolang
+
+
+ctx = libfoolang.AnalysisContext()
+u = ctx.get_from_buffer('main.txt', """
+Root(
+    A( a() )
+    B( a() b() )
+    C( a() b() c() )
+)
+""")
+if u.diagnostics:
+    for d in u.diagnostics:
+        print(d)
+    sys.exit(1)
+
+
+def repr_node(n):
+    return n.f_id.text if n else 'None'
+
+
+for l in u.root.f_nodes:
+    print('== {} =='.format(repr_node(l)))
+    for child in l.f_nodes:
+        print('  {} <= {} => {}'.format(repr_node(child.previous_sibling),
+                                        repr_node(child),
+                                        repr_node(child.next_sibling)))
+    print('')
+
+
+print('main.py: Done.')
