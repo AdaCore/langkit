59d378aa6ed25b548c852dfd75f40d9d7889e6fa
Pierre-Marie de Rodat <derodat@adacore.com>
2018-08-31 13:24:54 +0200

    Langkit_Support.Boxes: new package
    TN: R817-011

---
+++ langkit/support/langkit_support-boxes.adb
@@ -0,0 +1,69 @@
+package body Langkit_Support.Boxes is
+
+   --------------------
+   -- Create_Element --
+   --------------------
+
+   function Create_Element return Reference is
+   begin
+      return Ref : Reference do
+         Ref.Internal := new Element_Type;
+         Set_Refcount (Ref.Internal.all, 1);
+      end return;
+   end Create_Element;
+
+   ------------
+   -- Update --
+   ------------
+
+   procedure Update
+     (Self     : Reference;
+      Callback : access procedure (Self : in out Element_Type)) is
+   begin
+      Callback.all (Self.Internal.all);
+   end Update;
+
+   ---------------------
+   -- Internal_Access --
+   ---------------------
+
+   function Internal_Access (Self : Reference) return Element_Access is
+   begin
+      return Self.Internal;
+   end Internal_Access;
+
+   ----------------
+   -- Initialize --
+   ----------------
+
+   overriding procedure Initialize (Self : in out Reference) is
+   begin
+      Self.Internal := null;
+   end Initialize;
+
+   ------------
+   -- Adjust --
+   ------------
+
+   overriding procedure Adjust (Self : in out Reference) is
+      Element : Element_Type renames Self.Internal.all;
+   begin
+      Set_Refcount (Element, Refcount (Element) + 1);
+   end Adjust;
+
+   --------------
+   -- Finalize --
+   --------------
+
+   overriding procedure Finalize (Self : in out Reference) is
+      Next_Count : constant Natural := Refcount (Self.Internal.all);
+   begin
+      if Next_Count = 0 then
+         Release (Self.Internal.all);
+         Free (Self.Internal);
+      else
+         Set_Refcount (Self.Internal.all, Next_Count);
+      end if;
+   end Finalize;
+
+end Langkit_Support.Boxes;
