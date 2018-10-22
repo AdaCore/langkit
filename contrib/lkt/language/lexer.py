3841f8fcb969e8bb9090903a67403c0272e709c8
Raphaël AMIARD <raph.amiard@gmail.com>
2018-10-22 12:51:30 +0200

    RA22-015: First version of the lkt concrete syntax

---
+++ contrib/lkt/language/lexer.py
@@ -0,0 +1,79 @@
+from __future__ import absolute_import, division, print_function
+
+from langkit.lexer import (
+    Lexer, LexerToken, Literal, WithText, WithSymbol, Pattern, Ignore, Eof
+)
+
+
+class Token(LexerToken):
+    Identifier = WithSymbol()
+    String = WithText()
+
+    # Operators
+    ExclMark = WithText()
+    IntMark = WithText()
+    Div = WithText()
+    Comma = WithText()
+    Times = WithText()
+    Plus = WithText()
+    Pipe = WithText()
+    LeftArrow = WithText()
+    Dot = WithText()
+    Comb = WithText()
+    At = WithText()
+    LPar = WithText()
+    RPar = WithText()
+    ListPlus = WithText()
+    ListStar = WithText()
+
+    # Keywords
+    GrammarKw = WithText()
+    IsKw = WithText()
+    EndKw = WithText()
+    OrKw = WithText()
+
+
+lkt_lexer = Lexer(Token)
+
+lkt_lexer.add_patterns(
+    ("STRING_DBQ", r'\"(\\\"|[^\n\"])*\"'),
+    ("STRING_SQ",  r"'(\\'|[^\n'])*'"),
+)
+
+
+lkt_lexer.add_rules(
+    # Whitespace & EOF
+    (Pattern(r"[ \t\r\n\f]+"), Ignore()),
+    (Eof(),                Token.Termination),
+
+    # Operators
+    (Literal('!'),         Token.ExclMark),
+    (Literal('?'),         Token.IntMark),
+    (Literal('/'),         Token.Div),
+    (Literal('*'),         Token.Times),
+    (Literal('+'),         Token.Plus),
+    (Literal('|'),         Token.Pipe),
+    (Literal('<-'),        Token.LeftArrow),
+    (Literal('.'),         Token.Dot),
+    (Literal('('),         Token.LPar),
+    (Literal(')'),         Token.RPar),
+    (Literal('['),         Token.LPar),
+    (Literal('|>'),        Token.Comb),
+    (Literal(']'),         Token.RPar),
+    (Literal(','),         Token.Comma),
+    (Literal('@'),         Token.At),
+    (Literal('list+'),     Token.ListPlus),
+    (Literal('list*'),     Token.ListStar),
+
+    # Keywords
+    (Literal('grammar'),   Token.GrammarKw),
+    (Literal('is'),        Token.IsKw),
+    (Literal('end'),       Token.EndKw),
+    (Literal('or'),        Token.OrKw),
+
+    # Identifiers
+    (Pattern('[a-zA-Z_][a-zA-Z0-9_]*'), Token.Identifier),
+
+    # Strings
+    (Pattern('{STRING_SQ}|{STRING_DBQ}'), Token.String),
+)
---
+++ contrib/lkt/language/parser.py
@@ -0,0 +1,197 @@
+from __future__ import absolute_import, division, print_function
+
+from langkit.dsl import ASTNode, abstract, Field, EnumNode
+from langkit.parsers import Grammar, Or, List, Opt
+
+from language.lexer import lkt_lexer as Lex
+
+
+@abstract
+class LangkitNode(ASTNode):
+    """
+    Root node class for lkt AST nodes.
+    """
+    pass
+
+
+class LangkitRoot(LangkitNode):
+    decls = Field()
+
+
+@abstract
+class Decl(LangkitNode):
+    pass
+
+
+@abstract
+class Expr(LangkitNode):
+    pass
+
+
+class GrammarDecl(Decl):
+    name = Field()
+    rules = Field()
+
+
+class GrammarRuleDecl(Decl):
+    name = Field()
+    expr = Field()
+
+
+@abstract
+class GrammarExpr(Expr):
+    pass
+
+
+class ParseNodeExpr(GrammarExpr):
+    node_name = Field()
+    sub_exprs = Field()
+
+
+class GrammarRuleRef(GrammarExpr):
+    node_name = Field()
+
+
+@abstract
+class Name(Expr):
+    pass
+
+
+class DottedName(Name):
+    prefix = Field()
+    suffix = Field()
+
+
+class Id(Name):
+    token_node = True
+
+
+class TokenLit(GrammarExpr):
+    token_node = True
+
+
+class GrammarToken(GrammarExpr):
+    token_name = Field()
+    expr = Field()
+
+
+class GrammarOrExpr(GrammarExpr):
+    sub_exprs = Field()
+
+
+class GrammarOpt(GrammarExpr):
+    expr = Field()
+
+
+class GrammarOptGroup(GrammarExpr):
+    expr = Field()
+
+
+class GrammarCut(GrammarExpr):
+    pass
+
+
+class GrammarNull(GrammarExpr):
+    name = Field()
+
+
+class GrammarSkip(GrammarExpr):
+    name = Field()
+
+
+class GrammarPredicate(GrammarExpr):
+    expr = Field()
+    prop_ref = Field()
+
+
+class GrammarDontSkip(GrammarExpr):
+    expr = Field()
+    dont_skip = Field()
+
+
+class GrammarList(GrammarExpr):
+    kind = Field()
+    expr = Field()
+    sep = Field()
+
+
+class ListKind(EnumNode):
+    alternatives = ["one", "zero"]
+
+
+lkt_grammar = Grammar('main_rule')
+G = lkt_grammar
+lkt_grammar.add_rules(
+    main_rule=LangkitRoot(
+        List(G.root_decl, empty_valid=True), Lex.Termination
+    ),
+    root_decl=Or(G.grammar_decl),
+    id=Id(Lex.Identifier),
+
+    dotted_name=Or(
+        DottedName(G.dotted_name, ".", G.id),
+        G.id
+    ),
+
+    grammar_decl=GrammarDecl(
+        "grammar", G.id, "is", List(G.grammar_rule, empty_valid=True), "end"
+    ),
+    grammar_rule=GrammarRuleDecl(G.id, "<-", G.grammar_expr),
+    grammar_primary=Or(
+        G.token_literal,
+        G.grammar_cut,
+        G.grammar_skip,
+        G.grammar_null,
+        G.grammar_list_expr,
+        G.grammar_token,
+        G.parse_node_expr,
+        G.grammar_opt,
+        G.grammar_or_expr,
+        G.grammar_rule_ref,
+    ),
+    grammar_expr=Or(
+        GrammarDontSkip(
+            G.grammar_expr,
+            "|>", Lex.Identifier("dont_skip"),
+            "(", G.grammar_expr, ")"
+        ),
+        GrammarPredicate(
+            G.grammar_expr,
+            "|>", Lex.Identifier("when"),
+            "(", G.dotted_name, ")"
+        ),
+        G.grammar_primary
+    ),
+
+    grammar_opt=Or(
+        GrammarOpt("?", G.grammar_expr),
+        GrammarOptGroup("?", "(", List(G.grammar_expr, empty_valid=True), ")"),
+    ),
+    grammar_cut=GrammarCut("/"),
+    grammar_or_expr=GrammarOrExpr(
+        "or", "(",
+        Opt("|"), List(List(G.grammar_expr), sep="|"),
+        ")"
+    ),
+    token_literal=TokenLit(Lex.String),
+    parse_node_expr=ParseNodeExpr(
+        G.id, "(", List(G.grammar_expr, empty_valid=True), ")"
+    ),
+    grammar_rule_ref=GrammarRuleRef(G.id),
+    grammar_list_expr=GrammarList(
+        Or(ListKind.alt_one("list+"), ListKind.alt_zero("list*")),
+        "(", G.grammar_expr, Opt(",", G.grammar_expr), ")",
+    ),
+
+    grammar_skip=GrammarSkip(
+        Lex.Identifier(match_text="skip"), "(", G.id, ")"
+    ),
+
+    grammar_null=GrammarNull(
+        Lex.Identifier(match_text="skip"), "(", G.id, ")"
+    ),
+
+    grammar_token=GrammarToken(
+        "@", G.id, Opt("(", G.token_literal, ")")
+    )
+)
