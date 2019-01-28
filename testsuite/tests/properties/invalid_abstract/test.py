e1c5a2cee62be9da65f62c68b50bc5cd15dcc62c
Pierre-Marie de Rodat <derodat@adacore.com>
2019-01-28 17:37:38 +0100

    properties/invalid_abstract: extend testcase
    TN: S125-014

--- testsuite/tests/properties/invalid_abstract/test.py
+++ testsuite/tests/properties/invalid_abstract/test.py
@@ -2,35 +2,53 @@ from __future__ import absolute_import, division, print_function
 
 from langkit.dsl import ASTNode, T, abstract
 from langkit.expressions import AbstractProperty
-from langkit.parsers import Grammar
+from langkit.parsers import Grammar, List, Or
 
+from lexer_example import Token
 from utils import emit_and_print_errors
 
 
-def run(name, runtime_check):
-    """
-    Emit and print the errors we get for the below grammar with "expr" as
-    a property in BarNode.
-    """
-
+def run(name, runtime_check=False, abstract_root_prop=False):
     print('== {} =='.format(name))
 
-    class FooNode(ASTNode):
-        pass
+    if abstract_root_prop:
+        class FooNode(ASTNode):
+            root_prop = AbstractProperty(T.Bool, public=True)
 
-    @abstract
-    class BaseNode(FooNode):
-        prop = AbstractProperty(T.Bool, public=True)
+        @abstract
+        class BaseNode(FooNode):
+            root_prop = AbstractProperty(T.Bool, runtime_check=True)
 
-    class Example(BaseNode):
-        prop = AbstractProperty(T.Bool, runtime_check=runtime_check)
+        class Number(BaseNode):
+            pass
+
+        class Identifier(BaseNode):
+            pass
+
+    else:
+        class FooNode(ASTNode):
+            pass
+
+        @abstract
+        class BaseNode(FooNode):
+            prop = AbstractProperty(T.Bool, public=True)
+
+        class Number(BaseNode):
+            prop = AbstractProperty(T.Bool, runtime_check=runtime_check)
+
+        class Identifier(BaseNode):
+            prop = AbstractProperty(T.Bool, runtime_check=runtime_check)
 
     grammar = Grammar('main_rule')
-    grammar.add_rules(main_rule=Example('example'))
+    grammar.add_rules(main_rule=List(Or(
+        Number(Token.Number),
+        Identifier(Token.Identifier),
+    )))
     emit_and_print_errors(grammar)
     print('')
 
 
-run('Abstracts overrides without runtime check', False)
-run('Abstracts overrides with runtime check', True)
+run('Abstract overrides without runtime check', runtime_check=False)
+run('Abstract overrides with runtime check', runtime_check=True)
+run('Abstract in root not overriden in list', abstract_root_prop=True)
 print('Done')
