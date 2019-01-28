2cd0766be30420b336e78521510d1b33c3f96051
Pierre-Marie de Rodat <derodat@adacore.com>
2019-01-28 15:50:44 +0100

    properties/entity_bind_2: enhance to test eq prop input sanitization
    TN: S125-014

---
+++ testsuite/tests/properties/entity_bind_2/main.adb
@@ -0,0 +1,43 @@
+with Ada.Exceptions; use Ada.Exceptions;
+with Ada.Text_IO;    use Ada.Text_IO;
+
+with Libfoolang.Analysis; use Libfoolang.Analysis;
+with Libfoolang.Common;   use Libfoolang.Common;
+
+procedure Main is
+   Ctx : constant Analysis_Context := Create_Context;
+   U   : constant Analysis_Unit := Ctx.Get_From_Buffer
+     (Filename => "main.txt", Buffer  => "a 1");
+
+   R     : constant Foo_Node := U.Root;
+   Ident : constant Root_Node := R.Child (1).As_Root_Node;
+   Lit   : constant Root_Node := R.Child (2).As_Root_Node;
+
+   procedure Run (Arg1, Arg2 : Root_Node);
+
+   ---------
+   -- Run --
+   ---------
+
+   procedure Run (Arg1, Arg2 : Root_Node) is
+      Success : Boolean;
+   begin
+      Put_Line ("== " & Arg1.Short_Image
+                & ", " & Arg2.Short_Image & " ==");
+      Success := Lit.As_Literal.P_Solve_Eq (Arg1, Arg2);
+      Put_Line ("   No exception (" & Success'Image & ")");
+   exception
+      when Exc : Property_Error =>
+      Put_Line ("   Property_Error: " & Exception_Message (Exc));
+   end Run;
+
+begin
+   if U.Has_Diagnostics then
+      Put_Line ("Parsing errors...");
+      return;
+   end if;
+   Run (Ident, Ident);
+   Run (Ident, Lit);
+   Run (Lit, Ident);
+   Run (Lit, Lit);
+end Main;
--- testsuite/tests/properties/entity_bind_2/test.py
+++ testsuite/tests/properties/entity_bind_2/test.py
@@ -5,39 +5,58 @@ takes entities.
 
 from __future__ import absolute_import, division, print_function
 
-from langkit.dsl import ASTNode, Bool, Int, LogicVar, T, UserField
-from langkit.expressions import (AbstractProperty, Let, Property, Self, Bind,
+from langkit.dsl import ASTNode, Bool, LogicVar, T, UserField, abstract
+from langkit.expressions import (AbstractKind, Self, Bind, ignore,
                                  langkit_property)
-from langkit.parsers import Grammar
+from langkit.parsers import Grammar, List, Or
 
 from lexer_example import Token
 from utils import build_and_run
 
 
 class FooNode(ASTNode):
-    prop = AbstractProperty(runtime_check=True, type=Int, public=True)
+    pass
 
 
-class Literal(FooNode):
+@abstract
+class RootNode(FooNode):
+
+    var = UserField(LogicVar, public=False)
+
+    @langkit_property(kind=AbstractKind.abstract, return_type=T.Equation)
+    def xref_eq(arg1=T.FooNode.entity, arg2=T.FooNode.entity):
+        pass
+
+    @langkit_property(public=True)
+    def solve_eq(arg1=T.FooNode.entity, arg2=T.FooNode.entity):
+        return Self.xref_eq(arg1, arg2).solve
+
+
+class Identifier(RootNode):
     token_node = True
 
-    a = AbstractProperty(runtime_check=True, type=FooNode.entity)
-    var = UserField(LogicVar, public=False)
+    @langkit_property()
+    def xref_eq(arg1=T.FooNode.entity, arg2=T.FooNode.entity):
+        ignore(arg1, arg2)
+        return Self.var.domain([Self])
+
+
+class Literal(RootNode):
+    token_node = True
 
     @langkit_property(return_type=Bool)
     def is_eq(other=T.Literal.entity):
-        return (Self.as_entity == other)
+        return Self.as_entity == other
 
-    b = Property(Bind(Self.var, Self.a, eq_prop=Self.is_eq))
-
-    @langkit_property(public=True)
-    def public_prop():
-        return Let(lambda _=Self.b: Self.as_bare_entity)
+    @langkit_property()
+    def xref_eq(arg1=T.FooNode.entity, arg2=T.FooNode.entity):
+        return (Self.var.domain([arg1]) &
+                Bind(Self.var, arg2, eq_prop=Self.is_eq))
 
 
 foo_grammar = Grammar('main_rule')
 foo_grammar.add_rules(
-    main_rule=Literal(Token.Number),
+    main_rule=List(Or(Literal(Token.Number), Identifier(Token.Identifier)))
 )
-build_and_run(foo_grammar, 'main.py')
+build_and_run(foo_grammar, ada_main='main.adb')
 print('Done')
