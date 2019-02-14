92c298cca66f1c03579ed687fdb980a6e43eb285
Pierre-Marie de Rodat <derodat@adacore.com>
2019-02-14 10:49:05 +0100

    Reorder imports

--- langkit/compile_context.py
+++ langkit/compile_context.py
@@ -1546,8 +1546,8 @@ class CompileCtx(object):
 
         from langkit.compiled_types import CompiledTypeRepo
         from langkit.envs import EnvSpec
-        from langkit.parsers import Parser
         from langkit.expressions import PropertyDef
+        from langkit.parsers import Parser
         from langkit.passes import (
             ASTNodePass, EnvSpecPass, GlobalPass, GrammarRulePass,
             MajorStepPass, PassManager, PropertyPass, StopPipeline,
@@ -2296,7 +2296,7 @@ class CompileCtx(object):
         """
         from langkit.compiled_types import Argument
         from langkit.expressions import (Entity, FieldAccess, LocalVars, Match,
-                                         Self, construct, PropertyDef)
+                                         PropertyDef, Self, construct)
 
         ignored_props = set()
         redirected_props = {}
