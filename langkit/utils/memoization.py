46f22f8ca364a5060ce95d0d4201cc07fc762d75
Romain Beguet <beguet@adacore.com>
2019-03-29 19:22:11 +0100

    S328-024: Refine implementation to handle all parsers.

--- langkit/utils/memoization.py
+++ langkit/utils/memoization.py
@@ -1,12 +1,15 @@
 from __future__ import absolute_import, division, print_function
 
 
-def memoized(func):
+def memoized(func, pre_cache_miss=None):
     """
     Decorator to memoize a function.
     This function must be passed only hashable arguments.
 
     :param func: The function to decorate.
+    :param pre_cache_miss: The function to call in case of a cache miss, before
+        `func` is called. Can be used for example to prepare a value in the
+        memoization cache to break infinite recursions.
     """
     cache = {}
 
@@ -20,11 +23,30 @@ def memoized(func):
         try:
             result = cache[key]
         except KeyError:
+            if pre_cache_miss is not None:
+                cache[key] = pre_cache_miss(*args, **kwargs)
+
             result = func(*args, **kwargs)
             cache[key] = result
         return result
 
     return wrapper
+
+
+def memoized_with_default(default_value):
+    """
+    Decorator to memoize a function.
+
+    Unlike for the 'memoized' decorator, it prevents infinite recursions by
+    using the provided `default_value`, which will be used as the cached value
+    when an infinite recursion is detected.
+
+    :param default_value: The value to use to break infinite recursions.
+    """
+    def memoize(func):
+        return memoized(func, lambda *args, **kwargs: default_value)
+
+    return memoize
 
 
 def self_memoized(func):
--- testsuite/tests/grammar/opt_info/test.py
+++ testsuite/tests/grammar/opt_info/test.py
@@ -5,7 +5,7 @@ Test that the is_optional property on parse fields works as expected.
 from __future__ import absolute_import, division, print_function
 
 from langkit.dsl import ASTNode, Field
-from langkit.parsers import Grammar, Opt, Or
+from langkit.parsers import Grammar, Opt, Or, Null, DontSkip
 
 from utils import emit_and_print_errors
 
@@ -19,20 +19,65 @@ class Example(FooNode):
 
 
 class ExampleWrapper(FooNode):
-    opt_field = Field()
-    non_opt_field = Field()
+    # Optional fields
+    field_opt = Field()
+    field_or = Field()
+    field_defer = Field()
+    field_null = Field()
+    field_dont_skip = Field()
+
+    # Non optional fields
+    field_opt_bool = Field()
+    field_transform = Field()
+
+
+class HasExample(FooNode):
+    enum_node = True
+    qualifier = True
 
 
 foo_grammar = Grammar('main_rule')
 foo_grammar.add_rules(
     main_rule=Or(foo_grammar.rule_1, foo_grammar.rule_2),
-    rule_1=ExampleWrapper(Opt(Example("example")), Example("example")),
-    rule_2=ExampleWrapper(Example("example"), Example("example"))
+
+    rule_1=ExampleWrapper(
+        Opt(Example("example")),
+        Or(Example("example"), Null(Example)),
+        foo_grammar.sub_rule,
+        Null(Example),
+        DontSkip(Opt(Example("example"))),
+
+        Opt(Example("example")).as_bool(HasExample),
+        Example("example")
+    ),
+    rule_2=ExampleWrapper(
+        Example("example"),
+        Example("example"),
+        Example("example"),
+        Example("example"),
+        Example("example"),
+
+        HasExample("example"),
+        Example("example")
+    ),
+
+    sub_rule=Opt(Example("example"))
 )
 
 emit_and_print_errors(foo_grammar)
 
-for field in (ExampleWrapper.opt_field, ExampleWrapper.non_opt_field):
+fields = [
+    ExampleWrapper.field_opt,
+    ExampleWrapper.field_or,
+    ExampleWrapper.field_defer,
+    ExampleWrapper.field_null,
+    ExampleWrapper.field_dont_skip,
+
+    ExampleWrapper.field_opt_bool,
+    ExampleWrapper.field_transform
+]
+
+for field in fields:
     print("Field {} is {}".format(
         field,
         "optional" if field.is_optional else "not optional"
