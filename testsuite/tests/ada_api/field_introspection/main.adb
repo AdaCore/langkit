a46f8028c9ec127b99927eb6fe729ecc6d1f3b1f
Pierre-Marie de Rodat <derodat@adacore.com>
2019-04-04 12:25:01 +0200

    ada_api/field_introspection: new testcase
    TN: S402-009

---
+++ testsuite/tests/ada_api/field_introspection/main.adb
@@ -0,0 +1,45 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+with Libfoolang.Common;        use Libfoolang.Common;
+with Libfoolang.Introspection; use Libfoolang.Introspection;
+
+procedure Main is
+begin
+   for Id in Node_Type_Id'Range loop
+      Put_Line (DSL_Name (Id) & " (" & Id'Image & ")");
+
+      Put_Line ("   Fields (with abstract ones):");
+      for F of Fields (Id) loop
+         Put_Line ("   field " & Field_Name (F));
+      end loop;
+      New_Line;
+
+      if Is_Concrete (Id) then
+         declare
+            Kind  : constant Foo_Node_Kind_Type := Kind_For (Id);
+         begin
+            Put_Line ("   Fields (only concrete ones):");
+            for F of Fields (Kind) loop
+               if Is_Concrete (Id) then
+                  declare
+                     I : constant Positive := Index (Kind, F);
+                  begin
+                     Put_Line ("   " & Field_Name (F) & ":" & I'Image);
+
+                     --  Make sure Field_Reference_From_Index is consistent
+                     --  with Index.
+
+                     if Field_Reference_From_Index (Kind, I) /= F then
+                        raise Program_Error;
+                     end if;
+                  end;
+               end if;
+            end loop;
+            New_Line;
+         end;
+      end if;
+
+   end loop;
+
+   Put_Line ("Done.");
+end Main;
---
+++ testsuite/tests/ada_api/field_introspection/test.py
@@ -0,0 +1,73 @@
+"""
+Test that the introspection API works as expected for queries related to syntax
+fields.
+"""
+
+from __future__ import absolute_import, division, print_function
+
+from langkit.dsl import AbstractField, ASTNode, Field, NullField, T, abstract
+from langkit.parsers import Grammar, List, Or
+
+from lexer_example import Token
+from utils import build_and_run
+
+
+class FooNode(ASTNode):
+    pass
+
+
+@abstract
+class Decl(FooNode):
+    name = AbstractField(T.Name)
+    value = AbstractField(T.Expr)
+
+
+class VarDecl(Decl):
+    var_kw = Field()
+    name = Field()
+    value = Field()
+
+
+class FunDecl(Decl):
+    name = Field()
+    value = NullField()
+
+
+class VarKeyword(FooNode):
+    token_node = True
+
+
+class Name(FooNode):
+    token_node = True
+
+
+@abstract
+class Expr(FooNode):
+    pass
+
+
+class Number(Expr):
+    token_node = True
+
+
+class Ref(Expr):
+    name = Field()
+
+
+g = Grammar('main_rule')
+g.add_rules(
+    main_rule=List(g.decl),
+
+    decl=Or(g.var_decl, g.fun_decl),
+    var_decl=VarDecl(VarKeyword('var'), g.name, '=', g.expr, ';'),
+    fun_decl=FunDecl('def', g.name, ';'),
+
+    expr=Or(g.number, g.ref),
+    number=Number(Token.Number),
+    ref=Ref(g.name),
+
+    name=Name(Token.Identifier),
+)
+build_and_run(g, ada_main=['main.adb'])
+
+print('Done')
