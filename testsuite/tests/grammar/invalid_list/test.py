af36c2966cc6da48cb1743cb299eebc3b60d04af
Pierre-Marie de Rodat <derodat@adacore.com>
2019-05-23 14:47:42 +0200

    Parsers: reject given list_cls when element type is incompatible
    TN: S507-024

--- testsuite/tests/grammar/invalid_list/test.py
+++ testsuite/tests/grammar/invalid_list/test.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, division, print_function
 
 from langkit.dsl import ASTNode, Annotations, Field
-from langkit.parsers import Grammar, List
+from langkit.parsers import Grammar, List, Or
 
 from lexer_example import Token
 from utils import emit_and_print_errors
@@ -16,6 +16,9 @@ def run(label, **kwargs):
     class ListNode(FooNode):
         items = Field()
 
+    class Example(FooNode):
+        token_node = True
+
     class Num(FooNode):
         token_node = True
 
@@ -23,6 +26,7 @@ def run(label, **kwargs):
         pass
     T.FooNode = FooNode
     T.ListNode = ListNode
+    T.Example = Example
     T.Num = Num
 
     g = Grammar('main_rule')
@@ -37,5 +41,11 @@ run('Token element',
 run('Non-list list_cls',
     num=lambda T, _: T.Num(Token.Number),
     main_rule=lambda T, g: List(g.num, list_cls=T.ListNode))
+
+run('Invalid element type in list_cls',
+    example=lambda T, _: T.Example(Token.Example),
+    num=lambda T, _: T.Num(Token.Number),
+    main_rule=lambda T, g: Or(List(g.num, list_cls=T.Example.list),
+                              T.ListNode(g.example)))
 
 print('Done')
