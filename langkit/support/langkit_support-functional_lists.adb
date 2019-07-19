d2c87324dfada2455f5855a1f667ee9deea4ed1e
Raphaël AMIARD <raph.amiard@gmail.com>
2019-07-19 12:19:53 +0200

    WIP: add copyright headers

--- langkit/support/langkit_support-functional_lists.adb
+++ langkit/support/langkit_support-functional_lists.adb
@@ -1,4 +1,26 @@
-pragma Ada_2012;
+------------------------------------------------------------------------------
+--                                                                          --
+--                                 Langkit                                  --
+--                                                                          --
+--                        Copyright (C) 2019, AdaCore                       --
+--                                                                          --
+-- Langkit is free software; you can redistribute it and/or modify it under --
+-- terms of the  GNU General Public License  as published by the Free Soft- --
+-- ware Foundation;  either version 3,  or (at your option)  any later ver- --
+-- sion.   This software  is distributed in the hope that it will be useful --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY  or  FITNESS  FOR A PARTICULAR PURPOSE.                         --
+--                                                                          --
+-- As a special  exception  under  Section 7  of  GPL  version 3,  you are  --
+-- granted additional  permissions described in the  GCC  Runtime  Library  --
+-- Exception, version 3.1, as published by the Free Software Foundation.    --
+--                                                                          --
+-- You should have received a copy of the GNU General Public License and a  --
+-- copy of the GCC Runtime Library Exception along with this program;  see  --
+-- the files COPYING3 and COPYING.RUNTIME respectively.  If not, see        --
+-- <http://www.gnu.org/licenses/>.                                          --
+------------------------------------------------------------------------------
+
 package body Langkit_Support.Functional_Lists is
 
    package Alloc_List is new Bump.Alloc (List_Node, Node_Ptr);
