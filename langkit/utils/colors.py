e3c6a4f0e3caa5072998bd58ed4f144192ef3942
Raphaël AMIARD <raph.amiard@gmail.com>
2019-08-14 17:33:26 +0200

    langkit.utils.colors: Use built-in term colors

--- langkit/utils/colors.py
+++ langkit/utils/colors.py
@@ -16,12 +16,12 @@ class Colors(object):
     ENDC = '\033[0m'
     BOLD = '\033[1m'
 
-    RED = '\033[91m'
-    GREEN = '\033[92m'
-    YELLOW = '\033[93m'
-    BLUE = '\033[94m'
-    MAGENTA = '\033[95m'
-    CYAN = '\033[96m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    MAGENTA = '\033[35m'
+    CYAN = '\033[36m'
     GREY = '\033[97m'
 
     HEADER = MAGENTA
