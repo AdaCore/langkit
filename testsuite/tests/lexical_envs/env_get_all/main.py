d307b43489ff2b15fe5941ad1a54d01e09122c0c
Romain Beguet <beguet@adacore.com>
2019-09-16 17:16:50 +0200

    S819-034: Improve test case & fix style errors.

--- testsuite/tests/lexical_envs/env_get_all/main.py
+++ testsuite/tests/lexical_envs/env_get_all/main.py
@@ -7,32 +7,46 @@ import libfoolang
 
 print('main.py: Running...')
 
-ctx = libfoolang.AnalysisContext()
-
-
-def test_case(name, content):
-    u = ctx.get_from_buffer(name, content)
+def test_case(content):
+    ctx = libfoolang.AnalysisContext()
+    u = ctx.get_from_buffer('input', content)
     if u.diagnostics:
         for d in u.diagnostics:
             print(d)
         sys.exit(1)
 
     u.populate_lexical_env()
-    print(" ".join(str(x.text) for x in u.root.p_env_get_all))
+    print(' '.join(str(x.p_get_sym) for x in u.root.p_env_get_all))
 
-test_case(
-    'input1',
-    'a b c d e f g h i j k l m n o p q r s t u v w x y z'
-)
 
-test_case(
-    'input2',
-    'k z a b g e h s f i p l w y o q x t j u c d r n m v'
-)
+test_case("""
+def key_a node_a
+def key_b node_b
+def key_c node_c
+def key_d node_d
+def key_e node_e
+""")
 
-test_case(
-    'input3',
-    'z x v t r p n l j h f d b a c e g i k m o q s u w y'
-)
+test_case("""
+def key_d node_d
+def key_b node_b
+def key_a node_a
+def key_c node_c
+def key_e node_e
+""")
+
+test_case("""
+def key_x node_a
+def key_x node_b
+def key_y node_a
+def key_y node_b
+""")
+
+test_case("""
+def key_x node_b
+def key_x node_a
+def key_y node_b
+def key_y node_a
+""")
 
 print('main.py: Done.')
--- testsuite/tests/lexical_envs/env_get_all/test.py
+++ testsuite/tests/lexical_envs/env_get_all/test.py
@@ -1,13 +1,12 @@
 """
-Test add_to_env with multiple env_assoc having different destination
-environments.
+Test the "get all" procedure of lexical envs, and in particular its determinism.
 """
 
 from __future__ import absolute_import, division, print_function
 
-from langkit.dsl import (ASTNode, T, has_abstract_list)
+from langkit.dsl import ASTNode, T, has_abstract_list, synthetic
 from langkit.envs import EnvSpec, add_env, add_to_env_kv
-from langkit.expressions import No, Self, langkit_property
+from langkit.expressions import Field,  No, Self, UserField, langkit_property
 from langkit.parsers import Grammar, List
 
 from lexer_example import Token
@@ -20,14 +19,40 @@ class FooNode(ASTNode):
     def env_get_all():
         return Self.children_env.get(symbol=No(T.Symbol))
 
+    @langkit_property(return_type=T.FooNode, memoized=True)
+    def make_id(s=T.Symbol):
+        return SynthId.new(sym=s)
 
-@has_abstract_list
+    @langkit_property(return_type=T.FooNode)
+    def unique_id(s=T.Symbol):
+        return Self.unit.root.make_id(s)
+
+
+@synthetic
+class SynthId(FooNode):
+    sym = UserField(type=T.Symbol, public=False)
+
+    @langkit_property(return_type=T.String, public=True)
+    def get_sym():
+        return Self.sym.image
+
+
 class Id(FooNode):
     token_node = True
-    env_spec = EnvSpec(add_to_env_kv(Self.symbol, Self))
 
 
-class Program(Id.list):
+@has_abstract_list
+class Insert(FooNode):
+    sym  = Field(type=T.Id)
+    node = Field(type=T.Id)
+
+    env_spec = EnvSpec(add_to_env_kv(
+        Self.sym.symbol,
+        Self.unique_id(Self.node.symbol)
+    ))
+
+
+class Program(Insert.list):
     env_spec=EnvSpec(
         add_env()
     )
@@ -35,8 +60,9 @@ class Program(Id.list):
 
 G = Grammar('main_rule')
 G.add_rules(
-    main_rule=List(G.elem, list_cls=Program),
-    elem=Id(Token.Identifier),
+    main_rule=List(G.insert, list_cls=Program),
+    insert=Insert('def', G.ident, G.ident),
+    ident=Id(Token.Identifier),
 )
 
 build_and_run(G, 'main.py')
