3f4015ffe2a78d3dd0777a8a51a554260b2c5b6f
Pierre-Marie de Rodat <derodat@adacore.com>
2019-10-28 16:09:27 +0100

    properties/invalid_memoized: rework testcase and use tokens, not arrays
    Upcoming changes will add support for arrays in memoized properties, so
    use tokens instead to test the validity check.
    
    TN: SA28-048

--- testsuite/tests/properties/invalid_memoized/test.py
+++ testsuite/tests/properties/invalid_memoized/test.py
@@ -1,37 +1,30 @@
+"""
+Check that property memoization is properly rejected when using unsupported
+argument types.
+"""
+
 from __future__ import absolute_import, division, print_function
 
 from langkit.dsl import ASTNode, T
-from langkit.expressions import Property, Self
+from langkit.expressions import langkit_property
 from langkit.parsers import Grammar
 
 from utils import emit_and_print_errors
 
 
-def run(name, prop):
-    """
-    Emit and print the errors we get for the below grammar with `prop()` as
-    a property in Example.
-    """
-
-    print('== {} =='.format(name))
-
-    class FooNode(ASTNode):
-        pass
-
-    class Example(FooNode):
-        p = prop()
-
-    grammar = Grammar('main_rule')
-    grammar.add_rules(
-        main_rule=Example('example'),
-    )
-    emit_and_print_errors(grammar)
-    print('')
+class FooNode(ASTNode):
+    pass
 
 
-run('Invalid uses entity info',
-    lambda: Property(lambda a=T.entity.array:
-                     a.any(lambda item: item == Self.as_entity),
-                     memoized=True, public=True))
+class Example(FooNode):
+    @langkit_property(public=True, memoized=True)
+    def prop(a=T.Token.array):
+        return a.length == 0
 
+
+grammar = Grammar('main_rule')
+grammar.add_rules(
+    main_rule=Example('example'),
+)
+emit_and_print_errors(grammar)
 print('Done')
