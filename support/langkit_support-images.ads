d8f8f41276c95d965dcc01b84e4d20dbcaa27b48
Raphaël AMIARD <raph.amiard@gmail.com>
2019-11-20 15:39:35 +0100

    SB20-024: add Langkit_Support.Images.Array_Image

--- support/langkit_support-images.ads
+++ support/langkit_support-images.ads
@@ -29,4 +29,16 @@ package Langkit_Support.Images is
    function Stripped_Image (I : Integer) return String;
    --  Return the same as Integer'Image (I), but without any leading space
 
+   generic
+      type T is private;
+      type Idx is (<>);
+      type Array_Type is array (Idx range <>) of T;
+      with function Image (Self : T) return String is <>;
+   function Array_Image
+     (Self : Array_Type; Limit : Positive := 80) return String;
+   --  Return an image for the array, given an image function for elements. The
+   --  array will be represented enclosed in brackets, and elements will be
+   --  separated by colons. If the image is longer than ``Limit``, then some
+   --  wrapping will be applied.
+
 end Langkit_Support.Images;
