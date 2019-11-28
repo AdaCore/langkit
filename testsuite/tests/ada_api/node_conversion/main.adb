925091f4c4d5d6cfbe920a7ef92fec12751b953f
Pierre-Marie de Rodat <derodat@adacore.com>
2019-11-28 11:19:00 +0100

    Enhance error messages for As_* node conversion functions
    TN: SB28-011

---
+++ testsuite/tests/ada_api/node_conversion/main.adb
@@ -0,0 +1,35 @@
+with Ada.Exceptions; use Ada.Exceptions;
+with Ada.Text_IO;    use Ada.Text_IO;
+
+with Libfoolang.Analysis; use Libfoolang.Analysis;
+
+procedure Main is
+   U     : constant Analysis_Unit := Create_Context.Get_From_File ("foo.txt");
+   Decls : Var_Decl_List;
+   R     : Ref;
+begin
+   --  Downcast work only when the destination type is correct
+
+   --  This one works
+   Decls := U.Root.As_Var_Decl_List;
+   Put_Line ("Decls: " & Decls.Short_Image);
+
+   --  This one does not
+   begin
+      R := U.Root.As_Ref;
+      Put_Line ("UNREACHABLE" & R.Short_Image);
+   exception
+      when Exc : Constraint_Error =>
+         Put_Line ("R: Constraint_Error: " & Exception_Message (Exc));
+   end;
+
+   --  Upcasts always work
+
+   declare
+      F : constant Foo_Node := Decls.As_Foo_Node;
+   begin
+      Put_Line ("F: " & F.Short_Image);
+   end;
+
+   Put_Line ("Done.");
+end Main;
