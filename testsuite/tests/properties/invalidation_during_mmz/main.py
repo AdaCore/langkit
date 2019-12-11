b94b5cdd1a937121fc6fe4d2bc07c86b97ce1507
Romain Beguet <beguet@adacore.com>
2019-12-11 10:25:35 +0100

    properties/invalidation_during_mmz: new testcase
    TN: SC09-060

---
+++ testsuite/tests/properties/invalidation_during_mmz/main.py
@@ -0,0 +1,25 @@
+from __future__ import absolute_import, division, print_function
+
+import sys
+
+import libfoolang
+
+
+print('main.py: Running...')
+
+ctx = libfoolang.AnalysisContext()
+u = ctx.get_from_buffer('main.txt', 'example')
+if u.diagnostics:
+    for d in u.diagnostics:
+        print(d)
+    sys.exit(1)
+
+print('root.p_mmz_prop(42) = {}'.format(u.root.p_mmz_prop(42)))
+
+try:
+    result = u.root.p_mmz_prop(0)
+except libfoolang.PropertyError:
+    result = "PropertyError"
+print('root.p_mmz_prop(0)  = {}'.format(result))
+
+print('main.py: Done.')
