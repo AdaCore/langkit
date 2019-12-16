82d9e7c58fb86bc39f703a6ebcce6cad89d734b0
Pierre-Marie de Rodat <derodat@adacore.com>
2019-12-16 15:42:39 +0100

    misc/c_text_to_locale_string: import testcase from Libadalang
    TN: SC05-019

---
+++ testsuite/tests/misc/c_text_to_locale_string/test.py
@@ -0,0 +1,25 @@
+"""
+Test the "text_to_locale_string" C API helper.
+"""
+
+from __future__ import absolute_import, division, print_function
+
+from langkit.dsl import ASTNode
+from langkit.parsers import Grammar
+
+from utils import build_and_run
+
+
+class FooNode(ASTNode):
+    pass
+
+
+class Example(FooNode):
+    token_node = True
+
+
+foo_grammar = Grammar('main_rule')
+foo_grammar.add_rules(main_rule=Example('example'))
+
+build_and_run(foo_grammar, 'main.py')
+print('Done')
