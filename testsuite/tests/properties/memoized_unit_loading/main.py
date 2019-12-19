342118a4171866a53500f270035d21124e98cbec
Pierre-Marie de Rodat <derodat@adacore.com>
2019-12-19 09:24:15 +0100

    properties/memoized_unit_loading: flush to avoid output sync issues
    TN: S917-027

--- testsuite/tests/properties/memoized_unit_loading/main.py
+++ testsuite/tests/properties/memoized_unit_loading/main.py
@@ -5,16 +5,21 @@ import sys
 import libfoolang
 
 
-print('main.py: Running...')
+def pflush(msg):
+    print(msg)
+    sys.stdout.flush()
+
+
+pflush('main.py: Running...')
 
 ctx = libfoolang.AnalysisContext()
 u = ctx.get_from_buffer('main.txt', 'example')
 if u.diagnostics:
     for d in u.diagnostics:
-        print(d)
+        pflush(d)
     sys.exit(1)
 
 for i in range(3):
-    print('root.p_mmz_prop() = {}'.format(u.root.p_mmz_prop))
+    pflush('root.p_mmz_prop() = {}'.format(u.root.p_mmz_prop))
 
-print('main.py: Done.')
+pflush('main.py: Done.')
