d69d8cb6f89e45da02cc67f076ae89dba0469fcf
Pierre-Marie de Rodat <derodat@adacore.com>
2019-12-19 07:33:35 +0100

    $.Iterators: add a Kind_In node predicate
    TN: SC19-006

---
+++ testsuite/tests/ada_api/pred_kind_in/main.adb
@@ -0,0 +1,28 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+with Libfoolang.Analysis;  use Libfoolang.Analysis;
+with Libfoolang.Common;    use Libfoolang.Common;
+with Libfoolang.Iterators; use Libfoolang.Iterators;
+
+procedure Main is
+   U : constant Analysis_Unit := Create_Context.Get_From_Buffer
+     ("foo.txt", Buffer => "var a = 1; var b = a; def f;");
+begin
+   if U.Has_Diagnostics then
+      for D of U.Diagnostics loop
+         Put_Line (U.Format_GNU_Diagnostic (D));
+      end loop;
+      return;
+   end if;
+
+   declare
+      It : constant Traverse_Iterator'Class :=
+         Find (U.Root, Kind_In (Foo_Decl'First, Foo_Decl'Last));
+   begin
+      for N of It.Consume loop
+         Put_Line (N.Short_Image);
+      end loop;
+   end;
+
+   Put_Line ("Done.");
+end Main;
