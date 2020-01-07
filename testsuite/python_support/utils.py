0fca8d5d302be30f08043da5346db7a79e020037
Raphaël AMIARD <raph.amiard@gmail.com>
2020-01-07 15:05:33 +0100

    Minor: fix formatting

--- testsuite/python_support/utils.py
+++ testsuite/python_support/utils.py
@@ -156,19 +156,27 @@ def build_and_run(grammar, py_script=None, ada_main=None, lexer=None,
 
     :param langkit.lexer.Lexer lexer: The lexer to use along with the grammar.
         See emit_and_print_errors.
+
     :param None|str py_script: If not None, name of the Python script to run
         with the built library available.
+
     :param None|str|list[str] ada_main: If not None, list of name of main
         source files for Ada programs to build and run with the generated
         library. If the input is a single string, consider it's a single mail
         source file.
+
     :param None|str ocaml_main: If not None, name of the OCaml source file to
         build and run with the built library available.
+
     :param WarningSet warning_set: Set of warnings to emit.
+
     :param bool generate_unparser: Whether to generate unparser.
+
     :param langkit.compile_context.LibraryEntity|None symbol_canonicalizer:
         Symbol canonicalizer to use for this context, if any.
+
     :param bool mains: Whether to build mains.
+
     :param bool show_property_logging: If true, any property that has been
         marked with tracing activated will be traced on stdout by default,
         without need for any config file.
