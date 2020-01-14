d697a03b27d74edb97a67a6cc92ad590f4943160
Pierre-Marie de Rodat <derodat@adacore.com>
2020-01-14 17:38:10 +0100

    properties/dflt_arg_val_dispatch: new testcase
    TN: RA22-015

---
+++ testsuite/tests/properties/dflt_arg_val_dispatch/main.py
@@ -0,0 +1,25 @@
+from __future__ import absolute_import, division, print_function
+
+import sys
+
+import libfoolang
+
+
+print('main.py: Running...')
+
+ctx = libfoolang.AnalysisContext()
+u = ctx.get_from_buffer('main.txt', b'var a def b')
+if u.diagnostics:
+    for d in u.diagnostics:
+        print(d)
+    sys.exit(1)
+
+for decl in u.root:
+    for args in [(), (True, ), (False, )]:
+        print('{}.p_prop({}) = {}'.format(
+            type(decl).__name__,
+            ', '.join(str(a) for a in args),
+            decl.p_prop(*args),
+        ))
+
+print('main.py: Done.')
