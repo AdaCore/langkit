331d1fc37dc893a122f27eaac9767cfd0bcc43f6
Pierre-Marie de Rodat <derodat@adacore.com>
2020-01-21 15:12:34 +0100

    properties/full_sloc_image: split Python grammar CS testing
    TN: T102-018

---
+++ testsuite/tests/contrib/python_grammar/test.py
@@ -0,0 +1,21 @@
+"""
+RA22-015: check the Python grammar's concrete syntax.
+"""
+
+from __future__ import absolute_import, division, print_function
+ 
+import os
+import sys
+
+from utils import emit_and_print_errors, langkit_root
+ 
+ 
+# Make the Python grammar importable
+sys.path.append(os.path.join(langkit_root, 'contrib', 'python'))
+
+
+from language import lexer, parser
+
+
+emit_and_print_errors(parser.python_grammar, lexer.python_lexer,
+                      unparse_cs=True)
--- testsuite/tests/properties/full_sloc_image/test.py
+++ testsuite/tests/properties/full_sloc_image/test.py
@@ -1,28 +1,23 @@
 """
-This tests that the Langkit based python parser can be compiled and can parse a
-simple python program without errors.
-
-TODO: This is hackish and minimalistic. Since Langkit's python parser is a
-fully fledged Langkit generated lib, it would make sense to reuse Libadalang's
-parser test driver.
+Test that the "full_sloc_image" works as expected.
 """
 
 from __future__ import absolute_import, division, print_function
 
-import os
-from os import path as P
-import sys
+from langkit.dsl import ASTNode
+from langkit.parsers import Grammar
 from utils import build_and_run
 
 
-LK_PYTHON_LIB_DIR = P.join(
-    os.environ['LANGKIT_ROOT_DIR'], 'contrib', 'python'
-)
-sys.path.append(LK_PYTHON_LIB_DIR)
-
-from language import lexer, parser
+class FooNode(ASTNode):
+    pass
 
 
-# RA22-015: This will also dump the python grammar, which is not dumped as part
-# of another test, so keep it.
-build_and_run(parser.python_grammar, "main.py", lexer=lexer.python_lexer,)
+class Example(FooNode):
+    token_node = True
+
+
+g = Grammar('main_rule')
+g.add_rules(main_rule=Example('example'))
+build_and_run(g, 'main.py')
+print('Done')
