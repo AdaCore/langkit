1226d0bd4749f5dfbced1a31a96e0a3f25d4c5a7
Pierre-Marie de Rodat <derodat@adacore.com>
2020-01-23 11:31:34 +0100

    tests/stylechecks: remove trailing whitespaces

--- testsuite/tests/contrib/python_grammar/test.py
+++ testsuite/tests/contrib/python_grammar/test.py
@@ -3,13 +3,13 @@ RA22-015: check the Python grammar's concrete syntax.
 """
 
 from __future__ import absolute_import, division, print_function
- 
+
 import os
 import sys
 
 from utils import emit_and_print_errors, langkit_root
- 
- 
+
+
 # Make the Python grammar importable
 sys.path.append(os.path.join(langkit_root, 'contrib', 'python'))
 
