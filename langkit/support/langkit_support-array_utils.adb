ee88dfb1018558ba7c7c9345378965d641890f39
Pierre-Marie de Rodat <derodat@adacore.com>
2020-02-05 12:29:55 +0100

    Update copyrights for 2020
    TN: R411-040

--- langkit/support/langkit_support-array_utils.adb
+++ langkit/support/langkit_support-array_utils.adb
@@ -2,7 +2,7 @@
 --                                                                          --
 --                                 Langkit                                  --
 --                                                                          --
---                     Copyright (C) 2014-2018, AdaCore                     --
+--                     Copyright (C) 2014-2020, AdaCore                     --
 --                                                                          --
 -- Langkit is free software; you can redistribute it and/or modify it under --
 -- terms of the  GNU General Public License  as published by the Free Soft- --
@@ -19,29 +19,6 @@
 -- copy of the GCC Runtime Library Exception along with this program;  see  --
 -- the files COPYING3 and COPYING.RUNTIME respectively.  If not, see        --
 -- <http://www.gnu.org/licenses/>.                                          --
-------------------------------------------------------------------------------
-
-------------------------------------------------------------------------------
---                             G N A T C O L L                              --
---                                                                          --
---                     Copyright (C) 2015, AdaCore                          --
---                                                                          --
--- This library is free software;  you can redistribute it and/or modify it --
--- under terms of the  GNU General Public License  as published by the Free --
--- Software  Foundation;  either version 3,  or (at your  option) any later --
--- version. This library is distributed in the hope that it will be useful, --
--- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
--- TABILITY or FITNESS FOR A PARTICULAR PURPOSE.                            --
---                                                                          --
--- As a special exception under Section 7 of GPL version 3, you are granted --
--- additional permissions described in the GCC Runtime Library Exception,   --
--- version 3.1, as published by the Free Software Foundation.               --
---                                                                          --
--- You should have received a copy of the GNU General Public License and    --
--- a copy of the GCC Runtime Library Exception along with this program;     --
--- see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see    --
--- <http://www.gnu.org/licenses/>.                                          --
---                                                                          --
 ------------------------------------------------------------------------------
 
 package body Langkit_Support.Array_Utils is
