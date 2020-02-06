8f9c813e4f4497c172c11f7b3f7ad74439c21820
Raphaël AMIARD <raph.amiard@gmail.com>
2020-02-06 11:40:37 +0100

    RA22-015: Add lkt_syntax test, with ambiguous testcase for blocks

---
+++ testsuite/tests/lkt_syntax/main.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import, division, print_function
+import libfoolang
+
+
+print('main.py: Starting...')
+
+ctx = libfoolang.AnalysisContext()
+unit = ctx.get_from_file('leading_trivia.txt')
+print(unit.lookup_token(libfoolang.Sloc(3, 1)))
+
+print('main.py: Done.')
+print('')
