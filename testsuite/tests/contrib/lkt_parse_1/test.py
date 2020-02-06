497e2e2d27f5206d72dc5c72d047a7085fcb1b9d
Raphaël AMIARD <raph.amiard@gmail.com>
2020-02-06 11:47:39 +0100

    RA22-015: Remove whitelist from lkt_parse test

--- testsuite/tests/contrib/lkt_parse_1/test.py
+++ testsuite/tests/contrib/lkt_parse_1/test.py
@@ -28,45 +28,7 @@ tests = sorted(((P.join(root, f), P.basename(root))
                 for f in files
                 if f == 'expected_concrete_syntax.lkt'), key=lambda x: x[1])
 
-# TODO: for the moment we'll use a whitelist for tests, eventually we want to
-# parse them all.
-test_whitelist = {
-    'abstract_fields', 'add_to_env_foreign', 'add_to_env_mult_dests',
-    'analysis_unit', 'analysis_unit_from_node', 'array_types',
-    'auto_ple_dispatcher', 'auto_populate', 'bare_lexing', 'big_integer',
-    'c_text_to_locale_string', 'can_reach', 'categories', 'character', 'cond',
-    'custom_parsing_rule', 'dflt_arg_val', 'dflt_arg_val_dispatch',
-    'dflt_arg_val_predicate', 'dflt_field_val', 'domain_derived',
-    'dynvar_bind', 'dynvars_dflt', 'early_binding_error', 'entity_bind',
-    'entity_bind_2', 'entity_bind_3', 'entity_cast', 'entity_eq',
-    'entity_field_access', 'entity_length', 'entity_map', 'entity_match',
-    'entity_resolver', 'enum_node_inherit', 'enum_types', 'env_get_all',
-    'equality', 'exposed_bare_nodes', 'external', 'field_introspection',
-    'foreign_env_md', 'full_sloc_image', 'general', 'ghost_nodes', 'hashes',
-    'if_then', 'import_argcount', 'indent_trivia', 'invalidation_during_mmz',
-    'is_a', 'is_visible_from', 'let_underscore', 'lifetimes', 'logging',
-    'lookup_token', 'lower_dispatch', 'lower_dispatch_null',
-    'lower_dispatch_rewrite', 'map_index', 'match', 'memoized_big_table',
-    'memoized_env', 'memoized_inf_recurs', 'memoized_property_array_arg',
-    'memoized_property_error', 'memoized_unit', 'memoized_unit_loading', 'neq',
-    'new_node', 'newline', 'node_comparison', 'node_conversion',
-    'node_env_concrete_subclass', 'node_env_empty', 'node_negative_index',
-    'node_none_check', 'node_type_introspection', 'null_checks',
-    'null_list_get', 'null_node_prop', 'ple_after_reparse', 'ple_resilience',
-    'ple_resilience_2', 'ple_subunits', 'ple_subunits_2', 'populate_error',
-    'pred_kind_in', 'private_predicate_props', 'properties_introspection',
-    'property_error', 'public', 'qualifier_sloc_range', 'rebindings',
-    'ref_after_reparse', 'rewriting', 'siblings', 'stack_overflow',
-    'struct_of_array', 'struct_update', 'symbol_type', 'synthetic_props',
-    'then_analysis_unit', 'tokens', 'top_level_predicate', 'trailing_garbage',
-    'try', 'unbounded_string_buffer', 'unicode_buffer', 'unique', 'unit_canon',
-    'unit_filename', 'unparse_empty_list', 'unparse_no_canon',
-    'unparse_or_skip', 'wrapper_caches'
-}
-
-whitelisted_tests = [t for t in tests if t[1] in test_whitelist]
-
-for full_syntax_path, test_name in whitelisted_tests:
+for full_syntax_path, test_name in tests:
     header = 'Parsing concrete syntax for test {}'.format(test_name)
     print("{}\n{}\n".format(header, "=" * len(header)))
     sys.stdout.flush()
