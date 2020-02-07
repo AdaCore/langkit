431a759231e57b7da0e07485045b783cad4356db
Raphaël AMIARD <raph.amiard@gmail.com>
2020-02-07 12:43:51 +0100

    RA22-015: Add test for .contains

---
+++ testsuite/tests/properties/contains/main.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import, division, print_function
+
+import sys
+
+import libfoolang
+
+
+print('main.py: Running...')
+
+ctx = libfoolang.AnalysisContext()
+u = ctx.get_from_buffer('main.txt', b'1 2 3 4')
+if u.diagnostics:
+    for d in u.diagnostics:
+        print(d)
+    sys.exit(1)
+
+print('root.p_prop = {}'.format(u.root.p_prop))
+
+print('main.py: Done.')
---
+++ testsuite/tests/properties/contains/test.py
@@ -0,0 +1,36 @@
+from __future__ import absolute_import, division, print_function
+
+from langkit.dsl import ASTNode, Field
+from langkit.expressions import Property, Self
+from langkit.parsers import Grammar, List
+
+from lexer_example import Token
+from utils import build_and_run
+
+
+class FooNode(ASTNode):
+    pass
+
+
+class ListNode(FooNode):
+    nb_list = Field()
+    prop = Property(
+        Self.nb_list.map(lambda i, _: i).contains(1),
+        public=True
+    )
+
+
+class NumberNode(FooNode):
+    token_node = True
+
+
+foo_grammar = Grammar('main_rule')
+foo_grammar.add_rules(
+    main_rule=ListNode(
+        List(NumberNode(Token.Number))
+    ),
+)
+
+
+build_and_run(foo_grammar, 'main.py')
+print('Done')
