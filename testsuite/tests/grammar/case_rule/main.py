c86c7ab11c00446d4ef888e359afbe9014a75c6a
Pierre-Marie de Rodat <derodat@adacore.com>
2020-03-06 05:55:54 -0500

    grammar/case_rule: new testcase
    TN: RA22-015

---
+++ testsuite/tests/grammar/case_rule/main.py
@@ -0,0 +1,26 @@
+from __future__ import absolute_import, division, print_function
+
+import libfoolang
+
+
+print('main.py: Running...')
+
+ctx = libfoolang.AnalysisContext()
+
+for label, text in (
+    ('single-char', "'c'"),
+    ('simple-attr', "a'b"),
+    ('char-dot', "'a'.b"),
+    ('id-char', "a'b'"),
+):
+    print('== {} =='.format(label))
+    u = ctx.get_from_buffer('{}.txt'.format(label), text)
+    if u.diagnostics:
+        for d in u.diagnostics:
+            print(d)
+        print('--')
+    for t in u.iter_tokens():
+        print(t)
+    print('')
+
+print('main.py: Done.')
