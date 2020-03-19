86e6595f60be5714463577d3b41ec74bb87f31cb
Romain Beguet <beguet@adacore.com>
2020-03-19 18:03:46 +0100

    T218-020: Avoid creating array of children during PLE.
    Because returning this array from the `Children` function will fail when
    this array is too big.

---
+++ testsuite/tests/lexical_envs/ple_many_children/main.py
@@ -0,0 +1,19 @@
+import libfoolang
+
+import sys
+
+
+print('main.py: Running...')
+
+ctx = libfoolang.AnalysisContext()
+u = ctx.get_from_buffer('main.txt', b"""example """ * 5000000)
+if u.diagnostics:
+    for d in u.diagnostics:
+        print(d)
+    sys.exit(1)
+
+print('main.py: Populating lexical envs...')
+
+u.populate_lexical_env()
+
+print('main.py: Done')
