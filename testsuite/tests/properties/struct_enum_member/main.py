fc66aec04edfb74388c270265478a91e48e7811e
Raphaël AMIARD <raph.amiard@gmail.com>
2020-04-02 16:34:24 +0200

    T402-029: add test for enum in struct

---
+++ testsuite/tests/properties/struct_enum_member/main.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import, division, print_function
+
+import sys
+
+import libfoolang
+
+
+print('main.py: Running...')
+
+ctx = libfoolang.AnalysisContext()
+u = ctx.get_from_buffer('main.txt', b'def a = 1; def b (x) = 2;')
+if u.diagnostics:
+    for d in u.diagnostics:
+        print(d)
+    sys.exit(1)
+
+r = u.root
+for d in r:
+    print('{}.with_kind = {}'.format(d, d.p_with_kind))
+
+print('main.py: Done.')
