2bca15bf94f772ec06d13bfc60fdd1ce4bddb136
Pierre-Marie de Rodat <derodat@adacore.com>
2020-04-22 15:00:03 +0200

    grammar/precise_types: new testcase
    TN: T116-038

---
+++ testsuite/tests/grammar/precise_types/test.py
@@ -0,0 +1,48 @@
+"""
+Check precise types for parse fields.
+"""
+
+from __future__ import absolute_import, division, print_function
+
+from langkit.dsl import ASTNode, Field, T
+
+from utils import emit_and_print_errors
+
+
+class FooNode(ASTNode):
+    pass
+
+
+class A(FooNode):
+    token_node = True
+
+
+class B(FooNode):
+    token_node = True
+
+
+class SpecialList(FooNode.list):
+    pass
+
+
+class Holder(FooNode):
+    a_or_b = Field(type=T.FooNode)
+    a_or_b_list = Field(type=T.FooNode.list)
+    a_list_or_b_list = Field(type=T.FooNode)
+    special_a_list_or_special_b_list = Field(type=SpecialList)
+    special_a_or_b_list = Field(type=SpecialList)
+
+
+ctx = emit_and_print_errors(lkt_file='foo.lkt')
+for n in ctx.astnode_types:
+    if n.dsl_name == 'Holder':
+        node = n
+        break
+
+fields = {f.original_name.lower: f for f in node.get_fields()}
+for _, f in sorted(fields.items()):
+    print('== Doc for {} =='.format(f.qualname))
+    print(f.doc)
+    print('')
+
+print('Done')
