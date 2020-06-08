bf5c12e4b289f94fe66abbb0fbe183811b578f87
Pierre-Marie de Rodat <derodat@adacore.com>
2020-06-08 11:33:31 +0200

    Fix handling of invalid env kind in add_to_env
    So far, calling add_to_env on a non-primary lexical environment was
    crashing with a Constraint_Error. Raise a proper Property_Error instead
    in this case.
    
    TN: T604-031

---
+++ testsuite/tests/lexical_envs/add_to_env_nonprimary/test.py
@@ -0,0 +1,33 @@
+"""
+Check that add_to_env on a non-primary environment raises a Property_Error. It
+used to crash with a Constraint_Error.
+"""
+
+from langkit.dsl import ASTNode
+from langkit.envs import EnvSpec, add_env, add_to_env
+from langkit.expressions import Self, new_env_assoc
+
+from utils import build_and_run
+
+
+class FooNode(ASTNode):
+    pass
+
+
+class Example(FooNode):
+    token_node = True
+
+    env_spec = EnvSpec(
+        add_env(),
+        add_to_env(
+            new_env_assoc(
+                key="foo",
+                val=Self,
+                dest_env=Self.children_env.env_orphan
+            )
+        )
+    )
+
+
+build_and_run(lkt_file='expected_concrete_syntax.lkt', py_script='main.py')
+print('Done')
