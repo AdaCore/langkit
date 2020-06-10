f12af1e5424f42c8b1f97296effa479ff5fe1961
Pierre-Marie de Rodat <derodat@adacore.com>
2020-06-10 16:25:11 +0200

    Lkt lowering: handle ASTList[T] type references
    TN: RA22-015

--- testsuite/tests/grammar/indent_trivia/test.py
+++ testsuite/tests/grammar/indent_trivia/test.py
@@ -34,5 +34,5 @@ class Newline(FooNode):
 
 
 build_and_run(lkt_file='expected_concrete_syntax.lkt', py_script='main.py',
-              unparse_script=unparse_all_script)
+              unparse_script=unparse_all_script, types_from_lkt=True)
 print('Done')
