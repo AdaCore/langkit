f3f20ebbd8d5eb544ce41e0db9238fbcc470df87
Pierre-Marie de Rodat <derodat@adacore.com>
2020-08-21 10:18:03 +0200

    Prevent reference from writing to foreign envs by default
    This is similar to what we just did for set_initial_env and add_to_env.
    
    TN: T320-010

---
+++ testsuite/tests/lexical_envs/ref_env_foreign/main.py
@@ -0,0 +1,47 @@
+import sys
+
+import libfoolang
+
+
+print('main.py: Running...')
+
+ctx = libfoolang.AnalysisContext()
+ctx.discard_errors_in_populate_lexical_env(False)
+
+
+def process(name, buffer):
+    print('== {} =='.format(name))
+
+    u = ctx.get_from_buffer(name, buffer)
+    if u.diagnostics:
+        for d in u.diagnostics:
+            print('{}:{}'.format(name, d))
+        sys.exit(1)
+
+    try:
+        u.populate_lexical_env()
+    except libfoolang.PropertyError as exc:
+        print('   Got a {}: {}'.format(type(exc).__name__, exc))
+    else:
+        print('   All good...')
+    print('')
+
+
+# This deals with only local nodes and environments: there is no error here
+process('foo', b"""
+    def foo {
+    }
+
+    def bar {
+        +foo
+    }
+""")
+
+# Adding a reference to a foreign unit's lexical env is not allowed by default
+process('ref_to_foreign', b"""
+    def baz {
+        +foo
+    }
+""")
+
+print('main.py: Done.')
