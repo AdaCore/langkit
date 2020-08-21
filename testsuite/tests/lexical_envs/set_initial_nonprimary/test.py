14b6a1f0d40d6e02df0f36bd85d865802a6ad7bb
Pierre-Marie de Rodat <derodat@adacore.com>
2020-08-21 09:58:38 +0200

    PLE: forbid non-primary initial environments
    PLE is supposed to create only primary environments, so it is not
    correct to have a parent environment that is non-primary.
    
    TN: T320-010

---
+++ testsuite/tests/lexical_envs/set_initial_nonprimary/test.py
@@ -0,0 +1,26 @@
+"""
+Check that set_initial_env trying to set a non-primary environment raises a
+Property_Error.
+"""
+
+from langkit.dsl import ASTNode
+from langkit.envs import EnvSpec, set_initial_env
+from langkit.expressions import Self
+
+from utils import build_and_run
+
+
+class FooNode(ASTNode):
+    pass
+
+
+class Example(FooNode):
+    token_node = True
+
+    env_spec = EnvSpec(
+        set_initial_env(Self.parent.children_env.env_orphan),
+    )
+
+
+build_and_run(lkt_file='expected_concrete_syntax.lkt', py_script='main.py')
+print('Done')
