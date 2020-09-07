cbd21c6d14ca78f10358dccbdd4b6382d2ecd03e
Raphaël AMIARD <raph.amiard@gmail.com>
2020-09-07 16:52:38 +0200

    Add langkit_support vector get/pop test

---
+++ testsuite/tests/langkit_support/vectors_bounds_checks/main.adb
@@ -0,0 +1,61 @@
+with Ada.Text_IO;              use Ada.Text_IO;
+with System.Assertions;
+
+with Langkit_Support.Vectors;
+
+procedure Main is
+
+   package Int_Vectors is new Langkit_Support.Vectors (Integer);
+   procedure Put (V : Int_Vectors.Vector);
+
+   ---------
+   -- Put --
+   ---------
+
+   procedure Put (V : Int_Vectors.Vector) is
+   begin
+      Put ('[');
+      for I in V.First_Index .. V.Last_Index loop
+         if I > V.First_Index then
+            Put (", ");
+         end if;
+         Put (Integer'Image (V.Get (I)));
+      end loop;
+      Put (']');
+      New_Line;
+   end Put;
+
+   V : Int_Vectors.Vector;
+
+begin
+   Put_Line ("Empty vector");
+   Put (V);
+
+   Put_Line ("Add 5 elements");
+   for I in 1 .. 5 loop
+      V.Append (I);
+   end loop;
+   Put (V);
+
+   declare
+      Tmp : Integer;
+   begin
+      Tmp := V.Get (6);
+   exception
+      when Constraint_Error =>
+         Put_Line ("Out of bound access");
+   end;
+
+   declare
+      Tmp : Integer;
+   begin
+      for I in 1 .. 6 loop
+         Tmp := V.Pop;
+      end loop;
+   exception
+      when System.Assertions.Assert_Failure =>
+         Put_Line ("Out of bound access");
+   end;
+
+   V.Destroy;
+end Main;
