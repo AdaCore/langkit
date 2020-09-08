cb028566ff72dcf2b0a5852195d8004b986d2612
Pierre-Marie de Rodat <derodat@adacore.com>
2020-09-08 15:29:36 +0200

    Forbid node synthetization when Self.Self_Env is foreign
    TN: T319-033

---
+++ testsuite/tests/properties/new_node_foreign/main.py
@@ -0,0 +1,31 @@
+import sys
+
+import libfoolang
+
+
+print('main.py: Running...')
+
+ctx = libfoolang.AnalysisContext()
+ctx.discard_errors_in_populate_lexical_env(False)
+
+
+def load(filename, buffer):
+    u = ctx.get_from_buffer(filename, buffer)
+    if u.diagnostics:
+        for d in u.diagnostics:
+            print(d)
+        sys.exit(1)
+    return u
+
+
+root = load('root.txt', 'def root {}')
+root.populate_lexical_env()
+
+foo = load('foo.txt', 'root {}')
+print('Evaluating .f_synth...')
+try:
+    print('->', foo.root[0].f_synth)
+except libfoolang.PropertyError as exc:
+    print('Got a property error: {}'.format(exc))
+
+print('main.py: Done.')
---
+++ testsuite/tests/properties/new_node_foreign/test.py
@@ -0,0 +1,46 @@
+"""
+Check that node synthetization fails as expected when the Self.Self_Env is a
+foreign env.
+"""
+
+from langkit.compiled_types import T
+from langkit.dsl import ASTNode, Field, synthetic
+from langkit.envs import EnvSpec, add_env, set_initial_env_by_name
+from langkit.expressions import No, Self, lazy_field
+
+from utils import build_and_run
+
+
+class FooNode(ASTNode):
+    pass
+
+
+@synthetic
+class SynthNode(FooNode):
+    id = Field(type=T.Identifier)
+
+
+class Identifier(FooNode):
+    token_node = True
+
+
+class DefBlock(FooNode):
+    id = Field(type=T.Identifier)
+
+    env_spec = EnvSpec(add_env(names=[Self.id.symbol]))
+
+
+class RegularBlock(FooNode):
+    id = Field(type=T.Identifier)
+
+    env_spec = EnvSpec(set_initial_env_by_name(
+        Self.id.symbol, No(T.LexicalEnv)
+    ))
+
+    @lazy_field(public=True)
+    def synth():
+        return T.SynthNode.new(id=Self.id)
+
+
+build_and_run(lkt_file='expected_concrete_syntax.lkt', py_script='main.py')
+print('Done')
