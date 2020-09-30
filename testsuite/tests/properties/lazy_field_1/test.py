abe8f47579aa07d2715c52e305609f284b651d92
Pierre-Marie de Rodat <derodat@adacore.com>
2020-09-30 16:30:22 +0200

    Fix wrong assumption in codegen for node field initializers
    The computation of fields from the parent node assumed so far that in
    the list of fields, parent fields came first and only then fields for
    the current node. This assumption is wrong, because user fields can be
    added to the parent's list of fields after the current node is assigned
    its own fields.
    
    This commit removes this assumption, switching from list indexes to set
    operations in order to compute parent fields.
    
    TN: T319-033

---
+++ testsuite/tests/properties/lazy_field_1/test.py
@@ -0,0 +1,34 @@
+"""
+Regression test. We used to generate bad Ada code for the initializer of a
+class for which one of the ancestor has a lazy field.
+"""
+
+from langkit.compiled_types import T
+from langkit.dsl import ASTNode, Field, abstract
+from langkit.expressions import lazy_field
+
+from utils import build_and_run
+
+
+class FooNode(ASTNode):
+    pass
+
+
+@abstract
+class Expr(FooNode):
+    @lazy_field(public=True)
+    def my_field():
+        return 42
+
+
+class IntLit(Expr):
+    token_node = True
+
+
+class AddExpr(Expr):
+    left = Field(type=T.Expr)
+    right = Field(type=T.Expr)
+
+
+build_and_run(lkt_file='expected_concrete_syntax.lkt', ada_main='main.adb')
+print('Done')
