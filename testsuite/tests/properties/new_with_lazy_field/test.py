c958e37e85fa33e0eeae1344cdad92d33e8002e2
Pierre-Marie de Rodat <derodat@adacore.com>
2020-10-12 13:50:43 +0200

    Fix synthetization of nodes that have lazy fields
    TN: T319-033

---
+++ testsuite/tests/properties/new_with_lazy_field/test.py
@@ -0,0 +1,37 @@
+"""
+Check that the synthetization of a node that involves a lazy field works as
+expected.
+"""
+
+from langkit.compiled_types import T
+from langkit.dsl import ASTNode, synthetic
+from langkit.expressions import Self, langkit_property, lazy_field
+
+from utils import build_and_run
+
+
+class FooNode(ASTNode):
+    pass
+
+
+class Example(FooNode):
+
+    @lazy_field(return_type=T.SynthNode)
+    def new_node():
+        return T.SynthNode.new()
+
+    @langkit_property(return_type=T.Int, public=True)
+    def prop():
+        return Self.new_node.lf
+
+
+@synthetic
+class SynthNode(FooNode):
+
+    @lazy_field(return_type=T.Int)
+    def lf():
+        return 42
+
+
+build_and_run(lkt_file='expected_concrete_syntax.lkt', py_script='main.py')
+print('Done')
