ea427e069c335a411b0e72eb21dae676d26d220d
Pierre-Marie de Rodat <derodat@adacore.com>
2021-01-08 17:51:29 +0100

    lexical_envs/check_rebindable: new testcase
    TN: T227-041

---
+++ testsuite/tests/lexical_envs/check_rebindable/main.py
@@ -0,0 +1,28 @@
+import sys
+
+import libfoolang
+
+
+print("main.py: Running...")
+
+ctx = libfoolang.AnalysisContext()
+u = ctx.get_from_buffer("main.txt", buffer=b"""
+    def example example
+    def example +example
+    def +example example
+    def +example +example
+""")
+if u.diagnostics:
+    for d in u.diagnostics:
+        print("{}:{}".format(u.filename, d))
+    sys.exit(1)
+
+for n in u.root:
+    print("== {} ==".format(n))
+    try:
+        print(n.p_rebind)
+    except libfoolang.PropertyError as exc:
+        print("Exception:", exc)
+    print("")
+
+print("main.py: Done.")
