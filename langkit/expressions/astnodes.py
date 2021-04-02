2f31221e8253067abcfa706df867e8824de458e9
Pierre-Marie de Rodat <derodat@adacore.com>
2021-04-02 10:07:01 +0200

    langkit.expressions.astnodes: refactor and add type hints
    TN: U329-073

--- langkit/expressions/astnodes.py
+++ langkit/expressions/astnodes.py
@@ -1,22 +1,30 @@
-from langkit.compiled_types import T
+from typing import Callable, List, Optional
+
+from langkit.compiled_types import AbstractNodeData, T
 from langkit.diagnostics import check_source_language
-from langkit.expressions.base import (CallExpr, FieldAccessExpr, auto_attr,
-                                      construct)
+from langkit.expressions.base import (
+    AbstractExpression, CallExpr, FieldAccessExpr, ResolvedExpression,
+    auto_attr, construct
+)
 from langkit.expressions.structs import FieldAccess
 
 
-def get_builtin_field(name):
+def get_builtin_field(name: str) -> AbstractNodeData:
     """
     Fetch a builtin field/property in the root AST node.
 
-    :param str name: Name of the builtin to fetch.
-    :rtype: langkit.compiled_types.AbstractNodeData
+    :param name: Name of the builtin to fetch.
     """
     return T.root_node.get_abstract_node_data_dict()[name]
 
 
-def build_field_access(self, node_expr, builtin_field_name,
-                       bare_node_expr_constructor):
+def build_field_access(
+    node_expr: ResolvedExpression,
+    builtin_field_name: str,
+    args: List[Optional[ResolvedExpression]],
+    bare_node_expr_constructor: Callable[[], ResolvedExpression],
+    abstract_expr: Optional[AbstractExpression],
+) -> ResolvedExpression:
     """
     Helper for abstract expressions below. Return a resolved expression to
     evaluate either `node_expr`'s builtin property `field_name` (if `node_expr`
@@ -27,19 +35,21 @@ def build_field_access(self, node_expr, builtin_field_name,
     must return the same type as its input, while the property always returns
     an entity.
 
-    :param langkit.expressions.ResolvedExpression node_expr: Expression for the
-        input node/entity.
-    :param str builtin_field_name: Name of the builtin field to access.
+    :param prefix: Expression for the input node/entity.
+    :param builtin_field_name: Name of the builtin field to access.
+    :param args: Arguments for the field access.
     :param bare_node_expr_constructor: Callback used to build the expression
         that computes the field access in the case we have a bare node input.
-    :rtype: langkit.expressions.ResolvedExpression
+    :param abstract_expr: Abstract expression corresponding to this field
+        access.
     """
     if node_expr.type.is_entity_type:
         return FieldAccess.Expr(
-            node_expr, get_builtin_field(builtin_field_name), [],
-            implicit_deref=True, abstract_expr=self)
+            node_expr, get_builtin_field(builtin_field_name), args,
+            implicit_deref=True, abstract_expr=abstract_expr
+        )
     else:
-        return bare_node_expr_constructor(node_expr, abstract_expr=self)
+        return bare_node_expr_constructor()
 
 
 @auto_attr
@@ -61,10 +71,13 @@ def parent(self, node):
     )
 
     return build_field_access(
-        self, node_expr, 'parent',
-        lambda node_expr, abstract_expr: FieldAccessExpr(
+        node_expr, 'parent', [],
+        lambda: FieldAccessExpr(
             node_expr, 'Parent', T.root_node,
-            do_explicit_incref=False, abstract_expr=self))
+            do_explicit_incref=False, abstract_expr=self
+        ),
+        abstract_expr=self,
+    )
 
 
 @auto_attr
@@ -87,10 +100,13 @@ def parents(self, node):
     )
 
     return build_field_access(
-        self, node_expr, 'parents',
-        lambda node_expr, abstract_expr: CallExpr(
-            'Node_Parents', 'Parents', T.root_node.array, [node_expr],
-            abstract_expr=self))
+        node_expr, 'parents', [],
+        lambda: CallExpr(
+            'Node_Parents', 'Parents', T.root_node.array,
+            [node_expr], abstract_expr=self,
+        ),
+        abstract_expr=self,
+    )
 
 
 @auto_attr
@@ -108,7 +124,10 @@ def children(self, node):
     )
 
     return build_field_access(
-        self, node_expr, 'children',
-        lambda node_expr, abstract_expr: CallExpr(
+        node_expr, 'children', [],
+        lambda: CallExpr(
             'Node_Children', 'Children', T.root_node.array, [node_expr],
-            abstract_expr=self))
+            abstract_expr=self
+        ),
+        abstract_expr=self,
+    )
