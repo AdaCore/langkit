0fd8d9887ade6732f08af0dfbc18a808e7a148c1
Pierre-Marie de Rodat <derodat@adacore.com>
2021-04-14 18:04:16 +0200

    Langkit_Support.Slocs: add Value (string parsing) functions
    For GitHub issue #492

--- support/langkit_support-slocs.ads
+++ support/langkit_support-slocs.ads
@@ -48,6 +48,10 @@ package Langkit_Support.Slocs is
    No_Source_Location       : constant Source_Location       := (0, 0);
    No_Source_Location_Range : constant Source_Location_Range := (0, 0, 0, 0);
 
+   --------------------------
+   -- Constructors/getters --
+   --------------------------
+
    function Start_Sloc
      (Sloc_Range : Source_Location_Range) return Source_Location
    is ((Line => Sloc_Range.Start_Line, Column => Sloc_Range.Start_Column));
@@ -62,6 +66,10 @@ package Langkit_Support.Slocs is
         End_Line     => End_Sloc.Line,
         Start_Column => Start_Sloc.Column,
         End_Column   => End_Sloc.Column));
+
+   --------------------------
+   -- Location comparisons --
+   --------------------------
 
    function Compare
      (Reference, Compared : Source_Location) return Relative_Position
@@ -86,6 +94,16 @@ package Langkit_Support.Slocs is
                   and then Sloc /= No_Source_Location);
    --  Tell where Sloc is with respect to Sloc_Range
 
+   ------------------------
+   -- String conversions --
+   ------------------------
+
+   --  All functions below assume that the textual representation of
+   --  Source_Location values have the form "L:C" (L being the line number, C
+   --  the column number) and Source_Location_Range have the form
+   --  "L1:C1-L2-C2" (L1 and C1 are numbers for the start sloc, L2 and C2 are
+   --  the numbers for the end sloc).
+
    function Image (Sloc : Source_Location) return String is
      (Ada.Strings.Fixed.Trim (Line_Number'Image (Sloc.Line), Left) & ':'
       & Ada.Strings.Fixed.Trim (Column_Number'Image (Sloc.Column), Left));
@@ -95,7 +113,17 @@ package Langkit_Support.Slocs is
       & Image (End_Sloc (Sloc_Range)));
 
    function Image (Sloc : Source_Location) return Text_Type
-   is
-     (To_Text (Image (Sloc)));
+   is (To_Text (Image (Sloc)));
+
+   function Image (Sloc_Range : Source_Location_Range) return Text_Type
+   is (To_Text (Image (Sloc_Range)));
+
+   function Value (T : Text_Type) return Source_Location;
+   function Value (S : String) return Source_Location
+   is (Value (To_Text (S)));
+
+   function Value (T : Text_Type) return Source_Location_Range;
+   function Value (S : String) return Source_Location_Range
+   is (Value (To_Text (S)));
 
 end Langkit_Support.Slocs;
