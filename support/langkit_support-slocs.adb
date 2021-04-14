4ac75a79b763f1d5533020719d72006b3b23f0b8
Pierre-Marie de Rodat <derodat@adacore.com>
2021-04-14 18:04:16 +0200

    Langkit_Support.Slocs: add Value (string parsing) functions
    For GitHub issue #492

--- support/langkit_support-slocs.adb
+++ support/langkit_support-slocs.adb
@@ -21,6 +21,8 @@
 -- <http://www.gnu.org/licenses/>.                                          --
 ------------------------------------------------------------------------------
 
+with Ada.Strings.Wide_Wide_Fixed; use Ada.Strings.Wide_Wide_Fixed;
+
 package body Langkit_Support.Slocs is
 
    -------------
@@ -72,5 +74,53 @@ package body Langkit_Support.Slocs is
                     then After
                     else Inside));
    end Compare;
+
+   -----------
+   -- Value --
+   -----------
+
+   function Value (T : Text_Type) return Source_Location is
+      Colon_Index  : constant Natural := Index (T, ":");
+      Line_Slice   : Text_Type renames T (T'First .. Colon_Index - 1);
+      Column_Slice : Text_Type renames T (Colon_Index + 1 .. T'Last);
+      Line         : Line_Number;
+      Column       : Column_Number;
+   begin
+      if Colon_Index = 0 then
+         raise Constraint_Error with "invalid source location";
+      end if;
+
+      begin
+         Line := Line_Number'Wide_Wide_Value (Line_Slice);
+      exception
+         when Constraint_Error =>
+            raise Constraint_Error with
+               "invalid line number: "
+               & Image (Line_Slice, With_Quotes => True);
+      end;
+
+      begin
+         Column := Column_Number'Wide_Wide_Value (Column_Slice);
+      exception
+         when Constraint_Error =>
+            raise Constraint_Error with
+               "invalid column number: "
+               & Image (Column_Slice, With_Quotes => True);
+      end;
+
+      return (Line, Column);
+   end Value;
+
+   -----------
+   -- Value --
+   -----------
+
+   function Value (T : Text_Type) return Source_Location_Range is
+      Dash_Index  : constant Natural := Index (T, "-");
+      Start_Slice : Text_Type renames T (T'First .. Dash_Index - 1);
+      End_Slice   : Text_Type renames T (Dash_Index + 1 .. T'Last);
+   begin
+      return Make_Range (Value (Start_Slice), Value (End_Slice));
+   end Value;
 
 end Langkit_Support.Slocs;
---
+++ testsuite/tests/langkit_support/slocs/main.adb
@@ -0,0 +1,58 @@
+with Ada.Exceptions; use Ada.Exceptions;
+with Ada.Text_IO;    use Ada.Text_IO;
+
+with Langkit_Support.Slocs; use Langkit_Support.Slocs;
+with Langkit_Support.Text;  use Langkit_Support.Text;
+
+procedure Main is
+
+   procedure Check_Sloc (S : String);
+   procedure Check_Sloc_Range (S : String);
+
+   ----------------
+   -- Check_Sloc --
+   ----------------
+
+   procedure Check_Sloc (S : String) is
+      Sloc : Source_Location;
+   begin
+      Put ("""" & S & """ -> ");
+      Sloc := Value (S);
+      Put_Line (Image (Sloc));
+   exception
+      when Exc : Constraint_Error =>
+         Put_Line ("error: " & Exception_Message (Exc));
+   end Check_Sloc;
+
+   ----------------------
+   -- Check_Sloc_Range --
+   ----------------------
+
+   procedure Check_Sloc_Range (S : String) is
+      SR : Source_Location_Range;
+   begin
+      Put ("""" & S & """ -> ");
+      SR := Value (S);
+      Put_Line (Image (SR));
+   exception
+      when Exc : Constraint_Error =>
+         Put_Line ("error: " & Exception_Message (Exc));
+   end Check_Sloc_Range;
+
+begin
+   Check_Sloc ("0:0");
+   Check_Sloc ("123:456");
+   Check_Sloc ("");
+   Check_Sloc ("1");
+   Check_Sloc (":1");
+   Check_Sloc ("-1:2");
+   Check_Sloc ("a:2");
+
+   Check_Sloc_Range ("1:2-3:4");
+   Check_Sloc_Range ("1:2-3:");
+   Check_Sloc_Range (":2-3:4");
+   Check_Sloc_Range ("1:-3:4");
+   Check_Sloc_Range ("1:2-:4");
+   Check_Sloc_Range ("1:2-3:");
+   Check_Sloc_Range ("1:2-3:-1");
+end Main;
