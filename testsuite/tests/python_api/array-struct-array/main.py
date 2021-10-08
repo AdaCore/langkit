fda8373f8a9eb808b59551a2b44f99420bbc5645
Pierre-Marie de Rodat <derodat@adacore.com>
2021-10-08 08:31:14 +0000

    Python API: fix unwrapping of arrays of structs
    Unwrapping an array of structs is meant to take the ownership share for
    these structs, and thus to clear them. This means that the destructor of
    Python wrappers for structs must handle the case where they no longer
    have the ownership of a struct value: they didn't so far, which
    triggered crashes.
    
    TN: UA05-027

--- testsuite/tests/python_api/array-struct-array/main.py
+++ testsuite/tests/python_api/array-struct-array/main.py
@@ -1,6 +1,16 @@
 import libfoolang as lfl
+from libfoolang import ExampleHolder as EH
 
 
 ctx = lfl.AnalysisContext()
 u = ctx.get_from_buffer("test", "example")
-print(u.root.p_example_holders)
+n = u.root
+print(f"p_example_holder = {n.p_example_holders}")
+for a in [
+    [],
+    [EH([])],
+    [EH([n])],
+    [EH([n, n])],
+    [EH([n, n]), EH([n]), EH([])],
+]:
+    print(f"p_identity({a}) = {u.root.p_identity(a)}")
--- testsuite/tests/python_api/array-struct-array/test.py
+++ testsuite/tests/python_api/array-struct-array/test.py
@@ -1,3 +1,8 @@
+"""
+Check that the Python bindings to wrap/unwrap arrays of structs of arrays work
+as expected.
+"""
+
 from langkit.dsl import ASTNode, Struct, T, UserField
 from langkit.expressions import Entity, langkit_property
 
@@ -16,6 +21,10 @@ class Example(FooNode):
     @langkit_property(return_type=ExampleHolder.array, public=True)
     def example_holders():
         return ExampleHolder.new(examples=Entity.singleton).singleton
+
+    @langkit_property(return_type=ExampleHolder.array, public=True)
+    def identity(a=ExampleHolder.array):
+        return a
 
 
 build_and_run(lkt_file='expected_concrete_syntax.lkt', py_script='main.py')
