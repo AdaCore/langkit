15967b6905fd2faa23bd0964876b016523ce0134
Pierre-Marie de Rodat <derodat@adacore.com>
2021-10-14 13:54:50 +0000

    Complete the documentation fix for the "is_ghost" node property
    The previous commit lacked the update for token_end (it is equal to
    token_start).
    
    TN: UA12-003

--- langkit/compiled_types.py
+++ langkit/compiled_types.py
@@ -3482,8 +3482,8 @@ class ASTNodeType(BaseStructType):
 
                 Unlike regular nodes, ghost nodes cover no token in the input
                 source: they are logically located instead between two tokens.
-                The ``token_start`` of all ghost nodes is the token right after
-                this logical position, while they have no ``token_end``.
+                Both the ``token_start`` and the ``token_end`` of all ghost
+                nodes is the token right after this logical position.
                 """
             )),
 
