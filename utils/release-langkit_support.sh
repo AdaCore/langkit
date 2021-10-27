ac1c4a4534cf3f854cfa480ee0a0135fa8c178e2
Pierre-Marie de Rodat <derodat@adacore.com>
2021-10-27 13:31:37 +0000

    Add a script to create a release source package for Langkit_Support
    TN: TA19-011

---
+++ utils/release-langkit_support.sh
@@ -0,0 +1,24 @@
+#! /bin/sh
+
+set -e
+
+RELEASE=22.0.0
+SRC_PKG=/tmp/langkit_support-$RELEASE.tar.gz
+
+# Make sure the appropriate Langkit sources are checked out in
+# /tmp/langkit-$RELEASE
+SRC_DIR=/tmp/langkit-$RELEASE
+export PYTHONPATH=$SRC_DIR:$PYTHONPATH
+
+BUILD_DIR=/tmp/langkit_support-$RELEASE
+rm -rf $BUILD_DIR
+
+# Generate "langkit_support.gpr", move it to the top directory and import
+# sources. Then adjust source/object/library dirs.
+cp -ar $SRC_DIR/support/ $BUILD_DIR
+rm -rf $BUILD_DIR/obj
+rm -rf $BUILD_DIR/lib
+
+# Create the release tarball
+tar czf $SRC_PKG -C /tmp langkit_support-$RELEASE
+sha512sum $SRC_PKG
