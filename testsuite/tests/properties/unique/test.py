6f386a9674e744c0c8009d5d97387b5d05db9955
Laurent Thévenoux <thevenoux@adacore.com>
2021-10-28 14:33:41 +0000

    Lkt: add unique builtin to Array type in prelude.lkt
    Enable lkt_semantic_checks on properties/unique test by adding the
    ``unique`` buitin for the Array type in lkt's prelude.

--- testsuite/tests/properties/unique/test.py
+++ testsuite/tests/properties/unique/test.py
@@ -20,5 +20,6 @@ class Example(FooNode):
         return a.unique
 
 
-build_and_run(lkt_file='expected_concrete_syntax.lkt', py_script='main.py')
+build_and_run(lkt_file='expected_concrete_syntax.lkt', py_script='main.py',
+              lkt_semantic_checks=True)
 print('Done')
