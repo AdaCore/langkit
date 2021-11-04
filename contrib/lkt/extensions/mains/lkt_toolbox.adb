e7f2c5617d953af0f16bace47bfd9b529418f4db
Laurent Thévenoux <thevenoux@adacore.com>
2021-11-04 16:01:10 +0000

    Lkt: check for @invalid annotations in lkt_toolbox
    lkt_toolbox now raises an error if a declaration annotated with
    @invalid doesn't emit any diagnostic, and conversely, it also checks
    that a diagnostic is related to an @invalid declaration.
    
    TN: UB03-005

--- contrib/lkt/extensions/mains/lkt_toolbox.adb
+++ contrib/lkt/extensions/mains/lkt_toolbox.adb
@@ -1,3 +1,5 @@
+with Ada.Command_Line;            use Ada.Command_Line;
+with Ada.Containers.Hashed_Maps;
 with Ada.Directories;             use Ada.Directories;
 with Ada.Exceptions;
 with Ada.Strings.Fixed;           use Ada.Strings.Fixed;
@@ -13,6 +15,7 @@ with Langkit_Support.Diagnostics; use Langkit_Support.Diagnostics;
 with Langkit_Support.Diagnostics.Output;
 use Langkit_Support.Diagnostics.Output;
 
+with Langkit_Support.Slocs;       use Langkit_Support.Slocs;
 with Langkit_Support.Text;        use Langkit_Support.Text;
 
 with Liblktlang.Analysis;         use Liblktlang.Analysis;
@@ -42,12 +45,36 @@ procedure Lkt_Toolbox is
 
    use Liblktlang;
 
+   package Invalid_Decl_Maps is new Ada.Containers.Hashed_Maps
+     (Key_Type        => Analysis.LK_Node,
+      Element_Type    => Boolean,
+      Hash            => Analysis.Hash,
+      Equivalent_Keys => "=");
+
    procedure Print_Semantic_Result
      (S : Analysis.Semantic_Result; Unit : Analysis.Analysis_Unit);
    --  Print a semantic result
 
    function Format_Node (Decl_Node : Decl'Class) return String;
    --  Format node for semantic result printing
+
+   procedure Print_Lkt_Toolbox_Diagnostic
+     (Node : LK_Node'Class; Message : Wide_Wide_String);
+   --  Internal wrapper to ``Print_Diagnostic`` used by lkt_toolbox to print
+   --  additional diagnostics.
+
+   function Populate_Invalid_Decl_Map
+     (Node : Analysis.LK_Node'Class) return Common.Visit_Status;
+   --  Populate ``Invalid_Decl_Map`` and reject declarations with ``@invalid``
+   --  annotations that are nested in another declaration annotated with
+   --  ``@invalid``.
+
+   Invalid_Decl_Map : Invalid_Decl_Maps.Map;
+   --  Map of declarations annotated with ``@invalid``. The boolean elements of
+   --  the map are initialized to ``False`` and set to ``True`` whenever a
+   --  diagnostic is emitted for the related declaration. Therefore, this map
+   --  is used to check that at least one diagnostic has been emitted for each
+   --  declaration annotated with ``@invalid``.
 
    -----------------
    -- Format_Node --
@@ -60,6 +87,20 @@ procedure Lkt_Toolbox is
       return Decl_Node.P_As_Bare_Decl.Image;
    end Format_Node;
 
+   ----------------------------------
+   -- Print_Lkt_Toolbox_Diagnostic --
+   ----------------------------------
+
+   procedure Print_Lkt_Toolbox_Diagnostic
+     (Node : LK_Node'Class; Message : Wide_Wide_String)
+   is
+      Sloc_Range : constant Source_Location_Range := Node.Sloc_Range;
+      Unit       : constant Analysis.Analysis_Unit := Node.Unit;
+      Path       : constant String := Simple_Name (Unit.Get_Filename);
+   begin
+      Print_Diagnostic ((Sloc_Range, To_Unbounded_Text (Message)), Unit, Path);
+   end Print_Lkt_Toolbox_Diagnostic;
+
    ---------------------------
    -- Print_Semantic_Result --
    ---------------------------
@@ -67,33 +108,77 @@ procedure Lkt_Toolbox is
    procedure Print_Semantic_Result
      (S : Analysis.Semantic_Result; Unit : Analysis.Analysis_Unit)
    is
+      Node : constant LK_Node'Class := Analysis.Node (S);
    begin
       if Analysis.Error_Message (S) /= "" then
          declare
             Diag : constant Diagnostic :=
-              (Analysis.Node (S).Sloc_Range,
+              (Node.Sloc_Range,
                To_Unbounded_Text (Analysis.Error_Message (S)));
          begin
+            --  Emit an error if the declaration including ``Node`` has no
+            --  ``@invalid`` annotation. Update ``Invalid_Decl_Map`` otherwise.
+
+            if Node.P_Topmost_Invalid_Decl.Is_Null then
+               Set_Exit_Status (1);
+
+               Print_Lkt_Toolbox_Diagnostic
+                 (Node,
+                  "unexpected diagnostic, is @invalid annotation missing?");
+            else
+               Invalid_Decl_Map (Node.P_Topmost_Invalid_Decl) := True;
+            end if;
+
             Print_Diagnostic
-              (Diag, Unit,
-               Simple_Name (Analysis.Node (S).Unit.Get_Filename));
+              (Diag, Unit, Simple_Name (Node.Unit.Get_Filename));
          end;
       elsif
         not Arg.Check_Only.Get
         and then not Analysis.Result_Type (S).Is_Null
       then
-         Put_Line ("Expr " & Analysis.Node (S).Image);
+         Put_Line ("Expr " & Node.Image);
          Put_Line ("     has type " & Analysis.Result_Type (S).Image);
          New_Line;
       elsif
       not Arg.Check_Only.Get
         and then not Analysis.Result_Ref (S).Is_Null
       then
-         Put_Line ("Id   " & Analysis.Node (S).Image);
+         Put_Line ("Id   " & Node.Image);
          Put_Line ("     references " & Format_Node (Analysis.Result_Ref (S)));
          New_Line;
       end if;
    end Print_Semantic_Result;
+
+   -------------------------------
+   -- Populate_Invalid_Decl_Map --
+   -------------------------------
+
+   function Populate_Invalid_Decl_Map
+     (Node : Analysis.LK_Node'Class) return Common.Visit_Status
+   is
+      use type Common.LK_Node_Kind_Type;
+   begin
+      --  Populate ``Invalid_Decl_Map`` with declarations annotated with
+      --  ``@invalid``.
+
+      if Node.Kind = Common.Lkt_Full_Decl
+         and then Node.As_Full_Decl.P_Has_Annotation
+           (To_Unbounded_Text ("invalid"))
+      then
+         --  ``P_Topmost_Invalid_Decl`` should return the same node. In that
+         --  case, include this node in the map, otherwise nested ``@invalid``
+         --  declarations have been detected: emit a diagnostic.
+
+         if Invalid_Decl_Map.Contains (Node.P_Topmost_Invalid_Decl) then
+            Set_Exit_Status (1);
+
+            Print_Lkt_Toolbox_Diagnostic (Node, "nested @invalid declaration");
+         else
+            Invalid_Decl_Map.Include (Node.As_LK_Node, False);
+         end if;
+      end if;
+      return Common.Into;
+   end Populate_Invalid_Decl_Map;
 
    Ctx : constant Analysis.Analysis_Context := Analysis.Create_Context;
 begin
@@ -119,6 +204,8 @@ begin
                return;
             end if;
 
+            Unit.Root.Traverse (Populate_Invalid_Decl_Map'Access);
+
             declare
                Diags : constant Analysis.Tree_Semantic_Result :=
                  Unit.Root.P_Check_Semantic;
@@ -127,6 +214,20 @@ begin
                   Print_Semantic_Result (D, Unit);
                end loop;
             end;
+
+            --  Ensure that all ``@invalid`` declarations in the map have been
+            --  reported. Print a diagnostic otherwise.
+
+            for E in Invalid_Decl_Map.Iterate loop
+               if not Invalid_Decl_Maps.Element (E) then
+                  Set_Exit_Status (1);
+
+                  Print_Lkt_Toolbox_Diagnostic
+                    (Invalid_Decl_Maps.Key (E),
+                     "@invalid declaration without diagnostic");
+               end if;
+            end loop;
+
          end;
       end loop;
    end if;
