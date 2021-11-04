1a35ef7443e6566e1c817daebb627c206c0b5123
Pierre-Marie de Rodat <derodat@adacore.com>
2021-11-04 15:04:30 +0000

    Lkt lowering: handle block expressions
    TN: RA22-015

--- testsuite/tests/properties/let_underscore/test.py
+++ testsuite/tests/properties/let_underscore/test.py
@@ -25,5 +25,5 @@ class BarNode(FooNode):
 
 
 build_and_run(lkt_file='expected_concrete_syntax.lkt', py_script='main.py',
-              lkt_semantic_checks=True)
+              types_from_lkt=True)
 print('Done')
