e90bafac1a94bb6baa3d0b5ceed03fde58da8e1b
Pierre-Marie de Rodat <derodat@adacore.com>
2021-11-25 10:59:15 +0000

    Refactor tests for basic iterator uses in public APIs
    TN: U401-024

---
+++ testsuite/tests/iterators/basic/main.py
@@ -0,0 +1,58 @@
+import sys
+
+import libfoolang
+
+
+print('main.py: Running...')
+
+ctx = libfoolang.AnalysisContext()
+u = ctx.get_from_buffer('main.txt', b"example")
+if u.diagnostics:
+    for d in u.diagnostics:
+        print(d)
+    sys.exit(1)
+
+
+def print_title(label):
+    print(label)
+    print("#" * len(label))
+    print("")
+
+
+print_title("Int iterator")
+
+print("Base array:")
+for x in u.root.p_int_array:
+    print(f"  {x}")
+
+print("Iteration:")
+for x in u.root.p_int_iterator:
+    print(f"  {x}")
+print("")
+
+print("Identity on a null iterator...")
+print("-> {}".format(u.root.p_int_iterator_identity(None)))
+print("")
+
+print("Identity on a stale iterator...")
+it = u.root.p_int_iterator
+u.reparse(buffer=b" example ")
+try:
+    u.root.p_int_iterator_identity(it)
+    print("... got no exception")
+except libfoolang.StaleReferenceError:
+    print("... got a stale reference error")
+print("")
+
+print_title("Entity iterator")
+
+print("Base array:")
+for x in u.root.p_entities_array:
+    print(f"  {x}")
+
+print("Iteration:")
+for x in u.root.p_entities_iterator:
+    print(f"  {x}")
+print("")
+
+print('main.py: Done.')
