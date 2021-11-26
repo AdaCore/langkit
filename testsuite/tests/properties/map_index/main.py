0c920d2f00295c9e451e575b0945523765d5908b
Pierre-Marie de Rodat <derodat@adacore.com>
2021-11-26 16:54:46 +0000

    Property DSL: fix handling of index in filter/take_while lambdas
    TN: UB26-031

--- testsuite/tests/properties/map_index/main.py
+++ testsuite/tests/properties/map_index/main.py
@@ -3,15 +3,38 @@ import sys
 import libfoolang
 
 
-print('main.py: Running...')
+print("main.py: Running...")
+print("")
 
 ctx = libfoolang.AnalysisContext()
-u = ctx.get_from_buffer('main.txt', b'1 2 3 4')
+u = ctx.get_from_buffer("main.txt", b"1 2 3 4")
 if u.diagnostics:
     for d in u.diagnostics:
         print(d)
     sys.exit(1)
 
-print('root.p_prop = {}'.format(u.root.p_prop))
 
-print('main.py: Done.')
+def print_item(item):
+    if isinstance(item, libfoolang.IndexedNumber):
+        print(f"{item.index}: NumberNode({item.number.text})")
+    else:
+        assert isinstance(item, libfoolang.NumberNode)
+        print(f"NumberNode({item.text})")
+
+
+for prop in (
+    "p_map_no_idx",
+    "p_map_idx",
+    "p_filter_no_idx",
+    "p_filter_idx",
+    "p_filtermap_no_idx",
+    "p_filtermap_elt_idx",
+    "p_filtermap_filter_idx",
+    "p_filtermap_all_idx",
+):
+    print(f"== {prop} ==")
+    for item in getattr(u.root, prop):
+        print_item(item)
+    print("")
+
+print("main.py: Done.")
