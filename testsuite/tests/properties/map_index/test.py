3d528dd957aa8084a4eac129d4e2df05438684f4
Pierre-Marie de Rodat <derodat@adacore.com>
2021-11-26 16:54:46 +0000

    Property DSL: fix handling of index in filter/take_while lambdas
    TN: UB26-031

--- testsuite/tests/properties/map_index/test.py
+++ testsuite/tests/properties/map_index/test.py
@@ -1,22 +1,90 @@
-from langkit.dsl import ASTNode, Field
-from langkit.expressions import Property, Self
+"""
+Check that the various forms of the "map" DSL construct correctly handle the
+presence or absence of index.
+"""
+
+from langkit.compiled_types import T
+from langkit.dsl import ASTNode, Field, Struct, UserField
+from langkit.expressions import Self, String, langkit_property
 
 from utils import build_and_run
 
 
+class IndexedNumber(Struct):
+    index = UserField(type=T.Int, public=True)
+    number = UserField(type=T.NumberNode.entity, public=True)
+
+
 class FooNode(ASTNode):
-    pass
+
+    @langkit_property()
+    def create(index=T.Int, number=T.NumberNode.entity):
+        return IndexedNumber.new(index=index, number=number)
 
 
 class ListNode(FooNode):
-    nb_list = Field()
-    prop = Property(Self.nb_list.map(lambda i, _: i), public=True)
+    nb_list = Field(type=T.NumberNode.list)
+
+    @langkit_property()
+    def nb_list_entities():
+        return Self.nb_list.map(lambda n: n.as_bare_entity)
+
+    @langkit_property(public=True)
+    def map_no_idx():
+        return Self.nb_list_entities.map(
+            lambda n: Self.create(0, n)
+        )
+
+    @langkit_property(public=True)
+    def map_idx():
+        return Self.nb_list_entities.map(
+            lambda i, n: Self.create(i, n)
+        )
+
+    @langkit_property(public=True)
+    def filter_no_idx():
+        return Self.nb_list_entities.filter(
+            lambda n: n.text == String("2")
+        )
+
+    @langkit_property(public=True)
+    def filter_idx():
+        return Self.nb_list_entities.filter(
+            lambda i, n: (n.text == String("2")) | (i == 0)
+        )
+
+    @langkit_property(public=True)
+    def filtermap_no_idx():
+        return Self.nb_list_entities.filtermap(
+            lambda n: Self.create(0, n),
+            lambda n: n.text == String("2")
+        )
+
+    @langkit_property(public=True)
+    def filtermap_elt_idx():
+        return Self.nb_list_entities.filtermap(
+            lambda i, n: Self.create(i, n),
+            lambda n: n.text == String("2")
+        )
+
+    @langkit_property(public=True)
+    def filtermap_filter_idx():
+        return Self.nb_list_entities.filtermap(
+            lambda n: Self.create(0, n),
+            lambda i, n: (n.text == String("2")) | (i == 0)
+        )
+
+    @langkit_property(public=True)
+    def filtermap_all_idx():
+        return Self.nb_list_entities.filtermap(
+            lambda i, n: Self.create(i, n),
+            lambda i, n: (n.text == String("2")) | (i == 0)
+        )
 
 
 class NumberNode(FooNode):
     token_node = True
 
 
-build_and_run(lkt_file='expected_concrete_syntax.lkt', py_script='main.py',
-              types_from_lkt=True)
+build_and_run(lkt_file='expected_concrete_syntax.lkt', py_script='main.py')
 print('Done')
