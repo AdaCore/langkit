edb38f3109e73f21b507a96aa05bea80d2090b34
Pierre-Marie de Rodat <derodat@adacore.com>
2022-03-03 14:13:04 +0100

    GDB helpers: remove a GDB bug workaround
    This bug has been fixed recently in GDB. The workaround did not work
    properly on Windows, as the backslashes in directory separators were
    interpreted as escape sequences.
    
    TN: S521-024

--- langkit/gdb/units.py
+++ langkit/gdb/units.py
@@ -16,7 +16,4 @@ class AnalysisUnit:
         virtual_file = self.value['filename']
         vf_record = virtual_file['value'].dereference()
         v = dereference_fat_array_ptr(vf_record['full'])
-        # TODO: replace the call to eval below with a call to the .string()
-        # method. This does not work as of today because of a GDB bug: see
-        # R219-011.
-        return eval(str(v))
+        return v.string()
