f4fd415d69bb1e43d7f077d9584c1a4d9da7c36a
Pierre-Marie de Rodat <derodat@adacore.com>
2022-05-10 17:39:07 +0200

    grammar/invalid_lexers: DSL to Lkt transition
    For GitLab issue #622

--- testsuite/tests/grammar/invalid_lexers/test.py
+++ testsuite/tests/grammar/invalid_lexers/test.py
@@ -1,21 +1,18 @@
 import glob
 
 import langkit
-from langkit.dsl import ASTNode
 
 from utils import emit_and_print_errors
 
 
-for lkt_file in sorted(glob.glob('*.lkt')):
+# Compile all *.lkt" file except "common.lkt", which contains just common code
+# for the other sources.
+tests = [f for f in glob.glob("*.lkt") if f != "common.lkt"]
+
+
+for lkt_file in sorted(tests):
     print('== {} =='.format(lkt_file))
-
-    class FooNode(ASTNode):
-        pass
-
-    class Example(FooNode):
-        token_node = True
-
-    emit_and_print_errors(lkt_file=lkt_file)
+    emit_and_print_errors(lkt_file=lkt_file, types_from_lkt=True)
     langkit.reset()
     print('')
 
