11355123e1ed2dfb675a07cf5d05c5d868639fa1
Raphaël AMIARD <raph.amiard@gmail.com>
2022-05-17 17:24:57 +0200

    Add a test for the :typeref: directive in langkit docstrings
    For libadalang#923

---
+++ testsuite/tests/misc/docstrings_lkt_roles/test.py
@@ -0,0 +1,42 @@
+"""
+Check that the typeref langkit doc directive is correctly emitted in generated
+APIs.
+"""
+
+
+from langkit.dsl import ASTNode
+from langkit.expressions import langkit_property
+
+from utils import emit_and_print_errors
+
+
+class FooNode(ASTNode):
+    """
+    Log this docstring. This is a typeref: :typeref:`FooNode`.
+    """
+
+    @langkit_property(public=True)
+    def prop():
+        return 1
+
+
+class Example(FooNode):
+    pass
+
+
+emit_and_print_errors(lkt_file='foo.lkt')
+
+# Log docstrings
+for path in [
+    "build/src/libfoolang-analysis.ads",
+    "build/libfoolang.h",
+    "build/ocaml/libfoolang.mli",
+    "build/python/libfoolang/__init__.py",
+]:
+    with open(path) as f:
+        lines = f.readlines()
+        for line in lines:
+            if 'Log this docstring' in line:
+                print(path, line)
+
+print('Done')
