583bf96327dac54c98631e785edf986768a80d0b
Pierre-Marie de Rodat <derodat@adacore.com>
2022-07-19 15:55:43 +0200

    properties/rebindings: simplify and fix pasto
    Fix the "p_old" property so that it returns the old env (not the new
    one). Also remove lots of useless bits in this testcase, to avoid noise
    to the readers.
    
    TN: V708-016

--- testsuite/tests/properties/rebindings/main.py
+++ testsuite/tests/properties/rebindings/main.py
@@ -3,46 +3,49 @@ import sys
 import libfoolang
 
 
-def def_repr(n):
-    return '<{} {} {}>'.format(
-        type(n).__name__,
-        n.p_name,
-        n.sloc_range
-    )
+def block_repr(n):
+    return f"<{type(n).__name__} {n.f_name.text}>"
 
 
-libfoolang.DefNode.__repr__ = def_repr
+libfoolang.Block.__repr__ = block_repr
 
 
-def process(index, n):
+def exc_repr(exc):
+    return f"<{type(exc).__name__}: {exc}>"
+
+
+def print_rebindings(n):
+
     def wrapped_eval(lambda_fn):
         try:
             return lambda_fn()
         except libfoolang.PropertyError as exc:
-            return '<{}: {}>'.format(type(exc).__name__, exc)
+            return exc_repr(exc)
 
-    print('Processing {} ({})'.format(n, index))
-    parent = wrapped_eval(lambda: n.p_parent_rebindings)
-    new = wrapped_eval(lambda: n.p_new)
-    old = wrapped_eval(lambda: n.p_old)
-    print('  parent = {}'.format(parent))
-    print('  new = {}'.format(new))
-    print('  old = {}'.format(old))
+    # Print each element of the rebindings chain in "n"
+    while True:
+        print(f"{n}:")
+        new = wrapped_eval(lambda: n.p_new)
+        old = wrapped_eval(lambda: n.p_old)
+        print(f"  new = {new}")
+        print(f"  old = {old}")
 
-    # Computing "parent" raised a PropertyError => "parent" is a string
-    if not isinstance(parent, str):
-        process(index + 1, parent)
+        try:
+            n = n.p_parent_rebindings
+        except libfoolang.PropertyError as exc:
+            print(f"  no parent: {exc_repr(exc)}")
+            break
 
 
 ctx = libfoolang.AnalysisContext()
 u = ctx.get_from_buffer('main.txt', b"""
-(T) foo { a b }
+def foo
 
-(T U) old_bar { c d }
-(T U) new_bar { e f }
+def old_bar
+def new_bar
 
-(V) old_baz { g }
-(V) new_baz { h }
+def old_baz
+def new_baz
 """)
 if u.diagnostics:
     for d in u.diagnostics:
@@ -53,6 +56,6 @@ foo, old_bar, new_bar, old_baz, new_baz = u.root
 rebound_once = foo.p_rebind(old_bar, new_bar)
 rebound_twice = rebound_once.p_rebind(old_baz, new_baz)
 
-process(0, rebound_twice)
+print_rebindings(rebound_twice)
 
 print('main.py: Done.')
--- testsuite/tests/properties/rebindings/test.py
+++ testsuite/tests/properties/rebindings/test.py
@@ -2,10 +2,9 @@
 Check that .rebindings related built-in properties behave as expected.
 """
 
-from langkit.dsl import ASTNode, Field, abstract, has_abstract_list
-from langkit.envs import EnvSpec, add_env, add_to_env_kv
-from langkit.expressions import (AbstractProperty, Entity, New, Property, Self,
-                                 T, Var, langkit_property)
+from langkit.dsl import ASTNode, Field
+from langkit.envs import EnvSpec, add_env
+from langkit.expressions import Entity, Self, T, Var, langkit_property
 
 from utils import build_and_run
 
@@ -18,72 +17,48 @@ class Name(FooNode):
     token_node = True
 
 
-@abstract
-class DefNode(FooNode):
-    name = AbstractProperty(T.Symbol, public=True)
-    env_spec = EnvSpec(
-        add_to_env_kv(key=Self.name, value=Self)
-    )
+class Block(FooNode):
+    name = Field(type=T.Name)
 
+    env_spec = EnvSpec(add_env())
 
-class Block(DefNode):
-    params = Field(type=T.Params)
-    name_field = Field(type=T.Name)
-    vars = Field(type=T.BlockVar.list)
-
-    name = Property(Self.name_field.symbol)
+    @langkit_property()
+    def create_entity(rbdng=T.EnvRebindings):
+        """
+        Helper to create a new entity from ``Entity`` but with the given
+        rebindings.
+        """
+        e_info = Var(
+            T.entity_info.new(
+                md=Entity.info.md,
+                rebindings=rbdng,
+                from_rebound=False,
+            )
+        )
+        return Block.entity.new(node=Self, info=e_info)
 
     @langkit_property(public=True)
     def rebind(from_block=T.Block.entity, to_block=T.Block.entity):
-        rbdng = Var(Entity.info.rebindings.append_rebinding(
-            from_block.params.children_env,
-            to_block.children_env
-        ))
+        return Entity.create_entity(
+            Entity.info.rebindings.append_rebinding(
+                from_block.children_env,
+                to_block.children_env
+            )
+        )
 
-        e_info = Var(New(T.entity_info,
-                         md=Entity.info.md,
-                         rebindings=rbdng,
-                         from_rebound=False))
-
-        return New(Block.entity, node=Self, info=e_info)
+    # Accessors so that main.py can explore the rebindings chain
 
     @langkit_property(public=True)
     def parent_rebindings():
-        e_info = Var(New(T.entity_info,
-                         md=Entity.info.md,
-                         rebindings=Entity.info.rebindings.get_parent,
-                         from_rebound=False))
-        return New(Block.entity, node=Self, info=e_info)
+        return Entity.create_entity(Entity.info.rebindings.get_parent)
 
     @langkit_property(public=True)
     def new():
-        return (Entity.info.rebindings.new_env.env_node
-                .as_bare_entity)
+        return Entity.info.rebindings.new_env.env_node.as_bare_entity
 
     @langkit_property(public=True)
     def old():
-        return (Entity.info.rebindings.new_env.env_node
-                .as_bare_entity)
-
-    env_spec = EnvSpec(
-        add_to_env_kv(key=Self.name, value=Self),
-        add_env()
-    )
-
-
-@has_abstract_list
-class Param(DefNode):
-    name_field = Field(type=T.Name)
-    name = Property(Self.name_field.symbol)
-
-
-class Params(Param.list):
-    env_spec = EnvSpec(add_env())
-
-
-class BlockVar(DefNode):
-    name_field = Field(type=T.Name)
-    name = Property(Self.name_field.symbol)
+        return Entity.info.rebindings.old_env.env_node.as_bare_entity
 
 
 build_and_run(lkt_file='expected_concrete_syntax.lkt', py_script='main.py')
