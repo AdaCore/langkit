2f9dfa7256700941cf85e2d256e9c74e19396ac1
Raphaël AMIARD <raph.amiard@gmail.com>
2022-10-21 14:44:18 +0200

    Add App.default_get_files mechanism to Python API
    This will allow Libadalang to use GPR's API to get source files.
    
    Needed for libadalang#938
    
    (cherry picked from commit a4c7be1865a1887d709f9ddd274a993af93c02b9)

--- testsuite/tests/python_api/python_app/main.py
+++ testsuite/tests/python_api/python_app/main.py
@@ -6,5 +6,26 @@ class App(lfl.App):
         unit.root.dump()
 
 
+class App2(lfl.App):
+    def process_unit(self, unit):
+        unit.root.dump()
+
+    def default_get_files(self):
+        return ['input3', 'input1', 'input2']
+
+
+class App3(lfl.App):
+    def process_unit(self, unit):
+        unit.root.dump()
+
+
 if __name__ == '__main__':
+    print("Running App1")
     App.run(['input3', 'input1', 'input2'])
+    print()
+    print("Running App2")
+    App2.run([])
+    print()
+    print("Running App3")
+    App3.run([])
+    print()
