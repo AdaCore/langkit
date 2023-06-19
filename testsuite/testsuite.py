941d0813b40f22ee75dd21207f7de19f6a11e37f
Hugo Guerrier <guerrier@adacore.com>
2023-06-19 09:45:00 +0200

    Enable native-image testcases by default

--- testsuite/testsuite.py
+++ testsuite/testsuite.py
@@ -93,9 +93,9 @@ class LangkitTestsuite(Testsuite):
                  ' "mvn".'
         )
         parser.add_argument(
-            '--enable-native-image', action="store_true",
-            help="Enable testcases that require native-image (they are"
-                 " disabled by default)."
+            '--disable-native-image', action="store_true",
+            help="Disable testcases that require native-image (they are"
+                 " enabled by default)."
         )
 
         parser.add_argument(
@@ -141,7 +141,7 @@ class LangkitTestsuite(Testsuite):
             'restricted_env': args.restricted_env,
             'has_ocaml': not args.disable_ocaml,
             'has_java': not args.disable_java,
-            'has_native_image': args.enable_native_image,
+            'has_native_image': not args.disable_native_image,
             'has_gdb': not args.disable_gdb,
             'os': self.env.build.os.name,
         }
