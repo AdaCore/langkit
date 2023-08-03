a771d5bfbbf21b504ec56822c550660d25519904
Pierre-Marie de Rodat <derodat@adacore.com>
2023-08-03 11:15:23 +0000

    Java JNI: fix crash on property exception for result that needs wrapping

---
+++ testsuite/tests/java_api/array_exc/graal_c_api/test.py
@@ -0,0 +1,12 @@
+from utils import build_and_run
+
+import common
+
+
+common  # avoid pyflakes "unused" warning
+
+build_and_run(
+    lkt_file="expected_concrete_syntax.lkt",
+    java_main="Main",
+)
+print("Done")
