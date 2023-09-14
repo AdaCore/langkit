47e2b59cf98518deffe2ce9207db871b71c71efd
Pierre-Marie de Rodat <derodat@adacore.com>
2023-09-14 08:10:36 +0000

    properties/entity_cast: enable full Lkt lowering

--- testsuite/tests/properties/entity_cast/test.py
+++ testsuite/tests/properties/entity_cast/test.py
@@ -30,5 +30,9 @@ class Literal(FooNode):
                  public=True)
 
 
-build_and_run(lkt_file='expected_concrete_syntax.lkt', py_script='main.py')
+build_and_run(
+    lkt_file='expected_concrete_syntax.lkt',
+    py_script='main.py',
+    types_from_lkt=True,
+)
 print('Done')
