ab4636983007b87dfd53abb5f1873c0d50ce1493
Pierre-Marie de Rodat <derodat@adacore.com>
2023-10-09 10:42:03 +0000

    Logic predicates: fix the handling of default values + dynamic vars
    The logic to turn partial arguments into list of arguments in a property
    call was incorrect: when there are natural arguments with default values
    in addition to dynamic variables, emitting positional arguments
    accidentally associated dynamic variable to the arguments with default
    values. For instance, for the property:
    
        @with_dynvars(my_var)
        fun pred(n: Entity[FooNode], flag: Bool = false)
    
    then, creating the following logic predicate:
    
        bind my_var = 0;
        %predicate(FooNode.pred, node.lvar1, node.lvar2)
    
    used to generate the following property call:
    
        Foo_Node_P_Pred
          (Node,          --  For "node"
           Entities (1),  --  For "n"
           0);            --  For "my_var"
    
    whereas the property signature is actually:
    
        Foo_Node_P_Pred
          (Node: Foo_Node, N: FooNode, Flag: Boolean, My_Var: Integer);
    
    This change refactors the property partial evaluation handling for logic
    predicates to keep track of which argument/dynamic variable is passed,
    and to emit property calling code correctly.

--- testsuite/tests/properties/dflt_arg_val_predicate/test.py
+++ testsuite/tests/properties/dflt_arg_val_predicate/test.py
@@ -2,11 +2,15 @@
 Test that Prediate works well with default argument values.
 """
 
-from langkit.dsl import ASTNode, Bool, LogicVar, T, UserField
-from langkit.expressions import (And, Predicate, Self, Var, ignore,
-                                 langkit_property)
+from langkit.dsl import ASTNode, T, UserField
+from langkit.expressions import (
+    And, DynamicVariable, Predicate, Self, Var, langkit_property
+)
 
 from utils import build_and_run
+
+
+my_var = DynamicVariable("my_var", T.Int)
 
 
 class FooNode(ASTNode):
@@ -15,19 +19,61 @@ class FooNode(ASTNode):
 
 class Example(FooNode):
 
-    var1 = UserField(LogicVar, public=False)
-    var2 = UserField(LogicVar, public=False)
+    var1 = UserField(T.LogicVar, public=False)
+    var2 = UserField(T.LogicVar, public=False)
+
+    # Simple case: the predicate only has two arguments with default values
 
     @langkit_property()
-    def predicate(n=T.FooNode.entity,
-                  flag1=(Bool, False),
-                  flag2=(Bool, True)):
+    def predicate1(
+        n=T.FooNode.entity,
+        flag1=(T.Bool, False),
+        flag2=(T.Bool, True)
+    ):
         return And(flag1, n.is_null, flag2)
 
     @langkit_property(public=True)
-    def prop():
-        ignore(Var(Predicate(T.Example.predicate, Self.var1, Self.var2)))
-        return Self.as_bare_entity
+    def prop1():
+        eq = Var(Predicate(T.Example.predicate1, Self.var1, Self.var2))
+        return eq.solve
+
+    # More complex case: the predicate has one argument with a default value,
+    # but also takes a dynamic variable.
+
+    @langkit_property(dynamic_vars=[my_var])
+    def predicate2(
+        n=T.FooNode.entity,
+        flag=(T.Bool, False)
+    ):
+        return And(n.is_null, my_var == 0, flag)
+
+    @langkit_property(public=True)
+    def prop2():
+        eq = Var(
+            my_var.bind(
+                1, Predicate(T.Example.predicate2, Self.var1, Self.var2)
+            )
+        )
+        return eq.solve
+
+    # Variant of the complex case: the predicate takes both one argument with a
+    # default value and a dynamic variable with a default value.
+
+    @langkit_property(dynamic_vars=[(my_var, 0)])
+    def predicate3(
+        n=T.FooNode.entity,
+        flag=(T.Bool, False)
+    ):
+        return And(n.is_null, my_var == 0, flag)
+
+    @langkit_property(public=True)
+    def prop3():
+        eq = Var(
+            my_var.bind(
+                1, Predicate(T.Example.predicate3, Self.var1, Self.var2)
+            )
+        )
+        return eq.solve
 
 
 build_and_run(lkt_file='expected_concrete_syntax.lkt', py_script='main.py')
