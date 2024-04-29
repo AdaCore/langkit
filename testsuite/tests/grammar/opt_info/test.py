3df6923bcc81d7593402e0e908af7ee49bff6705
Pierre-Marie de Rodat <derodat@adacore.com>
2024-04-29 15:50:56 +0000

    Testsuite: enable lkt_compile tests to check the CompileCtx
    Transition the remaining Lkt tests that needed this feature from the
    Python driver to the lkt_compile one.

--- testsuite/tests/grammar/opt_info/test.py
+++ testsuite/tests/grammar/opt_info/test.py
@@ -2,22 +2,18 @@
 Test that the is_optional property on parse fields works as expected.
 """
 
-from utils import emit_and_print_errors
 
-
-ctx = emit_and_print_errors(lkt_file='foo.lkt', types_from_lkt=True)
-for n in ctx.astnode_types:
-    if n.dsl_name not in {
-        "ParserTest",
-        "SynthParent",
-        "NullFieldParent",
-        "OptionalByAnnotation",
-    }:
-        continue
-    for field in n.get_parse_fields():
-        print("Field {} is {}".format(
-            field.qualname,
-            "optional" if field.nullable else "not optional"
-        ))
-
-print('Done')
+def main(ctx):
+    for n in ctx.astnode_types:
+        if n.dsl_name not in {
+            "ParserTest",
+            "SynthParent",
+            "NullFieldParent",
+            "OptionalByAnnotation",
+        }:
+            continue
+        for field in n.get_parse_fields():
+            print("Field {} is {}".format(
+                field.qualname,
+                "optional" if field.nullable else "not optional"
+            ))
--- testsuite/tests/grammar/precise_types/test.py
+++ testsuite/tests/grammar/precise_types/test.py
@@ -2,80 +2,14 @@
 Check precise types for parse fields.
 """
 
-from langkit.dsl import ASTNode, AbstractField, Field, T, abstract, synthetic
-from langkit.expressions import Self, langkit_property
 
-from utils import emit_and_print_errors
+def main(ctx):
+    for n in ctx.astnode_types:
+        fields = {f.original_name: f for f in n.get_fields()}
+        for _, f in sorted(fields.items()):
+            print(f"== Doc for {f.qualname} ==")
+            print(f.doc)
+            print("")
 
-
-class FooNode(ASTNode):
-    pass
-
-
-class A(FooNode):
-    token_node = True
-
-
-class B(FooNode):
-    token_node = True
-
-
-class SpecialList(FooNode.list):
-    pass
-
-
-class Holder(FooNode):
-    a_or_b = Field(type=T.FooNode)
-    a_or_b_list = Field(type=T.FooNode.list)
-    a_list_or_b_list = Field(type=T.FooNode)
-    special_a_list = Field(type=SpecialList)
-    special_a_list_or_special_b_list = Field(type=SpecialList)
-    special_a_or_b_list = Field(type=SpecialList)
-    a_or_b_or_null = Field(type=T.FooNode)
-
-
-@abstract
-class ForSynthParent(FooNode):
-    f = AbstractField(type=T.FooNode)
-
-    @langkit_property(memoized=True)
-    def private_create(n=T.B):
-        return T.ForSynthChild1.new(f=n)
-
-    @langkit_property(public=True)
-    def public_create(n=T.B.entity):
-        return Self.private_create(n.node)
-
-
-@synthetic
-class ForSynthChild1(ForSynthParent):
-    f = Field(type=T.FooNode)
-
-
-class ForSynthChild2(ForSynthParent):
-    f = Field(type=T.FooNode)
-
-
-@synthetic
-class OnlySynthNode(FooNode):
-    f = Field(type=T.FooNode)
-
-    @langkit_property(memoized=True)
-    def private_create(n=T.A):
-        return T.OnlySynthNode.new(f=n)
-
-    @langkit_property(public=True)
-    def public_create(n=T.A.entity):
-        return Self.private_create(n.node)
-
-
-ctx = emit_and_print_errors(lkt_file="foo.lkt")
-print("")
-for n in ctx.astnode_types:
-    fields = {f.original_name: f for f in n.get_fields()}
-    for _, f in sorted(fields.items()):
-        print(f"== Doc for {f.qualname} ==")
-        print(f.doc)
-        print("")
 
 print('Done')
