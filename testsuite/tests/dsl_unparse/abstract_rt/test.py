3ff2217bc9b8410dd9a944ff6f3722b5de6c7b48
Pierre-Marie de Rodat <derodat@adacore.com>
2024-06-13 09:53:40 +0000

    dsl_unparse: mark arguments for "abstract rt. check" props as @ignored
    This is necessary to avoid "unused bindings" warnings when compiling the
    resulting Lkt sources.

---
+++ testsuite/tests/dsl_unparse/abstract_rt/test.py
@@ -0,0 +1,28 @@
+from langkit.compiled_types import T
+from langkit.dsl import ASTNode
+from langkit.expressions import AbstractKind, langkit_property
+
+from utils import emit_and_print_errors, unparse_script
+
+
+class FooNode(ASTNode):
+
+    @langkit_property(
+        kind=AbstractKind.abstract_runtime_check,
+        return_type=T.Bool,
+        public=True,
+    )
+    def p(i=T.Int):
+        pass
+
+
+class Example(FooNode):
+    token_node = True
+
+
+emit_and_print_errors(
+    lkt_file="expected_concrete_syntax.lkt",
+    unparse_script=unparse_script,
+    types_from_lkt=False,
+)
+print("Done")
