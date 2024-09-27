f5c19813eea4de273d4e7d42b33fe3ccdd56e677
Hugo Guerrier <hugogguerrier@gmail.com>
2024-09-27 15:48:24 +0200

    Melt the Java 'node_identity' test into the 'general' one

--- testsuite/tests/java_api/general/common/BindingsTests.java
+++ testsuite/tests/java_api/general/common/BindingsTests.java
@@ -288,6 +288,19 @@ public final class BindingsTests {
                 (root.getFieldDescription("p_all_items").memberRef ==
                  MemberReference.FOO_SEQUENCE_P_ALL_ITEMS)
             );
+
+            // Test node equality and hash codes
+            FooNode e1 = root.pWithMd(true, false);
+            FooNode e2 = root.pWithMd(true, true);
+            FooNode e3 = root.pWithMd(false, true);
+            System.out.println("e1.equals(e2) = " + e1.equals(e2));
+            System.out.println("e1.equals(e3) = " + e1.equals(e3));
+            System.out.println(
+                "hash(e1) == hash(e2) = " + (e1.hashCode() == e2.hashCode())
+            );
+            System.out.println(
+                "hash(e1) == hash(e3) = " + (e1.hashCode() == e3.hashCode())
+            );
         }
 
         // Display the footer
