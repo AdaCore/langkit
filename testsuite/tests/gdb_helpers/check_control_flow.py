30032360da4a74824455b1f266505c813d320a4b
Pierre-Marie de Rodat <derodat@adacore.com>
2024-10-11 12:50:11 +0000

    gdb_helpers: migrate testcase to pure Lkt
    (cherry picked from commit 16975440d04185bbd6d69b9fbb33ea1b4c024a69)

--- testsuite/tests/gdb_helpers/check_control_flow.py
+++ testsuite/tests/gdb_helpers/check_control_flow.py
@@ -22,7 +22,7 @@ run_foobreak("FooNode.test_control_flow")
 run_continue()
 gdb.test("foostate", """\
 Running FooNode.test_control_flow
-from @...test.py:@NUMBER
+from @...test.lkt:@NUMBER
   self = <Example main.txt:1:1-1:8>
   i = 1""")
 
@@ -31,37 +31,41 @@ gdb.test("foostate i", "i = 1")
 gdb.test("foostate nosuchvar", "No binding called nosuchvar")
 
 # Check state at various points during the property execution
-run_foonext("Now evaluating <parent at test.py:@NUMBER>")
+run_foonext("Now evaluating <parent at test.lkt:@NUMBER:@NUMBER>")
 run_foonext("""\
-<parent at test.py:@NUMBER> evaluated to: <FooNodeList main.txt:1:1-1:23>
+<parent at test.lkt:@NUMBER:@NUMBER> evaluated to: <FooNodeList main.txt:1:1-1:23>
 
-Now evaluating <children at test.py:@NUMBER>""")
+Now evaluating <children at test.lkt:@NUMBER:@NUMBER>""")
 run_foonext("""\
-<children at test.py:@NUMBER> evaluated to: Bare_Foo_Node array of\
+<children at test.lkt:@NUMBER:@NUMBER> evaluated to: Bare_Foo_Node array of\
  length 3 = {@...}
 
-Now evaluating <Block at ???>""")
-run_foonext("Now evaluating <Map at test.py:@NUMBER>")
-run_foonext("Now evaluating <FieldAccess for parents at test.py:@NUMBER>")
+Now evaluating <Let at test.lkt:@NUMBER:@NUMBER>""")
+run_foonext("Now evaluating <Map at test.lkt:@NUMBER:@NUMBER>")
+run_foonext("Now evaluating <FieldAccess for parents at test.lkt:@NUMBER:@NUMBER>")
 gdb.test("foostate", """\
 Running FooNode.test_control_flow
-from @...test.py:@NUMBER
+from @...test.lkt:@NUMBER
   self = <Example main.txt:1:1-1:8>
   i = 1
 
-Currently evaluating <Block at ???>
+Currently evaluating <Let at test.lkt:@NUMBER:@NUMBER>
+from @...test.lkt:@NUMBER
   nodes = Bare_Foo_Node array of length 3 =\
  {<Example main.txt:1:1-1:8>, <Example main.txt...
-  <parent at test.py:@NUMBER> -> <FooNodeList main.txt:1:1-1:23>
-  <children at test.py:@NUMBER> -> Bare_Foo_Node array of length 3 =\
+  <parent at test.lkt:@NUMBER:@NUMBER> -> <FooNodeList main.txt:1:1-1:23>
+  <children at test.lkt:@NUMBER:@NUMBER> -> Bare_Foo_Node array of length 3 =\
  {<Example main.txt:1:1-1:8>, <Example main.txt...
 
-Currently evaluating <Map at test.py:@NUMBER>
-from @...test.py:@NUMBER
+Currently evaluating <Let at test.lkt:@NUMBER:@NUMBER>
+from @...test.lkt:@NUMBER
+
+Currently evaluating <Map at test.lkt:@NUMBER:@NUMBER>
+from @...test.lkt:@NUMBER
   n = <Example main.txt:1:1-1:8>
 
-Currently evaluating <FieldAccess for parents at test.py:@NUMBER>
-from @...test.py:@NUMBER""")
+Currently evaluating <FieldAccess for parents at test.lkt:@NUMBER:@NUMBER>
+from @...test.lkt:@NUMBER""")
 
 #
 # Second property call: "out" command
@@ -73,31 +77,35 @@ gdb.test("fooout", "Not evaluating any expression currently")
 for i in range(5):
     run_foonext(None)
 run_fooout("""\
-<FieldAccess for parents at test.py:@NUMBER> evaluated to:\
+<FieldAccess for parents at test.lkt:@NUMBER:@NUMBER> evaluated to:\
  Bare_Foo_Node array of length 2 = {@...
 
-Now evaluating <length at test.py:@NUMBER>""")
+Now evaluating <length at test.lkt:@NUMBER:@NUMBER>""")
 run_fooout("""\
-<length at test.py:@NUMBER> evaluated to: 2
+<length at test.lkt:@NUMBER:@NUMBER> evaluated to: 2
 
-Now evaluating <children at test.py:@NUMBER>""")
+Now evaluating <children at test.lkt:@NUMBER:@NUMBER>""")
 run_fooout("""\
-<children at test.py:@NUMBER> evaluated to: Bare_Foo_Node array of length 0
+<children at test.lkt:@NUMBER:@NUMBER> evaluated to: Bare_Foo_Node array of length 0
 
-Now evaluating <Let at test.py:@NUMBER>""")
+Now evaluating <Let at test.lkt:@NUMBER:@NUMBER>""")
 run_fooout("""\
-<Let at test.py:@NUMBER> evaluated to: 1
+<Let at test.lkt:@NUMBER:@NUMBER> evaluated to: 1
 
-Now evaluating <Arithmetic + at test.py:@NUMBER>""")
+Now evaluating <Arithmetic + at test.lkt:@NUMBER:@NUMBER>""")
 run_fooout("""\
-<Arithmetic + at test.py:@NUMBER> evaluated to: 3
+<Arithmetic + at test.lkt:@NUMBER:@NUMBER> evaluated to: 3
 
-Now evaluating <Map at test.py:@NUMBER>""")
+Now evaluating <Map at test.lkt:@NUMBER:@NUMBER>""")
 run_fooout("""\
-<Map at test.py:@NUMBER> evaluated to: Integer array of length 3 = {@...
+<Map at test.lkt:@NUMBER:@NUMBER> evaluated to: Integer array of length 3 = {@...
 
-Now evaluating <Block at ???>""")
-run_fooout("<Block at ???> evaluated to: 5")
+Now evaluating <Let at test.lkt:@NUMBER:@NUMBER>""")
+run_fooout("""\
+<Let at test.lkt:@NUMBER:@NUMBER> evaluated to: 5
+
+Now evaluating <Let at test.lkt:@NUMBER:@NUMBER>""")
+run_fooout("<Let at test.lkt:@NUMBER:@NUMBER> evaluated to: 5")
 gdb.test("fooout", "Not evaluating any expression currently")
 
 #
@@ -106,33 +114,33 @@ gdb.test("fooout", "Not evaluating any expression currently")
 
 # Try to step inside while no expression is running (property just started)
 run_continue()
-run_foosi("Now evaluating <parent at test.py:@NUMBER>")
+run_foosi("Now evaluating <parent at test.lkt:@NUMBER:@NUMBER>")
 run_foosi("""\
-<parent at test.py:@NUMBER> evaluated to: <FooNodeList main.txt:1:1-1:23>
+<parent at test.lkt:@NUMBER:@NUMBER> evaluated to: <FooNodeList main.txt:1:1-1:23>
 
-Now evaluating <children at test.py:@NUMBER>""")
+Now evaluating <children at test.lkt:@NUMBER:@NUMBER>""")
 run_foosi("""\
-<children at test.py:@NUMBER> evaluated to:\
+<children at test.lkt:@NUMBER:@NUMBER> evaluated to:\
  Bare_Foo_Node array of length 3 = {@...
 
-Now evaluating <Block at ???>""")
-run_foosi("Now evaluating <Map at test.py:@NUMBER>")
-run_foosi("Now evaluating <FieldAccess for parents at test.py:@NUMBER>")
+Now evaluating <Let at test.lkt:@NUMBER:@NUMBER>""")
+run_foosi("Now evaluating <Map at test.lkt:@NUMBER:@NUMBER>")
+run_foosi("Now evaluating <FieldAccess for parents at test.lkt:@NUMBER:@NUMBER>")
 run_foosi("""\
-<FieldAccess for parents at test.py:@NUMBER> evaluated to:\
+<FieldAccess for parents at test.lkt:@NUMBER:@NUMBER> evaluated to:\
  Bare_Foo_Node array of length 2 = {@...
 
-Now evaluating <length at test.py:@NUMBER>""")
+Now evaluating <length at test.lkt:@NUMBER:@NUMBER>""")
 run_foosi("""\
-<length at test.py:@NUMBER> evaluated to: 2
+<length at test.lkt:@NUMBER:@NUMBER> evaluated to: 2
 
-Now evaluating <children at test.py:@NUMBER>""")
+Now evaluating <children at test.lkt:@NUMBER:@NUMBER>""")
 run_foosi("""\
-<children at test.py:@NUMBER> evaluated to: Bare_Foo_Node array of length 0
+<children at test.lkt:@NUMBER:@NUMBER> evaluated to: Bare_Foo_Node array of length 0
 
-Now evaluating <Let at test.py:@NUMBER>""")
+Now evaluating <Let at test.lkt:@NUMBER:@NUMBER>""")
 run_foosi(
-    "Now evaluating <FieldAccess for control_flow_helper at test.py:@NUMBER>"
+    "Now evaluating <FieldAccess for control_flow_helper at test.lkt:@NUMBER:@NUMBER>"
 )
 
 # This should get us into the call to the control_flow_helper property
--- testsuite/tests/gdb_helpers/helpers.py
+++ testsuite/tests/gdb_helpers/helpers.py
@@ -36,7 +36,7 @@ break_label = re.compile("# BREAK:([a-z_]+)$")
 
 dsl_break_map: dict[str, int] = {}
 """
-Mapping from breakpoint labels in "test.py" to the corresponding line numbers.
+Mapping from breakpoint labels in "test.lkt" to the corresponding line numbers.
 """
 
 thread_notif_pattern = r"@/(\[New Thread .*\])?/ @/(Thread \d+ hit )?/"
@@ -47,7 +47,7 @@ command has returned.
 
 
 # Fill ``dsl_break_map``
-with open("test.py") as f:
+with open("test.lkt") as f:
     for i, line in enumerate(f, 1):
         m = break_label.search(line)
         if m:
@@ -59,7 +59,7 @@ def break_dsl(label: str) -> None:
     Create a breakpoint in the DSL on the line that contains the break comment
     marker matching ``label``.
     """
-    run_foobreak(f"test.py:{dsl_break_map[label]}")
+    run_foobreak(f"test.lkt:{dsl_break_map[label]}")
 
 
 def check_var_state(var_name: str, expected: str) -> None:
