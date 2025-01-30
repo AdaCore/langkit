9fc8227d0ac1553cf7040226ad584b9e1829a493
Pierre-Marie de Rodat <derodat@adacore.com>
2025-01-30 15:30:27 +0100

    Doc: fix configuration warnings

--- doc/conf.py
+++ doc/conf.py
@@ -108,7 +108,7 @@ pygments_style = 'sphinx'
 try:
     import sphinx_rtd_theme
     html_theme = "sphinx_rtd_theme"
-    html_theme_path = sphinx_rtd_theme.get_html_theme_path()
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 except ImportError:
     html_theme = 'default'
 
@@ -139,7 +139,7 @@ except ImportError:
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
