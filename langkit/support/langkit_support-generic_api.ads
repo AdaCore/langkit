781e4a3dc2786b30af41b618a1be95fdeeb65683
Pierre-Marie de Rodat <derodat@adacore.com>
2025-06-03 13:24:57 +0000

    Langkit_Support.Generic_API: make the docstrings Sphinx-friendly

--- langkit/support/langkit_support-generic_api.ads
+++ langkit/support/langkit_support-generic_api.ads
@@ -26,19 +26,23 @@ package Langkit_Support.Generic_API is
    --
    --  All operations done using the generic API are done in the context of a
    --  Langkit-generated library. Because of this, all subprograms in the
-   --  ``Langkit_Support.Generic_API`` take, in one way or another, an argument
-   --  that contains a reference to the language Id for that library: a
-   --  ``Language_Id`` argument directly, or for instance a ``Lk_Node``
+   --  :ada:ref:`Langkit_Support.Generic_API` packages take, in one way or
+   --  another, an argument that contains a reference to the language ID for
+   --  that library: a :ada:ref:`Language_Id` argument directly, or for
+   --  instance a :ada:ref:`Langkit_Support.Generic_API.Analysis.Lk_Node`
    --  argument (analysis contexts/units/nodes contain references to the
-   --  language Id).
+   --  language ID).
    --
    --  Note however that some subprograms accept null values for
-   --  library-sensitive arguments, for instance they accept ``No_Lk_Node`` for
-   --  their ``Lk_Node`` argument. The special ``No_Lk_Node`` value contains no
-   --  reference to a particular Langkit-generated library. So even though such
-   --  subprograms take a ``Lk_Node`` argument, they also need to take a
-   --  ``Language_Id`` argument to receive the language Id information in all
-   --  cases.
+   --  library-sensitive arguments, for instance they accept
+   --  :ada:ref:`Langkit_Support.Generic_API.Analysis.No_Lk_Node` for their
+   --  :ada:ref:`Langkit_Support.Generic_API.Analysis.Lk_Node` argument. The
+   --  special :ada:ref:`Langkit_Support.Generic_API.Analysis.No_Lk_Node` value
+   --  contains no reference to a particular Langkit-generated library. So even
+   --  though such subprograms take a
+   --  :ada:ref:`Langkit_Support.Generic_API.Analysis.Lk_Node` argument, they
+   --  also need to take a :ada:ref:`Language_Id` argument to receive the
+   --  language ID information in all cases.
 
    function Language_Name (Id : Language_Id) return Name_Type;
    --  Return the name of the language that the library corresponding to ``Id``
@@ -56,26 +60,26 @@ package Langkit_Support.Generic_API is
 
    function Language (Rule : Grammar_Rule_Ref) return Language_Id;
    --  Return the language ID corresponding to the given grammar rule. Raise a
-   --  ``Precondition_Failure`` exception if ``Rule`` is
-   --  ``No_Grammar_Rule_Ref``.
+   --  :ada:ref:`Langkit_Support.Errors.Precondition_Failure` exception if
+   --  ``Rule`` is :ada:ref:`No_Grammar_Rule_Ref`.
 
    function Default_Grammar_Rule (Id : Language_Id) return Grammar_Rule_Ref;
    --  Return the default grammar rule for the given language
 
    function Grammar_Rule_Name (Rule : Grammar_Rule_Ref) return Name_Type;
    --  Return the name for the given grammar rule. Raise a
-   --  ``Precondition_Failure`` exception if ``Rule`` is
-   --  ``No_Grammar_Rule_Ref``.
+   --  :ada:ref:`Langkit_Support.Errors.Precondition_Failure` exception if
+   --  ``Rule`` is :ada:ref:`No_Grammar_Rule_Ref`.
 
    function Is_Public (Rule : Grammar_Rule_Ref) return Boolean;
    --  Return whether the given grammar rule is public. Raise a
-   --  ``Precondition_Failure`` exception if ``Rule`` is
-   --  ``No_Grammar_Rule_Ref``.
+   --  :ada:ref:`Langkit_Support.Errors.Precondition_Failure` exception if
+   --  ``Rule`` is :ada:ref:`No_Grammar_Rule_Ref`.
 
    function Grammar_Rule_Doc (Rule : Grammar_Rule_Ref) return Text_Type;
    --  Return the doc associated to this grammar rule. Raise a
-   --  ``Precondition_Failure`` exception if ``Rule`` is
-   --  ``No_Grammar_Rule_Ref``.
+   --  :ada:ref:`Langkit_Support.Errors.Precondition_Failure` exception if
+   --  ``Rule`` is :ada:ref:`No_Grammar_Rule_Ref`.
 
    type Any_Grammar_Rule_Index is new Natural;
    subtype Grammar_Rule_Index is
@@ -84,19 +88,22 @@ package Langkit_Support.Generic_API is
    --  Language-specific index to designate a grammar rule.
    --
    --  A given languages accepts ``N`` grammar rules, so the only valid indexes
-   --  for it are ``1 .. N``. The ``Last_Grammar_Rule`` function below gives
-   --  the actual ``N`` for a given language.
+   --  for it are ``1 .. N``. The :ada:ref:`Last_Grammar_Rule` function below
+   --  gives the actual ``N`` for a given language.
 
    function To_Index (Rule : Grammar_Rule_Ref) return Grammar_Rule_Index;
    --  Return the index of the given grammar rule. Raise a
-   --  ``Precondition_Failure`` exception if ``Rule`` is
-   --  ``No_Grammar_Rule_Ref``.
+   --  :ada:ref:`Langkit_Support.Errors.Precondition_Failure` exception if
+   --  ``Rule`` is :ada:ref:`No_Grammar_Rule_Ref`.
 
    function From_Index
      (Id : Language_Id; Rule : Grammar_Rule_Index) return Grammar_Rule_Ref;
-   --  Return the grammar rule for the given language corresponding to
-   --  the ``Rule`` index. Raise a ``Precondition_Failure`` exception if
+   --  Return the grammar rule for the given language corresponding to the
+   --  ``Rule`` index. Raise a
+   --  :ada:ref:`Langkit_Support.Errors.Precondition_Failure` exception if
    --  ``Rule`` is not a valid grammar rule index for the given language.
+   --
+   --% belongs-to: Grammar_Rule_Ref
 
    function Last_Grammar_Rule (Id : Language_Id) return Grammar_Rule_Index;
    --  Return the index of the last grammar rule for the given language
@@ -113,16 +120,19 @@ package Langkit_Support.Generic_API is
 
    function Language (Kind : Token_Kind_Ref) return Language_Id;
    --  Return the language ID corresponding to the given token kind. Raise a
-   --  ``Precondition_Failure`` exception if ``Kind`` is ``No_Token_Kind_Ref``.
+   --  :ada:ref:`Langkit_Support.Errors.Precondition_Failure` exception if
+   --  ``Kind`` is :ada:ref:`No_Token_Kind_Ref`.
 
    function Token_Kind_Name (Kind : Token_Kind_Ref) return Name_Type;
    --  Return the name for the given token kind. Raise a
-   --  ``Precondition_Failure`` exception if ``Kind`` is ``No_Token_Kind_Ref``.
+   --  :ada:ref:`Langkit_Support.Errors.Precondition_Failure` exception if
+   --  ``Kind`` is :ada:ref:`No_Token_Kind_Ref`.
 
    function Is_Comment (Kind : Token_Kind_Ref) return Boolean;
    --  Return whether unparsing must treat the given token kind as a comment,
    --  i.e. a trivia to preserve in unparsed sources. Raise a
-   --  ``Precondition_Failure`` exception if ``Kind`` is ``No_Token_Kind_Ref``.
+   --  :ada:ref:`Langkit_Support.Errors.Precondition_Failure` exception if
+   --  ``Kind`` is :ada:ref:`No_Token_Kind_Ref`.
 
    type Any_Token_Kind_Index is new Natural;
    subtype Token_Kind_Index is
@@ -131,21 +141,27 @@ package Langkit_Support.Generic_API is
    --  Language-specific index to designate a token kind.
    --
    --  A given languages accepts ``N`` token kinds, so the only valid indexes
-   --  for it are ``1 .. N``. The ``Last_Token_Kind`` function below gives the
-   --  actual ``N`` for a given language.
+   --  for it are ``1 .. N``. The :ada:ref:`Last_Token_Kind` function below
+   --  gives the actual ``N`` for a given language.
 
    function To_Index (Kind : Token_Kind_Ref) return Token_Kind_Index;
    --  Return the index of the given token kind. Raise a
-   --  ``Precondition_Failure`` exception if ``Kind`` is ``No_Token_Kind``.
+   --  :ada:ref:`Langkit_Support.Errors.Precondition_Failure` exception if
+   --  ``Kind`` is :ada:ref:`No_Token_Kind_Ref`.
 
    function From_Index
      (Id : Language_Id; Kind : Token_Kind_Index) return Token_Kind_Ref;
    --  Return the token kind for the given language corresponding to the
-   --  ``Kind`` index. Raise a ``Precondition_Failure`` exception if ``Kind``
-   --  is not a valid token kind index for the given language.
+   --  ``Kind`` index. Raise a
+   --  :ada:ref:`Langkit_Support.Errors.Precondition_Failure` exception if
+   --  ``Kind`` is not a valid token kind index for the given language.
+   --
+   --% belongs-to: Token_Kind_Ref
 
    function Last_Token_Kind (Id : Language_Id) return Token_Kind_Index;
    --  Return the index of the last token kind for the given language
+   --
+   --% belongs-to: Token_Kind_Ref
 
    --------------------
    -- Token families --
@@ -159,16 +175,18 @@ package Langkit_Support.Generic_API is
 
    function Language (Family : Token_Family_Ref) return Language_Id;
    --  Return the language ID corresponding to the given token family. Raise a
-   --  ``Precondition_Failure`` exception if ``Family`` is
-   --  ``No_Token_Family_Ref``.
+   --  :ada:ref:`Langkit_Support.Errors.Precondition_Failure` exception if
+   --  ``Family`` is :ada:ref:`No_Token_Family_Ref`.
 
    function Token_Family_Name (Family : Token_Family_Ref) return Name_Type;
    --  Return the name for the given token family. Raise a
-   --  ``Precondition_Failure`` exception if ``Family`` is
-   --  ``No_Token_Family_Ref``.
+   --  :ada:ref:`Langkit_Support.Errors.Precondition_Failure` exception if
+   --  ``Family`` is :ada:ref:`No_Token_Family_Ref`.
 
    function Token_Family (Kind : Token_Kind_Ref) return Token_Family_Ref;
-   --  Return the token family that owns tokens of the given ``Kind``
+   --  Return the token family that owns tokens of the given ``Kind``. Raise a
+   --  :ada:ref:`Langkit_Support.Errors.Precondition_Failure` exception if
+   --  ``Kind`` is :ada:ref:`No_Token_Kind`.
 
    type Any_Token_Family_Index is new Natural;
    subtype Token_Family_Index is
@@ -177,21 +195,27 @@ package Langkit_Support.Generic_API is
    --  Language-specific index to designate a token family.
    --
    --  A given languages accepts ``N`` token families, so the only valid
-   --  indexes for it are ``1 .. N``. The ``Last_Token_Family`` function below
-   --  gives the actual ``N`` for a given language.
+   --  indexes for it are ``1 .. N``. The :ada:ref:`Last_Token_Family` function
+   --  below gives the actual ``N`` for a given language.
 
    function To_Index (Family : Token_Family_Ref) return Token_Family_Index;
    --  Return the index of the given token family. Raise a
-   --  ``Precondition_Failure`` exception if ``Family`` is ``No_Token_Family``.
+   --  :ada:ref:`Langkit_Support.Errors.Precondition_Failure` exception if
+   --  ``Family`` is :ada:ref:`No_Token_Family_Ref`.
 
    function From_Index
      (Id : Language_Id; Family : Token_Family_Index) return Token_Family_Ref;
    --  Return the token family for the given language corresponding to the
-   --  ``Family`` index. Raise a ``Precondition_Failure`` exception if
+   --  ``Family`` index. Raise a
+   --  :ada:ref:`Langkit_Support.Errors.Precondition_Failure` exception if
    --  ``Family`` is not a valid token family index for the given language.
+   --
+   --% belongs-to: Token_Family_Ref
 
    function Last_Token_Family (Id : Language_Id) return Token_Family_Index;
    --  Return the index of the last token family for the given language
+   --
+   --% belongs-to: Token_Family_Ref
 
 private
 
