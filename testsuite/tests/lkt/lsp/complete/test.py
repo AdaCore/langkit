db18c381556fc75981528457e70337a292fa63ea
Pierre-Marie de Rodat <derodat@adacore.com>
2026-01-27 20:48:36 +0000

    lkt/lsp/complete: sort completion results
    Currently, the order of items returned by `LktNode.p_complete` is not
    significant and depends deeply on implementation details. Sort its
    result so that baselines are more resilient to changes in the
    implementation of completion.

--- testsuite/tests/lkt/lsp/complete/test.py
+++ testsuite/tests/lkt/lsp/complete/test.py
@@ -12,20 +12,17 @@ def complete_kind(name: lkt.DefId) -> str:
     return lkt.CompletionItemKind._c_to_py[name.p_completion_item_kind - 1]
 
 
-for d in dots:
-    print(f"Completing {d} ('{d.text}')")
-    for ci in d.p_complete:
-        if ci.declaration.f_syn_name is not None:
-            print(
-                f" - {ci.declaration.p_full_name}:"
-                f" {complete_kind(ci.declaration.f_syn_name)}"
-            )
-ref = refs[-2:]
-for r in ref:
-    print(f"Completing {r} ('{r.text}')")
-    for ci in r.p_complete:
-        if ci.declaration.f_syn_name is not None:
-            print(
-                f" - {ci.declaration.p_full_name}:"
-                f" {complete_kind(ci.declaration.f_syn_name)}"
-            )
+def sorted_complete(n: lkt.LktNode) -> list[lkt.Decl]:
+    result = [
+        ci.declaration
+        for ci in n.p_complete
+        if ci.declaration.f_syn_name is not None
+    ]
+    result.sort(key=lambda d: d.p_full_name)
+    return result
+
+
+for n in dots + refs[-2:]:
+    print(f"Completing {n} ('{n.text}')")
+    for d in sorted_complete(n):
+        print(f" - {d.p_full_name}: {complete_kind(d.f_syn_name)}")
