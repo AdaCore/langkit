3798d54c11cf394e112b8b8aa49ceb8548a13ccb
Pierre-Marie de Rodat <derodat@adacore.com>
2026-02-13 13:09:36 +0000

    Liblktlang: fix PLE crash in case of invalid Lkt filename

--- testsuite/drivers/lkt_toolbox_driver.py
+++ testsuite/drivers/lkt_toolbox_driver.py
@@ -3,13 +3,20 @@ from drivers.base_driver import BaseDriver
 
 class LktToolboxDriver(BaseDriver):
     """
-    Custom driver that will automatically run the "lkt_toolbox" utility
-    on the testcase's "test.lkt" file.
+    Custom driver that will automatically run the "lkt_toolbox" utility on an
+    Lkt source file.
     """
+
+    @property
+    def entry_point(self) -> str:
+        """
+        Return the name of the source file on which to run "lkt_toolbox".
+        """
+        return self.test_env.get("entry_point", "test.lkt")
 
     def run(self):
         self.run_and_check(
-            ["lkt_toolbox", "--check-invalid-decls", "test.lkt"],
+            ["lkt_toolbox", "--check-invalid-decls", self.entry_point],
             memcheck=self.memcheck_for_lkt,
             valgrind_suppressions=["gnat"],
             for_coverage=True,
