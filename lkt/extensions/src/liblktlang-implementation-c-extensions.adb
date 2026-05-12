cb613db69515e836abf4fee2616bef463b9d520f
Hugo Guerrier <guerrier@adacore.com>
2026-05-12 11:56:20 +0200

    Lkt: Remove 'LKT_PATH' fetching duplication
    Before this patch, the 'LKT_PATH' fetching logic was duplicated in the
    Python API. Now it is delegating all computation to the Ada
    implementation.

--- lkt/extensions/src/liblktlang-implementation-c-extensions.adb
+++ lkt/extensions/src/liblktlang-implementation-c-extensions.adb
@@ -10,7 +10,26 @@ package body Liblktlang.Implementation.C.Extensions is
    -- lkt_create_default_provider --
    ---------------------------------
 
-   function lkt_create_default_provider
+   function lkt_create_default_provider return lkt_unit_provider
+   is
+      Result : Internal_Unit_Provider_Access;
+   begin
+      Clear_Last_Exception;
+      Result := Create;
+      return Wrap_Private_Provider (Result);
+   exception
+      when Exc : others =>
+         pragma Annotate (Xcov, Exempt_On, "defensive code");
+         Set_Last_Exception (Exc);
+         return lkt_unit_provider (System.Null_Address);
+         pragma Annotate (Xcov, Exempt_Off);
+   end lkt_create_default_provider;
+
+   --------------------------------------------------
+   -- lkt_create_default_provider_from_directories --
+   --------------------------------------------------
+
+   function lkt_create_default_provider_from_directories
      (Directories : System.Address) return lkt_unit_provider
    is
       Result : Internal_Unit_Provider_Access;
@@ -38,6 +57,6 @@ package body Liblktlang.Implementation.C.Extensions is
          Set_Last_Exception (Exc);
          return lkt_unit_provider (System.Null_Address);
          pragma Annotate (Xcov, Exempt_Off);
-   end lkt_create_default_provider;
+   end lkt_create_default_provider_from_directories;
 
 end Liblktlang.Implementation.C.Extensions;
--- lkt/extensions/src/liblktlang-implementation-c-extensions.ads
+++ lkt/extensions/src/liblktlang-implementation-c-extensions.ads
@@ -1,6 +1,9 @@
 package Liblktlang.Implementation.C.Extensions is
 
-   function lkt_create_default_provider
+   function lkt_create_default_provider return lkt_unit_provider
+   with Export, Convention => C;
+
+   function lkt_create_default_provider_from_directories
      (Directories : System.Address) return lkt_unit_provider
    with Export, Convention => C;
 
